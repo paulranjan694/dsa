746f98a5fac5c55afd0e0138b3f2dc6bd2d7399e
Ranjan Paul <paulranjan694@gmail.com>
2024-06-23 19:45:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,35 +1,22 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int partitionIndex = -1;
-        boolean ans=false;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i-1] != nums[i] && nums[i-1]>nums[i]){
-                partitionIndex = i;
-                break;
-            }
-        }
-    
-        if(partitionIndex!= -1){
-            if(nums[0] <= target && nums[partitionIndex-1] >= target){
-                ans=bsearch(nums,0,partitionIndex-1,target);
-            }else{
-                ans=bsearch(nums,partitionIndex,nums.length-1,target);
-            }
-        }else{
-            ans=bsearch(nums,0,nums.length-1,target);
-        }
-        return ans;
-    }
+         int s=0,e=nums.length-1;
 
-    public boolean bsearch(int[] arr,int s,int e,int k){
         while(s<=e){
-            int mid = (s+e)/2;
-            if(arr[mid] == k){
-                return true;
-            }else if(arr[mid] > k){
-                e = mid-1;
-            }else{
-                s=mid+1;
+            int mid=(s+e)/2;
+            if(nums[mid]==target) return true;
+            if(nums[s] <= nums[mid]){//left array sorted
+                if(nums[s] <= target && target <= nums[mid]){
+                    e=mid-1;
+                }else{
+                    s=mid+1;
+                }
+            }else{//right array sorted
+                if(nums[mid] <= target && target <= nums[e]){
+                    s=mid+1;
+                }else{
+                    e=mid-1;
+                }
             }
         }
         return false;
