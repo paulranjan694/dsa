93371099fe39e225f5a72f0489cb34058a7f8939
Ranjan Paul <paulranjan694@gmail.com>
2024-06-23 19:44:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,34 +1,22 @@
 class Solution {
     public int search(int[] nums, int target) {
-         int partitionIndex = -1,ans=-1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i-1]>nums[i]){
-                partitionIndex = i;
-                break;
-            }
-        }
-    
-        if(partitionIndex!= -1){
-            if(nums[0] <= target && nums[partitionIndex-1] >= target){
-                ans=bsearch(nums,0,partitionIndex-1,target);
-            }else{
-                ans=bsearch(nums,partitionIndex,nums.length-1,target);
-            }
-        }else{
-            ans=bsearch(nums,0,nums.length-1,target);
-        }
-        return ans;
-    }
+        int s=0,e=nums.length-1;
 
-    public int bsearch(int[] arr,int s,int e,int k){
         while(s<=e){
-            int mid = (s+e)/2;
-            if(arr[mid] == k){
-                return mid;
-            }else if(arr[mid] > k){
-                e = mid-1;
-            }else{
-                s=mid+1;
+            int mid=(s+e)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[s] <= nums[mid]){//left array sorted
+                if(nums[s] <= target && target <= nums[mid]){
+                    e=mid-1;
+                }else{
+                    s=mid+1;
+                }
+            }else{//right array sorted
+                if(nums[mid] <= target && target >= nums[e]){
+                    s=mid+1;
+                }else{
+                    e=mid-1;
+                }
             }
         }
         return -1;
