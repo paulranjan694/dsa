ce0239f7ce93ffc45176922fd9a062fd35ba24dc
Ranjan Paul <paulranjan694@gmail.com>
2024-07-06 11:28:42 +0530

    Time: 2 ms (55.06%), Space: 43 MB (54.32%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,17 +1,16 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-        return backtrackSol(n,0,0, ans,"");
+        backtrackSol(n,0,0, ans,"");
+        return ans;
     }
 
-    public List<String> backtrackSol(int n, int open, int close, List<String> ans, String s){
+    public void backtrackSol(int n, int open, int close, List<String> ans, String s){
         if(s.length() == 2*n){
             ans.add(s);
-            return ans;
+            return;
         }
 
-        //  List<String> list1 = new ArrayList<>();
-        //  List<String> list2 = new ArrayList<>();
         if(open<n){
             backtrackSol(n, open+1,close,ans,s+"(");
         }
@@ -20,16 +19,6 @@ class Solution {
             backtrackSol(n, open,close+1,ans,s+")");
         }
 
-        // for(String e:list1){
-        //     ans.add(e);
-        // }
-        
-        // for(String e:list2){
-        //     ans.add(e);
-        // }
-
-
-        return ans;
 
     }
 }
