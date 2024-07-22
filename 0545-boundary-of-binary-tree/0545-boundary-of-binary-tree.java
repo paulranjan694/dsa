71418cb6a88ec9666c8b194b637639cdbef2b8ae
Ranjan Paul <paulranjan694@gmail.com>
2024-07-22 22:45:28 +0530

    Time: 2 ms (10.72%), Space: 44.4 MB (72.27%) - LeetHub

---
+++ 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+        ans.add(root.val);
+        leftboundary(root.left, ans);
+        addLeafNode(root,ans, true);
+        Stack<Integer> st = new Stack<>();
+        rightboundary(root.right, st);
+        while(!st.empty()){
+            int e = st.pop();
+            ans.add(e);
+        }
+        return ans;
+    }
+
+    public void leftboundary(TreeNode root, List<Integer> res){
+        if(root == null) return;
+        if(root.left !=null){
+            res.add(root.val);
+            leftboundary(root.left,res);
+        }else if(root.right!=null){
+            res.add(root.val);
+            leftboundary(root.right,res);
+        }
+    }
+
+    public void addLeafNode(TreeNode root, List<Integer> res, boolean isRoot){
+        if(root == null) return;
+        if(root.left == null && root.right == null && !isRoot){
+             res.add(root.val);
+             return;
+        }
+        addLeafNode(root.left,res, false);
+        addLeafNode(root.right,res, false);
+    }
+
+    public void rightboundary(TreeNode root, Stack<Integer> res){
+        if(root == null) return;
+        if(root.right!=null){
+            res.push(root.val);
+            rightboundary(root.right,res);
+        }else if(root.left !=null){
+            res.push(root.val);
+            rightboundary(root.left,res);
+        }
+    }
+}
