7874eb970fbb538454551046d30415134f768ad1
Ranjan Paul <paulranjan694@gmail.com>
2024-07-25 01:10:41 +0530

    Time: 2 ms (100%), Space: 45.3 MB (81.29%) - LeetHub

---
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode ans=null;
+        while(root!=null){
+            if(root.val <= p.val){
+                root = root.right;
+            }else{
+                ans=root;
+                root = root.left;
+            }
+        }
+        return ans;
+    }
+}
