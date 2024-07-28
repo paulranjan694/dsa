74ba8261ec336f60a0102332b9f0464d6e9d04d3
Ranjan Paul <paulranjan694@gmail.com>
2024-07-28 21:56:11 +0530

    Time: 0 ms (100%), Space: 41.3 MB (88.36%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,12 +14,40 @@
  * }
  */
 class Solution {
-    List<Integer> res = new ArrayList<>();
+    // List<Integer> res = new ArrayList<>();
+    // public List<Integer> preorderTraversal(TreeNode root) {
+    //     if(root==null) return res;
+    //     res.add(root.val);
+    //     preorderTraversal(root.left);
+    //     preorderTraversal(root.right);
+    //     return res;
+    // }
+
+    //Moris Preorder Traversal
     public List<Integer> preorderTraversal(TreeNode root) {
-        if(root==null) return res;
-        res.add(root.val);
-        preorderTraversal(root.left);
-        preorderTraversal(root.right);
+        List<Integer> res = new ArrayList<>();
+        TreeNode curr = root;
+
+        while(curr!=null){
+            if(curr.left==null){
+                res.add(curr.val);
+                curr = curr.right;
+            }else{
+                TreeNode prev = curr.left;
+                while(prev.right!=null && prev.right!=curr){
+                    prev=prev.right;
+                }
+
+                if(prev.right == null){
+                    res.add(curr.val);
+                    prev.right = curr;
+                    curr = curr.left;
+                }else{
+                     prev.right = null;
+                     curr=curr.right;
+                }
+            }
+        }
         return res;
     }
 
