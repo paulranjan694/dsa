f4baccfc1a77204f91b58bbd2b004429a9f7f266
Ranjan Paul <paulranjan694@gmail.com>
2024-08-01 09:36:06 +0530

    Time: 15 ms (87.36%), Space: 57.7 MB (48.41%) - LeetHub

--- 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.java
+++ 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.java
@@ -4,11 +4,26 @@ class Solution {
         for(int i=1;i<heights.length;i++){
             int diff = heights[i] - heights[i-1];
             if(diff>0){
-                pq.offer(diff);
-                if(pq.size() > ladders){
-                    int ele = pq.poll();
-                    if(ele <= bricks){
-                        bricks-=ele;
+                // pq.offer(diff);
+                // if(pq.size() > ladders){
+                //     int ele = pq.poll();
+                //     if(ele <= bricks){
+                //         bricks-=ele;
+                //     }else{
+                //         return i-1;
+                //     }
+                // }
+                if(pq.size() < ladders){
+                    pq.add(diff);
+                }else{
+                    int br = diff;
+                    if(!pq.isEmpty() && pq.peek() < diff){
+                        br = pq.poll();
+                        pq.add(diff);
+                    }
+
+                    if(br<=bricks){
+                        bricks-=br;
                     }else{
                         return i-1;
                     }
