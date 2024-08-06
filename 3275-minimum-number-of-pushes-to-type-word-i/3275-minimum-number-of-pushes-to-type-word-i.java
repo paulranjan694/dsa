ca375be7d26d893dcac3275a28a1eec36d4986a8
Ranjan Paul <paulranjan694@gmail.com>
2024-08-07 01:47:31 +0530

    Time: 0 ms (100%), Space: 41.8 MB (47.32%) - LeetHub

--- 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.java
@@ -1,29 +1,18 @@
 class Solution {
     public int minimumPushes(String word) {
-        int[] freq = new int[26];
-
-        for(int i=0;i<word.length();i++){
-            char ch = word.charAt(i);
-            freq[ch-'a']++;
+         int ans=0;
+        int temp=word.length();
+        int i=1;
+        while(temp>8){
+            ans=ans+(8*i);
+            temp=temp-8;
+            i++;
         }
-
-        Arrays.sort(freq);
-        int[] sortedFreq = new int[26];
-
-        for(int i=0;i<26;i++){
-            sortedFreq[i] = freq[25-i];
+        if(temp%8==0){
+            ans=ans+(8*i);
         }
-
-        // List<Integer> list = new ArrayList<>();
-        // for(int i : freq){
-        //     if(i!=0) list.add(i);
-        // }
-
-        // Collections.sort(list,Collections.reverseOrder());
-        int sum=0;
-        for(int i=0;i<sortedFreq.length;i++){
-           sum+= (((i/8)+1 )* sortedFreq[i]);
-        }
-        return sum;
+        else {
+            ans = ans + ((temp % 8) * i);
+        }return ans;
     }
 }
