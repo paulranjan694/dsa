190934f952b4fa3f1fd3b7e4488139ea24f4a6b9
Ranjan Paul <paulranjan694@gmail.com>
2024-08-07 01:45:41 +0530

    Time: 2 ms (16.19%), Space: 42.1 MB (13.95%) - LeetHub

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            freq[ch-'a']++;
+        }
+
+        Arrays.sort(freq);
+        int[] sortedFreq = new int[26];
+
+        for(int i=0;i<26;i++){
+            sortedFreq[i] = freq[25-i];
+        }
+
+        // List<Integer> list = new ArrayList<>();
+        // for(int i : freq){
+        //     if(i!=0) list.add(i);
+        // }
+
+        // Collections.sort(list,Collections.reverseOrder());
+        int sum=0;
+        for(int i=0;i<sortedFreq.length;i++){
+           sum+= (((i/8)+1 )* sortedFreq[i]);
+        }
+        return sum;
+    }
+}
