e95d7c9669a7040d685f36a784ea5b965c8e4e71
Ranjan Paul <paulranjan694@gmail.com>
2024-08-09 01:07:06 +0530

    Time: 1 ms (97.83%), Space: 44.9 MB (71.4%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -3,12 +3,11 @@ class Solution {
         int end=0,far=0,jumps=0;
         while(far<nums.length-1){
             int temp=far;
-            for(int i = end; i<=far;i++){
-                far = Math.max(end+nums[end],far);
+            for(int i = end;i<=temp;i++){
+                far = Math.max(i+nums[i],far);
             }
             end=temp+1;
-           
-                jumps++;
+           jumps++;
         }
         return jumps;
     }
