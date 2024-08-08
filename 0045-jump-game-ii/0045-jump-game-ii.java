317be7db70f0f7623b3ffdb3f320a01c73373f84
Ranjan Paul <paulranjan694@gmail.com>
2024-08-09 01:04:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,17 +1,15 @@
 class Solution {
     public int jump(int[] nums) {
-        int left=0,right=0,minJumps = 0,n=nums.length;
-
-        while(right < n-1){
-            int furthest = 0;
-
-            for(int i=left;i<=right;i++)
-                furthest = Math.max(furthest,nums[i]+i);
-            
-            left = right + 1;
-            right = furthest;
-            minJumps++; 
+        int end=0,far=0,jumps=0;
+        while(far<nums.length-1){
+            int temp=far;
+            for(int i = end; i<=far;i++){
+                far = Math.max(end+nums[end],far);
+            }
+            end=temp+1;
+           
+                jumps++;
         }
-        return minJumps;
+        return jumps;
     }
 }
