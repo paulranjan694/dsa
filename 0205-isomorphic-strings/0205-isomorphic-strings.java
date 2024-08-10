7a8752b93153c66740dde8886325c6b9d0adff01
Ranjan Paul <paulranjan694@gmail.com>
2024-08-10 14:00:41 +0530

    Time: 5 ms (96.54%), Space: 42.5 MB (24.21%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,27 +1,48 @@
 class Solution {
+    // public boolean isIsomorphic(String s, String t) {
+    //     if(s.length() != t.length()) return false;
+    //     HashMap<Character,Character> mapS = new HashMap<>();
+    //     HashMap<Character,Character> mapT = new HashMap<>();
+    //     for(int i=0;i<s.length();i++){
+    //         char sch = s.charAt(i);
+    //         char tch = t.charAt(i);
+    //         if(mapS.containsKey(sch)){
+    //             if(mapS.get(sch) != tch){
+    //                 return false;
+    //             }
+    //         }else{
+    //             mapS.put(sch,tch);
+    //         }
+
+    //         if(mapT.containsKey(tch)){
+    //             if(mapT.get(tch) != sch){
+    //                 return false;
+    //             }
+    //         }else{
+    //             mapT.put(tch,sch);
+    //         }
+
+
+    //     }
+    //     return true;
+    // }
+
     public boolean isIsomorphic(String s, String t) {
-        if(s.length() != t.length()) return false;
-        HashMap<Character,Character> mapS = new HashMap<>();
-        HashMap<Character,Character> mapT = new HashMap<>();
+         if(s.length() != t.length()) return false;
+
+        int[] arrS = new int[256];
+        int[] arrT = new int[256];
+
         for(int i=0;i<s.length();i++){
             char sch = s.charAt(i);
             char tch = t.charAt(i);
-            if(mapS.containsKey(sch)){
-                if(mapS.get(sch) != tch){
-                    return false;
-                }
-            }else{
-                mapS.put(sch,tch);
+            if(arrS[sch] != arrT[tch]){
+                return false;
             }
 
-            if(mapT.containsKey(tch)){
-                if(mapT.get(tch) != sch){
-                    return false;
-                }
-            }else{
-                mapT.put(tch,sch);
-            }
-
+            arrS[sch] = i+1;
+            arrT[tch] = i+1;
+ 
 
         }
         return true;
