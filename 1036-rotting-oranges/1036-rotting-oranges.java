ae5684ea5740d1c64ce1bb831e59d9d99a385264
Ranjan Paul <paulranjan694@gmail.com>
2024-08-21 01:25:01 +0530

    Time: 2 ms (71.35%), Space: 42.6 MB (29.76%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,69 +1,57 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int ans=0;
-        int n = grid.length;
-        int m = grid[0].length;
-
-        ans = bfs(grid);
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                }
-            }
-        }
-        return ans;
-    }
-
-    static class Pair{
-        int row,col;
-        Pair(int row,int col){
-            this.row=row;
-            this.col=col;
-        }
-    }
-
-    private int bfs(int[][] grid){
-       
+        int n=grid.length;
+        int m=grid[0].length;
+        int count=0;
+        int ans = 0;
+        int[][] visited = new int[n][m];
         Queue<Pair> q = new LinkedList<>();
-        int n = grid.length;
-        int m = grid[0].length;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 if(grid[i][j] == 2){
-                   q.add(new Pair(i,j));
+                    visited[i][j] = 2;
+                    q.add(new Pair(i,j,0));
+                }
+                if(grid[i][j] == 1){
+                    count++;
                 }
             }
         }
-        
-        int time=0;
+
         int[] delRow = {1,0,-1,0};
         int[] delCol = {0,-1,0,1};
 
         while(!q.isEmpty()){
-            int len=q.size();
-            while(len-- > 0){
-                int r = q.peek().row;
-                int c = q.peek().col;
-                q.poll();
-                // if(grid[r][c] != 2) continue;
-                
-                for(int i=0;i<4;i++){
-                    int newRow = r + delRow[i];
-                    int newCol = c + delCol[i];
+            int row = q.peek().row;
+            int col = q.peek().col;
+            int time = q.peek().time;
+            ans=Math.max(time,ans);
+            q.poll();
 
-                    if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] == 1){
-                        q.add(new Pair(newRow, newCol));
-                        grid[newRow][newCol]=-2;
-                    }
+            for(int i=0;i<4;i++){
+                int newRow = row + delRow[i];
+                int newCol = col + delCol[i];
+
+                if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] == 1 && visited[newRow][newCol] != 2){
+                    q.add(new Pair(newRow, newCol, time+1));
+                    visited[newRow][newCol]=2;
+                    count--;
                 }
             }
-
-            if(q.size() > 0) time++;
-
         }
-        return time;
+
+        return count == 0 ? ans : -1;
+
+
+    }
+
+    static class Pair{
+        int row,col,time;
+        Pair(int row, int col, int time){
+            this.row=row;
+            this.col=col;
+            this.time=time;
+        }
     }
 }
