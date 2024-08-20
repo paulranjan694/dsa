50e9713ac3afb9d4d7798b67305f19b44d0fb93f
Ranjan Paul <paulranjan694@gmail.com>
2024-08-20 22:13:49 +0530

    Time: 3 ms (84.54%), Space: 49.5 MB (50.03%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,14 +2,15 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int[][] visited = new int[n][m];
+       // int[][] visited = new int[n][m];
         int ans = 0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == '1' && visited[i][j] == 0){
+                if(grid[i][j] == '1'){
                     ans++;
-                    bfs(grid,visited,i,j,n,m);
+                    // bfs(grid,visited,i,j,n,m);
+                    dfs(grid,i,j,n,m);
                 }
             }
         }
@@ -17,50 +18,63 @@ class Solution {
         return ans;
     }
 
-    private void getDirection(int[][] dir){
-        dir[0][0]=0;
-        dir[0][1]=1;
-        dir[1][0]=0;
-        dir[1][1]=-1;
-        dir[2][0]=-1;
-        dir[2][1]=0;
-        dir[3][0]=1;
-        dir[3][1]=0;
+    private void dfs(char[][] grid, int row, int col, int n, int m){
+        //base cond
+        if(row<0 || row>=n || col<0 || col>=m || grid[row][col] != '1') return;
+
+        grid[row][col]='0'; //mark visited with 0
+
+        dfs(grid,row,col+1,n,m);
+        dfs(grid,row,col-1,n,m);
+        dfs(grid,row-1,col,n,m);
+        dfs(grid,row+1,col,n,m);
+
     }
 
-    private void bfs(char[][] grid, int[][] visited, int row, int col, int n, int m){
-        visited[row][col] = 1;
-        Queue<Pair> queue = new LinkedList<Pair>();
-        queue.add(new Pair(row,col));
+    // private void getDirection(int[][] dir){
+    //     dir[0][0]=0;
+    //     dir[0][1]=1;
+    //     dir[1][0]=0;
+    //     dir[1][1]=-1;
+    //     dir[2][0]=-1;
+    //     dir[2][1]=0;
+    //     dir[3][0]=1;
+    //     dir[3][1]=0;
+    // }
 
-        int[][] dir = new int[4][2];
-        getDirection(dir);
+    // private void bfs(char[][] grid, int[][] visited, int row, int col, int n, int m){
+    //     visited[row][col] = 1;
+    //     Queue<Pair> queue = new LinkedList<Pair>();
+    //     queue.add(new Pair(row,col));
 
-        while(!queue.isEmpty()){
-            int trow = queue.peek().row;
-            int tcol = queue.peek().col;
-            queue.remove();
+    //     int[][] dir = new int[4][2];
+    //     getDirection(dir);
 
-            for(int i=0;i<4;i++){
-                int dr=dir[i][0];
-                int dc=dir[i][1];
+    //     while(!queue.isEmpty()){
+    //         int trow = queue.peek().row;
+    //         int tcol = queue.peek().col;
+    //         queue.remove();
 
-                int r = trow+dr;
-                int c = tcol+dc;
-                if(r>=0 && r<n && c>=0 && c<m && grid[r][c]== '1' && visited[r][c]==0){
-                    queue.add(new Pair(r,c));
-                    visited[r][c]=1;
-                }
-            }
-        }
-    } 
+    //         for(int i=0;i<4;i++){
+    //             int dr=dir[i][0];
+    //             int dc=dir[i][1];
+
+    //             int r = trow+dr;
+    //             int c = tcol+dc;
+    //             if(r>=0 && r<n && c>=0 && c<m && grid[r][c]== '1' && visited[r][c]==0){
+    //                 queue.add(new Pair(r,c));
+    //                 visited[r][c]=1;
+    //             }
+    //         }
+    //     }
+    // } 
 
 }
 
-class Pair{
-        int row,col;
-        public Pair(int row,int col){
-            this.row=row;
-            this.col=col;
-        }
-}
+// class Pair{
+//         int row,col;
+//         public Pair(int row,int col){
+//             this.row=row;
+//             this.col=col;
+//         }
+// }
