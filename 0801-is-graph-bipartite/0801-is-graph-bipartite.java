c6dc52dcbc217565f4d8e09571ba7ae6ee239f9b
Ranjan Paul <paulranjan694@gmail.com>
2024-08-31 22:22:20 +0530

    Time: 6 ms (17.93%), Space: 45.1 MB (39.43%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -2,42 +2,39 @@ class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
         int[] color = new int[n];
-		
-		// ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        
-        // for(int i=0;i<=n;i++){
-        //     adj.add(new ArrayList<Integer>());
-        // }
 
-        // for(Vector<Integer> list : edges){
-        //     int u = list.get(0);
-        //     int v = list.get(1);
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
 
-        //     adj.get(u).add(v);
-        //     adj.get(v).add(u);
-        // }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<graph[i].length;j++){
+                adj.get(i).add(graph[i][j]);
+            }
+        }
 
-		for(int i=0;i<n;i++){
-			if(color[i] == 0){
-				if(dfs(graph,color,i,1) == false) return false;
-			}
-		}
-		return true;
+        for(int i=0;i<n;i++){
+            if(color[i] == 0){
+                if(dfs(color,adj,i,1) == false) return false;
+            }
+        }
+        return true;
     }
 
-    private boolean dfs(int[][] adj, int[] color, int node, int currColor){
-		if(color[node] != 0){
-			if(color[node] == currColor){
-				return true;
-			}else{
-				return false;
-			}
-		}
+    boolean dfs(int[] color, ArrayList<ArrayList<Integer>> adj, int node, int currentColor){
+        if(color[node] != 0){
+            if(color[node] == currentColor){
+                return true;
+            }else{
+                return false;
+            }
+        }
 
-		color[node] = currColor;
-		for(int nbr : adj[node]){
-			if(dfs(adj,color,nbr,-currColor) == false) return false;
-		}
-		return true;
-	}
+        color[node] = currentColor;
+        for(int nbr : adj.get(node)){
+            if(dfs(color,adj, nbr, -currentColor) == false) return false;
+        }
+        return true;
+    }
 }
