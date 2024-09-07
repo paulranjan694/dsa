482e5be01cafe9b52e8a42a1acf15df4521af705
Ranjan Paul <paulranjan694@gmail.com>
2024-09-07 11:36:52 +0530

    Time: 1 ms (23.37%), Space: 42 MB (20.1%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,4 +1,32 @@
 class Solution {
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+        int[][] dp = new int[n][m];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i == 0 && j == 0 && obstacleGrid[i][j] != 1) dp[i][j] = 1;
+                else{
+                    if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                    else{
+                        int up=0,left=0;
+                        if(i-1 >= 0) up = dp[i-1][j];
+                        if(j - 1 >= 0) left = dp[i][j-1];
+                        dp[i][j] = up + left;
+                    }
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+
+    /*
+    ******---Memoization---*******
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int n = obstacleGrid.length;
         int m = obstacleGrid[0].length;
@@ -24,5 +52,5 @@ class Solution {
         dp[n][m] = up + left;
 
         return dp[n][m];
-    }
+    }*/
 }
