3115064ff5d220afc9d1bf93a6d9952bee6342ac
Ranjan Paul <paulranjan694@gmail.com>
2024-09-07 21:49:29 +0530

    Time: 1 ms (99.6%), Space: 44.2 MB (37.99%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -3,12 +3,16 @@ class Solution {
         int n = triangle.size();
         int[][] dp = new int[n][n+1];
         for(int i=0;i<n;i++)
-        Arrays.fill(dp[i],-1);
-        return minTotal(triangle, n-1,n,dp);
+            Arrays.fill(dp[i],-1);
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<triangle.get(n-1).size();i++){
+            int p = minTotal(triangle, n-1,i,dp);
+            min = Math.min(min,p);
+        }
+        return min;
     }
 
     private int minTotal(List<List<Integer>> triangle, int n, int idx, int[][] dp){
-        //if(n < 0 || idx < 0 || idx >triangle.get(n).size()) return 0;
         if(n==0){
             dp[n][idx] = triangle.get(n).get(0);
             return triangle.get(n).get(0);
@@ -16,29 +20,17 @@ class Solution {
 
         int min = Integer.MAX_VALUE;
         if(idx >= 0 && dp[n][idx] != -1) return dp[n][idx];
-        if(idx >= 0 && idx < triangle.get(n).size()){
-            int path1 = Integer.MAX_VALUE ,path2 = Integer.MAX_VALUE;
-            if(idx < triangle.get(n-1).size())
-                path1 = minTotal(triangle,n-1,idx,dp) + triangle.get(n).get(idx);
 
-            if(idx - 1 >= 0 && idx-1 < triangle.get(n-1).size())
-                path2 = minTotal(triangle,n-1,idx-1,dp) + triangle.get(n).get(idx);
+        int path1 = Integer.MAX_VALUE ,path2 = Integer.MAX_VALUE;
+        if(idx < triangle.get(n-1).size())
+            path1 = minTotal(triangle,n-1,idx,dp) + triangle.get(n).get(idx);
 
-            min = Math.min(min,path1);
-            min = Math.min(min,path2);
-        }else{
-            for(int i=0;i<triangle.get(n).size();i++){
-                int path1 = Integer.MAX_VALUE ,path2 = Integer.MAX_VALUE;
-                if(i < triangle.get(n-1).size())
-                    path1 = minTotal(triangle,n-1,i,dp) + triangle.get(n).get(i);
+        if(idx - 1 >= 0 && idx-1 < triangle.get(n-1).size())
+            path2 = minTotal(triangle,n-1,idx-1,dp) + triangle.get(n).get(idx);
 
-                if(i - 1 >= 0 && i-1 < triangle.get(n-1).size())
-                    path2 = minTotal(triangle,n-1,i-1,dp) + triangle.get(n).get(i);
-
-                min = Math.min(min,path1);
-                min = Math.min(min,path2);
-            }
-        }
+        min = Math.min(min,path1);
+        min = Math.min(min,path2);
+        
         
         dp[n][idx] = min;
         return min;
