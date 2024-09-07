a113151c3f4dde26e89e38ac0c772f10315d355e
Ranjan Paul <paulranjan694@gmail.com>
2024-09-07 11:02:00 +0530

    Time: 0 ms (100%), Space: 40 MB (96.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,4 +1,24 @@
 class Solution {
+
+    //Tabulation
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j] = 1;
+                else{
+                    int up=0, left=0;
+                    if(i - 1 >= 0) up = dp[i-1][j];
+                    if(j - 1 >= 0) left = dp[i][j-1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+    /*
+    //Memoization
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         return utils(m-1,n-1,dp);
@@ -17,5 +37,5 @@ class Solution {
         dp[m][n] = up + left;
 
         return dp[m][n];
-    }
+    }*/
 }
