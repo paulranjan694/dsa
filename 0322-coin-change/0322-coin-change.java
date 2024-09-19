7d50fa266bfb7374bdad7e378177f31a2542a218
Ranjan Paul <paulranjan694@gmail.com>
2024-09-19 20:15:03 +0530

    Time: 26 ms (33.12%), Space: 44.9 MB (30.22%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,24 @@
+class Solution {
+    private final int pow = (int) Math.pow(10, 9);
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+        for(int[] row:dp)
+            Arrays.fill(row,-1);
+        int min = utils(coins,amount,n-1,dp);
+        return min >= pow ? -1 : min;
+    }
+
+    private int utils(int[] coins, int amount,int idx, int[][] dp){
+        if(idx==0){
+            if(amount % coins[0] == 0) return amount/coins[0];
+            else return pow;
+        }
+        if(dp[idx][amount] != -1) return dp[idx][amount];
+        int pick = Integer.MAX_VALUE;
+        if(amount >= coins[idx]) pick = 1 + utils(coins, amount - coins[idx],idx,dp);
+        int notPick = utils(coins, amount, idx-1,dp);
+        dp[idx][amount] = Math.min(pick,notPick);
+        return dp[idx][amount];
+    }
+}
