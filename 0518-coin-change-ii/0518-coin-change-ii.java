930a496b30ac6fb478e04728c1ddc0a479451fd6
Ranjan Paul <paulranjan694@gmail.com>
2024-09-20 00:41:37 +0530

    Time: 8 ms (32%), Space: 48.4 MB (10.09%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,6 +2,27 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int[][] dp = new int[n][amount+1];
+        
+        //base case
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0] ==0) dp[0][i] = 1;
+            else dp[0][1] = 0;
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                int notpick = dp[i-1][j];
+                int pick = 0;
+                if(j>=coins[i]) pick = dp[i][j-coins[i]];
+                dp[i][j] = pick + notpick;
+            }
+        }
+        return dp[n-1][amount];
+    }
+
+    public int change2(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
         for(int[] ar:dp) Arrays.fill(ar,-1);
         return utils(amount,coins,n-1,dp);
     }
