444912d251ba1279519163ce8f204c2db25b9a2a
Ranjan Paul <paulranjan694@gmail.com>
2024-09-19 20:25:36 +0530

    Time: 17 ms (49.38%), Space: 44.8 MB (34.48%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,6 +1,29 @@
 class Solution {
     private final int pow = (int) Math.pow(10, 9);
+    
     public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+        
+        //base case
+        for(int T=0;T<=amount;T++){
+            if(T % coins[0] == 0) dp[0][T] = T/coins[0];
+            else dp[0][T] = pow;
+        }
+
+        for(int i=1;i<n;i++){
+            for(int T=0;T<=amount;T++){
+                int pick = Integer.MAX_VALUE;
+                if(T >= coins[i]) pick = 1 + dp[i][T-coins[i]];
+                int notPick = dp[i-1][T];
+                dp[i][T] = Math.min(pick,notPick); 
+            }
+        }
+
+        return dp[n-1][amount] >= pow ? -1 : dp[n-1][amount];
+    }
+
+    public int coinChange2(int[] coins, int amount) {
         int n = coins.length;
         int[][] dp = new int[n][amount+1];
         for(int[] row:dp)
