2a58f142feb3540f56d54e8f563d8465d64fc9d9
Ranjan Paul <paulranjan694@gmail.com>
2024-09-29 09:41:43 +0530

    Time: 67 ms (45.42%), Space: 64.2 MB (36.64%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,5 +1,43 @@
 class Solution {
+
     public int maxProfit(int[] prices) {
+        int n =prices.length;
+        int[][][] dp = new int[n+1][2][3];
+        //base case
+        //1. cap==0
+        for(int i = 0;i <= n;i++){
+            for(int j=0;j<2;j++){
+                dp[i][j][0] = 0;
+            }
+        }
+        //2. index==0
+        for(int j=0;j<2;j++){
+            for( int k = 0; k<3;k++){
+                dp[n][j][k] = 0;
+            }
+        }
+
+        for(int idx = n-1;idx>=0;idx--){
+            for(int buy = 0; buy < 2; buy++){
+                for(int cap=1;cap<3;cap++){
+                    int profit = 0;
+                    if(buy==1)
+                        profit = Math.max(-prices[idx] + dp[idx+1][0][cap], dp[idx+1][1][cap]);
+                    else
+                        profit = Math.max(prices[idx] + dp[idx+1][1][cap-1], dp[idx+1][0][cap]);
+
+                    dp[idx][buy][cap] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][2];
+
+
+       
+    }
+
+    public int maxProfit2(int[] prices) {
         int n =prices.length;
         int[][][] dp = new int[n][2][3];
         for(int[][] arr : dp){
