2850a0ba9773a632fcf56c4883a05205145cb7b4
Ranjan Paul <paulranjan694@gmail.com>
2024-09-30 20:54:04 +0530

    Time: 6 ms (88.81%), Space: 43.9 MB (78.81%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,5 +1,38 @@
 class Solution {
+
+    //using binary search, just to find the length of LIS, we are not storing the LIS.
     public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(nums[0]);
+
+        for(int i = 1; i < n; i++){
+            if(nums[i]>list.get(list.size()-1)){
+                list.add(nums[i]);
+            }else{
+                int idx = lowerBound(nums[i],list,0, list.size()-1);
+                list.set(idx,nums[i]);
+            }
+        }
+        return list.size();
+    }
+
+    private int lowerBound(int ele, ArrayList<Integer> list, int s, int e){
+        while(s<e){
+            int mid = (s+e) / 2;
+            if(list.get(mid)== ele){
+                return mid;
+            }
+            if(list.get(mid)> ele){
+                e = mid;
+            }else{
+                s = mid+1;
+            }
+        }
+        return s;
+    }
+
+    public int lengthOfLIS2(int[] nums) {
         int n = nums.length;
         int[][] dp = new int[n][n+1];
         for(int[] row : dp) Arrays.fill(row,-1);
@@ -16,4 +49,4 @@ class Solution {
         int notTake = utils(nums,idx+1,prevIdx,dp);
         return dp[idx][prevIdx+1] =  Math.max(take,notTake);
     }
-}
+}         
