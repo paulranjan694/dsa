9ccb3fd54ffa8182825b8c692e17fb1accc04fdc
Ranjan Paul <paulranjan694@gmail.com>
2024-10-11 11:25:46 +0530

    Time: 6 ms (77.61%), Space: 46 MB (37.55%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        for(int i=1;i<intervals.length;i++){
-            if(intervals[i-1][1]>intervals[i][0])
+        Arrays.sort(intervals,(a,b) -> a[0]-b[0]);
+        for(int i = 1; i<intervals.length;i++){
+            if(intervals[i][0] < intervals[i-1][1]){
                 return false;
+            }
         }
         return true;
     }
