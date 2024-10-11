5549ecae89e97c36d1f33db823884b0bd6c5082a
Ranjan Paul <paulranjan694@gmail.com>
2024-10-11 11:50:25 +0530

    Time: 4 ms (90.14%), Space: 46.1 MB (28.87%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -4,22 +4,23 @@ class Solution {
         int[] start = new int[n];
         int[] end = new int[n];
 
-        for(int i = 0;i<n;i++){
-            start[i]=intervals[i][0];
-            end[i]=intervals[i][1];
+        for(int i=0;i<n;i++){
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
         }
 
         Arrays.sort(start);
         Arrays.sort(end);
-        int rooms=0;
-        int j=0;
-        for(int i=0;i<n && j<n;i++){
+
+        int rooms=0,minrooms=Integer.MIN_VALUE;
+        for(int i=0,j=0;i<n && j<n;){
             if(start[i]<end[j]){
-                rooms++;
+                rooms++;i++;
             }else{
-                j++;
+                rooms--;j++;
             }
+            minrooms=Math.max(minrooms,rooms);
         }
-        return rooms;
+        return minrooms;
     }
 }
