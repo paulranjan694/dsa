32f2831ee0693adfa807f2e0268cbc7067047845
Ranjan Paul <paulranjan694@gmail.com>
2024-10-11 18:07:22 +0530

    Time: 10 ms (38.47%), Space: 45.5 MB (39.45%) - LeetHub

---
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -0,0 +1,43 @@
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String st : strs){
+            int len = st.length();
+            sb.append(len);
+            sb.append("#");
+            sb.append(st);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> decodedStr = new ArrayList<>();
+        int prev = 0;
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i) == '#'){
+                String newStr = getString(s, prev,i);
+                int len = Integer.parseInt(newStr);
+                newStr = getString(s, i+1, i+1+len);
+                decodedStr.add(newStr);
+                prev = i+1+len;
+                i = i+len+1;
+            }
+        }
+        return decodedStr;
+    }
+
+    private String getString(String s, int start,int end){
+        StringBuilder sb = new StringBuilder();
+        for(int i= start; i < end; i++){
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
