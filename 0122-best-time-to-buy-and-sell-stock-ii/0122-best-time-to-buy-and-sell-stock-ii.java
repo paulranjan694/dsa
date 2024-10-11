aefe573764658a8c25429993f43b812bf5e9d8f9
Ranjan Paul <paulranjan694@gmail.com>
2024-10-11 21:47:33 +0530

    Time: 3 ms (10.7%), Space: 49.7 MB (5.15%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,40 +1,22 @@
 class Solution {
-
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int[][] dp = new int[n+1][2];
-        
-        //base case
-        dp[n][0] = dp[n][1] = 0;
-
-        for(int day=n-1;day>=0;day--){
-            for(int buy = 0;buy<2;buy++){
-                if(buy==1){
-                    dp[day][buy] = Math.max(-prices[day] + dp[day+1][0], dp[day+1][1]);
-                }else{
-                    dp[day][buy] = Math.max(prices[day] + dp[day+1][1], dp[day+1][0]);
-                }
-            }
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
-        return dp[0][1];
+        return utils(0,0, prices,dp);
     }
 
-    public int maxProfit2(int[] prices) {
-        int n = prices.length;
-        int[][] dp = new int[n][2];
-        for(int[] row:dp) Arrays.fill(row,-1);
-        return utils(prices,0,1,dp);
-    }
-
-    private int utils(int[] prices, int day, int buy, int[][] dp){
-        //base case
+    private int utils(int day, int buy, int[] prices, int[][] dp){
         if(day==prices.length) return 0;
         if(dp[day][buy] != -1) return dp[day][buy];
         int profit = 0;
-        if(buy==1){
-            profit = Math.max(-prices[day] + utils(prices,day+1,0,dp)/*buy*/, utils(prices,day+1,1,dp)/**not buy */);
+
+        if(buy == 1){
+            profit += Math.max((+prices[day]+utils(day+1,0,prices,dp)), utils(day+1,1,prices,dp));
         }else{
-            profit = Math.max(prices[day] + utils(prices,day+1,1,dp)/**sell */, utils(prices,day+1,0,dp)/**don't sell */);
+            profit += Math.max((-prices[day]+utils(day+1,1,prices,dp)), utils(day+1,0,prices,dp));
         }
         return dp[day][buy] = profit;
     }
