928e9d2127e8296bfc1eef3507d4543a9ba5fb64
Ranjan Paul <paulranjan694@gmail.com>
2024-10-13 18:12:56 +0530

    Time: 1 ms (99.83%), Space: 46.4 MB (6.29%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,20 @@
+/**
+    move all non-zeros first and then append all zeros
+    tc - o(n)
+    sc - o(1)
+ */
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i=0,j=0,n=nums.length;
-        while(j<n){
-            if(nums[j]!=0){
-                nums[i]=nums[j];
-                i++;
+        int idx=0,n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i] != 0){
+                nums[idx] = nums[i];
+                idx++;
             }
-            j++;
         }
 
-        while(i<n){
-            nums[i]=0;
-            i++;
+        for(int i=idx;i<n;i++){
+            nums[i] = 0;
         }
-
     }
 }
