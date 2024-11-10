31b0c8388e264487569f067e965734a72e718892
Ranjan Paul <paulranjan694@gmail.com>
2024-11-10 11:54:53 +0530

    Time: 0 ms (100%), Space: 41 MB (67.23%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,18 +1,11 @@
-/**
-    check if arr[i-1] < ar[i] Pair exists == 1, return true else not sorted and rotated.
-    check the last and first pair too...
-
- */
 class Solution {
     public boolean check(int[] nums) {
-        int n =nums.length;
-        int count=0;
+        int count=0,n=nums.length;
         for(int i=0;i<n;i++){
-            if(nums[i] > nums[(i+1) % n]){
+            if(nums[i % n] > nums[(i+1) % n]){
                 count++;
+                if(count > 1) return false;
             }
-
-            if(count > 1) return false;
         }
         return true;
     }
