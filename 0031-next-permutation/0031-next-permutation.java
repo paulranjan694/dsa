2129cb09d71fb9384c035d767a4a3a2c2712091a
Ranjan Paul <paulranjan694@gmail.com>
2024-11-11 22:46:15 +0530

    Time: 0 ms (100%), Space: 43.2 MB (13.3%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,3 +1,5 @@
+// TC - O(n)
+// SC - O(1)
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
@@ -15,34 +17,12 @@ class Solution {
         }else{
             for(int i = n-1;i>=0;i--){
                 if(nums[idx] < nums[i]){
-                swap(nums,idx,i);
-                break;
+                    swap(nums,idx,i);
+                    break;
                 }
             }
             reverse(nums,idx+1);
         }
-
-
-
-        // int i=n-2;
-        // boolean found=false;
-        // while(i>=0){
-        //     if(nums[i] < nums[i+1]){
-        //         int j=n-1;
-        //         while(j >= 0 && nums[j] <= nums[i]){
-        //             j--;
-        //         }
-        //         swap(nums,i,j);
-        //         reverse(nums,i+1);
-        //         found=true;
-        //         break;
-        //     }
-        //     i--;
-        // }
-
-        // if(found==false){
-        //     reverse(nums,0);
-        // }
     }
 
     private void reverse(int[] nums, int s){
