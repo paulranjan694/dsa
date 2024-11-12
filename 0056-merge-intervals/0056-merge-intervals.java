99863efb028bedc9f9e76a245f21f965e23e3105
Ranjan Paul <paulranjan694@gmail.com>
2024-11-13 00:00:33 +0530

    Time: 8 ms (88.64%), Space: 46.2 MB (91.03%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -7,24 +7,20 @@ class Solution {
             return a[0] - b[0];
         });
 
-        int start=intervals[0][0], end=intervals[0][1];
+        //int start=intervals[0][0], end=intervals[0][1];
         List<int[]> t = new ArrayList<>();
-        for(int i = 1;i<intervals.length;i++){
-            if(end >= intervals[i][0]){
-                if(end < intervals[i][1]) end = intervals[i][1];
+        for(int i = 0;i<intervals.length;i++){
+            if(t.isEmpty() || t.get(t.size()-1)[1] < intervals[i][0]){
+               t.add(intervals[i]);
             }else{
-                t.add(new int[]{start,end});
-                start = intervals[i][0];
-                end = intervals[i][1];
+               t.get(t.size()-1)[1] = Math.max(t.get(t.size()-1)[1], intervals[i][1]);
             }
         }
-        t.add(new int[]{start,end});
+       // t.add(new int[]{start,end});
 
         int[][] ans = new int[t.size()][2];
         for(int i=0;i<t.size();i++){
-            int[] a = t.get(i);
-            ans[i][0] = a[0];
-            ans[i][1] = a[1];
+            ans[i] = t.get(i);
         }
 
         return ans;
