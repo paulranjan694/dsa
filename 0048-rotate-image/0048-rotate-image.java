d120e6ffc1e96d9920a0fe7812fb48339f3ed9cc
Ranjan Paul <paulranjan694@gmail.com>
2024-11-12 23:00:58 +0530

    Time: 0 ms (100%), Space: 42.2 MB (44.26%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,33 +1,33 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n=matrix.length;
-
-        for(int i=0,j=0;i<n&&j<n;i++,j++){
-            for(int k=j;k<n;k++){
-                if(i!=k){
-                    swap(matrix,i,k,k,i);
+        int n = matrix.length;
+        for(int i = 0; i < n;i++){
+            for(int j=i+1; j < n; j++){
+                if(i!=j){
+                    swap(matrix,i,j,j,i);
                 }
             }
         }
-        
 
-        for(int i=0;i<n;i++){
-            reverse(matrix[i],0,n-1);
+        for(int i = 0; i < n;i++){
+            reverse(matrix[i]);
         }
     }
 
-    public void swap(int[][] matrix, int k,int l,int m,int p){
-        int t = matrix[k][l];
-        matrix[k][l] = matrix[m][p];
-        matrix[m][p] = t;
-    }
-
-    public void reverse(int[] a,int s,int e){
+    private void reverse(int[] nums){
+        int s=0;
+        int e = nums.length-1;
         while(s<=e){
-            int t = a[s];
-            a[s] = a[e];
-            a[e] = t;
+            int t = nums[s];
+            nums[s]=nums[e];
+            nums[e] = t;
             s++;e--;
         }
+    }
+
+    private void swap(int[][] nums, int i, int j, int k, int m){
+        int t = nums[i][j];
+        nums[i][j]=nums[k][m];
+        nums[k][m] = t;
     }
 }
