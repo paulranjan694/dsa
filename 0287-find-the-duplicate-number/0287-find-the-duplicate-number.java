c7dc390d2c50ba6801bb762d081b0b4cbfbc145d
Ranjan Paul <paulranjan694@gmail.com>
2024-11-14 00:40:31 +0530

    Time: 5 ms (65.07%), Space: 58.2 MB (45.97%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,5 +1,28 @@
 class Solution {
+
+    //Floyd's algo/ cycle detectation and common node from start and cyclic intersection 
+    //tc - O(2n) ~ O(n)
+    //sc - O(1)
     public int findDuplicate(int[] nums) {
+        int fast = nums[0];
+        int slow = nums[0];
+        do{
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        }while(fast != slow);
+
+        fast = nums[0];
+        while(fast != slow){
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+
+        return slow;
+    }
+    //using negative marking 
+    //tc - O(2n) ~ O(n)
+    //sc - O(1)
+    public int findDuplicate2(int[] nums) {
         int n = nums.length,ans=-1;
 
         for(int i=0;i<n;){
