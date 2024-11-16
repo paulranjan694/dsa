26c9231399947b16767ae8f65a65420d2cf33e68
Ranjan Paul <paulranjan694@gmail.com>
2024-11-16 12:16:51 +0530

    Time: 0 ms (100%), Space: 42.1 MB (60.16%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,36 +1,38 @@
 class Solution {
     public double myPow(double x, int n) {
-        boolean odd = false, negative = false;
-        long y = (long)n;
-        //if(n%2!=0) odd = true;
-        if(n < 0) negative = true;
+        boolean isNegative = false;
 
-        //if(odd) n-=1;
-        if(negative) y =(y*-1);
+        if(n < 0){
+            isNegative = true;
+            n*=-1;
+        }
 
-        double pow = findPow(x,y);
+       double pow = powUtils(x,n);
 
-        //if(odd) pow *= n;
-        if(negative) pow = 1.0/pow;
-
-        return pow;
+       if(isNegative){
+            return 1/pow;
+       }
+       return pow;
     }
 
-    double findPow(double x, long n){
-        if(x==1) return x;
-        if(n==0) return 1;
-        if(n==1) return x;
+    public double powUtils(double x, int n){
+        //base case
+        if(n == 0) return 1.0;
 
-        double p = 0.0;
-        if(n%2!=0){
-            n-=1;
-            p=findPow(x,(n/2));
-            p=p*p*x;
-        }else{
-            p=findPow(x,(n/2));
-            p=p*p;
+        double pow = 1.0;
+        boolean isOdd = false;
+
+        if(n%2 != 0){
+           isOdd = true;
         }
-        
-        return p;
+
+        double tempPow = powUtils(x,n/2);
+        pow *= (tempPow * tempPow);
+
+        if(isOdd){
+            pow*=x;
+        }
+
+        return pow;
     }
 }
