58e8e67835ce957d2393e18ce9eea02cfb935f6b
Ranjan Paul <paulranjan694@gmail.com>
2024-11-17 09:37:54 +0530

    Time: 0 ms (100%), Space: 40.5 MB (38.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,41 +1,24 @@
 class Solution {
-
-    //Tabulation
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) dp[i][j] = 1;
-                else{
-                    int up=0, left=0;
-                    if(i - 1 >= 0) up = dp[i-1][j];
-                    if(j - 1 >= 0) left = dp[i][j-1];
-                    dp[i][j] = up + left;
-                }
-            }
+        int dp[][] = new int[m][n];
+        for(int[] a : dp){
+            Arrays.fill(a,-1);
         }
-        return dp[m-1][n-1];
+        return utils(0,0,m,n,dp);
     }
 
-    /*
-    //Memoization
-    public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        return utils(m-1,n-1,dp);
-    }
-
-    private int utils(int m ,int n, int[][] dp){
-        //base cond
-        if(m==0 && n==0){
-            return dp[m][n] = 1;
+    public int utils(int r,int c, int m ,int n,int[][] dp){
+        //base case
+        if(r==m-1 && c==n-1){
+            return 1;
         }
-
-        if( m < 0 || n < 0) return 0;
-        if(dp[m][n] != 0) return dp[m][n];
-        int up = utils(m-1,n,dp);
-        int left = utils(m,n-1,dp);
-        dp[m][n] = up + left;
-
-        return dp[m][n];
-    }*/
+        if(r >= m || c >= n){
+            return 0;
+        }
+        int res = 0;
+        if(dp[r][c] != -1) return dp[r][c];
+        res += utils(r,c+1,m,n,dp);
+        res += utils(r+1,c,m,n,dp);
+        return dp[r][c] = res;
+    }
 }
