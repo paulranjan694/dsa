dad4a6d2a9d47458d1d42d5200c1ad51666aed52
Ranjan Paul <paulranjan694@gmail.com>
2024-11-17 15:52:37 +0530

    Time: 0 ms (100%), Space: 40.4 MB (38.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,23 +1,42 @@
 class Solution {
+    public int uniquePaths(int m, int n) {
+        int tempN = m+n-2;
+        return nCr(tempN,m-1);
+    }
 
-     public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m+1][n+1];
-
-        //base case
-        dp[m-1][n-1]=1;
-
-        for(int r = m-1;r>=0;r--){
-            for(int c = n-1;c>=0;c--){
-                if(r == m-1 && c==n-1) continue;
-                int temp=0;
-                temp+= dp[r][c+1];
-                temp+= dp[r+1][c];
-                dp[r][c] = temp;
-            }
+    public int nCr(int n , int r){
+        double ans = 1;
+        for(int i = 1; i <= r; i++){
+            ans*= (n-r+i);
+            ans/=(i);
         }
-        return dp[0][0];
-     }
+        return (int)ans;
+    }
 
+    //tc - O(n^2)
+    //sc - O(n^2)
+    //dp - tabulation soln
+    public int uniquePaths_dp(int m, int n) {
+    int dp[][] = new int[m+1][n+1];
+
+    //base case
+    dp[m-1][n-1]=1;
+
+    for(int r = m-1;r>=0;r--){
+        for(int c = n-1;c>=0;c--){
+            if(r == m-1 && c==n-1) continue;
+            int temp=0;
+            temp+= dp[r][c+1];
+            temp+= dp[r+1][c];
+            dp[r][c] = temp;
+        }
+    }
+    return dp[0][0];
+    }
+
+     //tc - O(n^2)
+     //sc - O(n^2) + recursion stack space O(n^2)
+     //dp - memo soln
     public int uniquePaths2(int m, int n) {
         int dp[][] = new int[m][n];
         for(int[] a : dp){
