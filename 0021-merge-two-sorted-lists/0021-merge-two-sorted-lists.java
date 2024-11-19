cfb1f6d6c6321d78401ba9889c46898396f92c1c
Ranjan Paul <paulranjan694@gmail.com>
2024-11-20 00:43:00 +0530

    Time: 0 ms (100%), Space: 42.4 MB (49.05%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,45 +10,36 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null && list2 == null) return null;
-        ListNode dummy = new ListNode(-1);
         ListNode h1 = list1;
         ListNode h2 = list2;
-        ListNode temp = dummy;
 
-        while(h1 != null || h2 != null){
-            if(h1 != null && h2 != null){
-                if(h1.val > h2.val){
-                    ListNode node = new ListNode(h2.val);
-                    temp.next = node;
-                    temp = temp.next;
-                    h2 = h2.next;
-                }else if(h1.val < h2.val){
-                    ListNode node = new ListNode(h1.val);
-                    temp.next = node;
-                    temp = temp.next;
-                    h1 = h1.next;
-                }else{
-                    ListNode node = new ListNode(h1.val);
-                    temp.next = node;
-                    temp = temp.next;
-                    h1 = h1.next;
-                    ListNode node1 = new ListNode(h2.val);
-                    temp.next = node1;
-                    temp = temp.next;
-                    h2 = h2.next;
-                }
-            }else if(h1 != null){
-                    ListNode node = new ListNode(h1.val);
-                    temp.next = node;
-                    temp = temp.next;
-                    h1 = h1.next;
-            }else if(h2 != null){
-                    ListNode node1 = new ListNode(h2.val);
-                    temp.next = node1;
-                    temp = temp.next;
-                    h2 = h2.next;
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        ListNode t = null;
+        while(h1!=null && h2!=null){
+            if(h1.val >= h2.val){
+                t = new ListNode(h2.val);
+                h2 = h2.next;
+            }else if(h1.val < h2.val){
+                t = new ListNode(h1.val);
+                h1 = h1.next;
             }
+            temp.next = t;
+            temp=temp.next;
+        }
+
+        while(h1!=null){
+            t = new ListNode(h1.val);
+            temp.next = t;
+            temp=temp.next;
+            h1 = h1.next;
+        }
+
+        while(h2!=null){
+            t = new ListNode(h2.val);
+            temp.next = t;
+            temp=temp.next;
+            h2 = h2.next;
         }
 
         return dummy.next;
