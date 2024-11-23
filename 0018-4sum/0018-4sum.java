318dd978215fc83ee8da35931f585497be5a29d3
Ranjan Paul <paulranjan694@gmail.com>
2024-11-24 00:00:39 +0530

    Time: 19 ms (35.72%), Space: 43.5 MB (91.2%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,28 +1,26 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n=nums.length;
-        List<List<Integer>> res=new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
         Arrays.sort(nums);
-        for(int i=0;i<n;i++){
-            while(i>0 && i<n && nums[i]==nums[i-1]) i++;
-
-            for(int j=i+1;j<n;j++){
-                while(j>i+1 && j<n && nums[j]==nums[j-1]) j++;
-
-                int k = j+1,l=n-1;
-                while(k<l){
-                    long sum = (long)nums[i] + nums[j] + nums[k] +nums[l];
-                    if(sum > target){
-                        l--;
-                    }else if(sum < target){
-                        k++;
+        for(int i = 0; i< n; i++){
+           while(i>0 && i<n && nums[i-1] == nums[i]) i++;
+           for(int j = i+1; j<n;j++){
+               while(j>i+1 && j<n && nums[j-1] == nums[j]) j++;
+                int left = j+1, right = n-1;
+                while(left < right){
+                    long sum = (long)nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum == (long)target){
+                        res.add(Arrays.asList(nums[i],nums[j],nums[left], nums[right]));
+                        left++;
+                    while(left < right && nums[left -1] == nums[left]) left++;
+                    }else if(sum > (long)target){
+                        right--;
                     }else{
-                        res.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
-                        k++;
-                        while(k<l && nums[k]==nums[k-1]) k++;
+                        left++;
                     }
                 }
-            }
+           }
         }
         return res;
     }
