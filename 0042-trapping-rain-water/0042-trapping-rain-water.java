394755996f831c4ac79517c1600bd7106c9b0b77
Ranjan Paul <paulranjan694@gmail.com>
2024-11-24 13:20:56 +0530

    Time: 1 ms (65.74%), Space: 46 MB (66.61%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int trap(int[] height) {
+    //optimise - TC - O(n) SC - O(1)
+    public int trap2(int[] height) {
         int n = height.length;
         int lmax=0,rmax=0, l=0,r=n-1, total = 0;
         while(l < r){
@@ -21,6 +22,29 @@ class Solution {
                 r--;
             }
         }
+        return total;
+    }
+
+    //TC - O(n). SC - O(n) => prefix and suffix arrays take n space
+    public int trap(int[] height) {
+        int n = height.length, total = 0;
+        int[] prefixmax = new int[n];
+        int[] suffixmax = new int[n];
+        prefixmax[0] = height[0];
+        suffixmax[n-1] = height[n-1];
+
+        for(int i = 1; i < n; i++){
+            prefixmax[i] = Math.max(prefixmax[i-1],height[i]);
+        }
+
+        for(int i = n-2; i >= 0; i--){
+            suffixmax[i] = Math.max(suffixmax[i+1],height[i]);
+        }
+
+        for(int i=0;i<n;i++){
+            total += Math.min(prefixmax[i],suffixmax[i]) - height[i];
+        }
+
         return total;
     }
 }
