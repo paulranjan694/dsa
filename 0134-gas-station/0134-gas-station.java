93c3ecefc704544b92826be6472f364f86678c7d
Ranjan Paul <paulranjan694@gmail.com>
2024-11-24 17:29:36 +0530

    Time: 3 ms (39.31%), Space: 56.5 MB (22.85%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,15 +1,16 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int total=0,curr=0,ans=0;
-        for(int i=0;i<gas.length;i++){
+        int ans = 0, n = gas.length,total=0, curr = 0;
+        for(int i = 0; i<n;i++){
             total += gas[i] - cost[i];
             curr += gas[i] - cost[i];
 
-            if(curr < 0){//if curr is -ve, that means the index is not the right index to start off and we can't traverse whole arr
-                curr=0;
-                ans=i+1;
+            if(curr < 0){
+                ans = i+1;
+                curr = 0;
             }
         }
-        return total < 0 ? -1 : ans;//if total diff of gas and cost is -ve, we can traverse whole arr, so return -1
+
+        return total < 0 ? -1 :ans;
     }
 }
