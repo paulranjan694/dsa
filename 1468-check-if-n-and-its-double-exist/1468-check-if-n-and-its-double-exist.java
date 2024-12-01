028f3f6b82409d85e85a27180571afd7f4ae0348
Ranjan Paul <paulranjan694@gmail.com>
2024-12-01 12:33:00 +0530

    Time: 3 ms (33.07%), Space: 44.1 MB (7.1%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -1,10 +1,19 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
         int n = arr.length;
+        Map<Integer,Integer> hm = new HashMap<>();
+        
+        for(int i=0;i<n;i++){
+            int p = arr[i] *2;
+            hm.putIfAbsent(p,i);
+        }
+
         for(int i = 0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i!=j && arr[i]==2*arr[j]) return true;
-            }
+            int f = hm.getOrDefault(arr[i],-1);
+            if(f>=0 && i != f) return true;
+            // for(int j=0;j<n;j++){
+            //     if(i!=j && arr[i]==2*arr[j]) return true;
+            // }
         }
         return false;
     }
