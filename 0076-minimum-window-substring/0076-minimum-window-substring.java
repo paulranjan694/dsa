fc1d5a377f5c0adeba6761bfa7fa3676fe19de96
Ranjan Paul <paulranjan694@gmail.com>
2024-12-09 19:43:50 +0530

    Time: 3 ms (88.01%), Space: 44.6 MB (85.4%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -3,17 +3,20 @@ class Solution {
         int n1=s.length(),n2=t.length();
         if(n2 > n1) return "";
 
-        HashMap<Character,Integer> hm = new HashMap<>();
+        // HashMap<Character,Integer> hm = new HashMap<>();
+        int[] hash = new int[256];
         for(int i=0;i<n2;i++){
             char ch = t.charAt(i);
-            hm.put(ch, hm.getOrDefault(ch,0)+1);
+            hash[ch]++;
+            // hm.put(ch, hm.getOrDefault(ch,0)+1);
         }
         int minlen=Integer.MAX_VALUE, startIdx=-1, l=0,r=0,cnt=0;
         while(r<n1){
             char c = s.charAt(r);
-            int f = hm.getOrDefault(c,0);
-            if(f>0) cnt++;
-            hm.put(c,hm.getOrDefault(c,0)-1);
+            //int f = hm.getOrDefault(c,0);
+            if(hash[c]>0) cnt++;
+            //hm.put(c,hm.getOrDefault(c,0)-1);
+            hash[c]--;
             
             while(cnt==n2){
                 if(minlen > (r-l+1)){
@@ -21,9 +24,10 @@ class Solution {
                     startIdx = l;
                 }
                 char cr = s.charAt(l);
-                hm.put(cr,hm.getOrDefault(cr,0)+1);
-                int f1 = hm.getOrDefault(cr,0);
-                if(f1>0) cnt--;
+                // hm.put(cr,hm.getOrDefault(cr,0)+1);
+                hash[cr]++;
+                //int f1 = hm.getOrDefault(cr,0);
+                if(hash[cr]>0) cnt--;
                 l++;
             }
             r++;
