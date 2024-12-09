9675b3b6c60ddd794c57d028a05bafb11677f4b5
Ranjan Paul <paulranjan694@gmail.com>
2024-12-09 23:56:26 +0530

    Time: 31 ms (57.38%), Space: 55.6 MB (10.96%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,35 +1,37 @@
-class Distance{
-    int x;
-    int y;
-    int distance;
-    public Distance(int x,int y){
+class SortComparator implements Comparator<Pair>{
+    @Override
+    public int compare(Pair a, Pair b){
+        return Integer.compare(b.dist,a.dist);
+    }
+}
+
+class Pair{
+    int x, y, dist;
+    Pair(int x, int y){
         this.x=x;
         this.y=y;
-        this.distance = (x*x)+(y*y);
+        this.dist = (x*x) + (y*y);
     }
 }
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Queue<Distance> pq = new PriorityQueue<>((Distance d1, Distance d2) -> d2.distance - d1.distance);
-        for(int i=0;i<k;i++){
-            pq.add(new Distance(points[i][0], points[i][1]));
-        }
-
-        for(int i=k;i<points.length;i++){
-            int dist = (points[i][0] * points[i][0]) + (points[i][1] * points[i][1]);
-            if(dist < pq.peek().distance){
+        PriorityQueue<Pair> pq= new PriorityQueue<>(new SortComparator());
+        int n = points.length;
+        for(int i=0;i<n;i++){
+            Pair p = new Pair(points[i][0],points[i][1]);
+            pq.add(p);
+            while(!pq.isEmpty() && pq.size()> k ){
                 pq.poll();
-                pq.add(new Distance(points[i][0], points[i][1]));
             }
         }
 
         int[][] ans = new int[k][2];
-        int i=0;
-        while(!pq.isEmpty()){
-            Distance d = pq.poll();
-            ans[i][0] = d.x;
-            ans[i][1] = d.y;
-            i++;
+        int idx=k-1;
+        while(k-- > 0){
+            Pair p1 = pq.poll();
+            ans[idx][0] = p1.x;
+            ans[idx][1] = p1.y;
+            idx--;
         }
         return ans;
     }
