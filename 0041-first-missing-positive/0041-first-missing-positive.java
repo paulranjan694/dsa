a47e3a32bcb055df1780a9c3fa47a4458607a536
Ranjan Paul <paulranjan694@gmail.com>
2024-12-15 12:44:31 +0530

    Time: 1 ms (100%), Space: 57.4 MB (40.92%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,45 +1,27 @@
 class Solution {
-
-    //tc - O(n)
-    //sc - O(1)
     public int firstMissingPositive(int[] nums) {
-        int n= nums.length;
-
-        for(int i =0;i<n;i++){
-            if(nums[i] <= 0) nums[i] = n+2;
-        }
-
-        for(int i = 0 ; i < n; i++){
-            int idx = Math.abs(nums[i]) - 1;
-            if(idx >= 0 && idx < n){
-                nums[idx] = Math.abs(nums[idx]) * -1;
+        int n = nums.length;
+        boolean oneExists = false;
+        for(int i=0;i<n;i++){
+            if(nums[i]>n || nums[i]<=0){
+                nums[i]=n+1;
             }
         }
 
-        for(int i = 1; i <= n; i++){
-            if(nums[i-1] > 0){
-                return i;
+        for(int i=0;i<n;i++){
+            int idx = Math.abs(nums[i]);
+            if(idx<=n){
+                if(nums[idx-1] > 0){
+                    nums[idx-1] = -1 * nums[idx-1];
+                }
+            }
+        }
+
+        for(int i = 0;i<n;i++){
+            if(nums[i]>=0){
+                return i+1;
             }
         }
         return n+1;
-       
-    }
-
-    //tc - O(n)
-    //sc - O(n)
-    public int firstMissingPositive2(int[] nums) {
-        int max = 0, n= nums.length;
-        Set<Integer> set = new HashSet<>();
-        for(int i=0;i<n;i++){
-            max = Math.max(max,nums[i]);
-            set.add(nums[i]);
-        }
-
-        for(int i = 1; i <= max;i++){
-            if(!set.contains(i)){
-                return i;
-            }
-        }
-        return max+1;
     }
 }
