dafa2bef3c24ba9520ff60cbe2f11c77938ea3a6
Ranjan Paul <paulranjan694@gmail.com>
2024-12-28 20:00:58 +0530

    Time: 3 ms (80.79%), Space: 42.8 MB (15.31%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -15,67 +15,82 @@
  */
 
 class Pair{
-    int index;
-    int level;
+    int r,c;
     TreeNode node;
-    Pair(TreeNode node,int index, int level){
-        this.index = index;
-        this.node = node;
-        this.level = level;
+
+    Pair(int r, int c, TreeNode n){
+        this.r=r;
+        this.c=c;
+        this.node=n;
     }
 }
 
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-       List<List<Integer>> res = new ArrayList<>();
-       if(root == null) return res;
-
-        Queue<Pair> queue = new LinkedList<>();
-        Map<Integer,Map<Integer,PriorityQueue<Integer>>> map = new TreeMap<>();
-        int level = 0;
-        queue.offer(new Pair(root,0,level));
-        while(!queue.isEmpty()){
-            level++;
-            int length = queue.size();
-            while(length-- > 0){
-                Pair p = queue.poll();
-                int index = p.index;
+        Queue<Pair> q = new LinkedList<>();
+        Map<Integer,Map<Integer,PriorityQueue<Integer>>> hm = new TreeMap<>();
+        q.add(new Pair(0,0,root));
+        while(!q.isEmpty()){
+            int len = q.size();
+            while(len-- > 0){
+                Pair p = q.poll();
+                int r = p.r;
+                int c = p.c;
                 TreeNode node = p.node;
-                int nodelevel = p.level;
-                
-                if(!map.containsKey(index)){
-                    map.put(index, new TreeMap<>());
+
+                // if(hm.containsKey(c)){
+                //     Map<Integer,PriorityQueue<Integer>> map = hm.get(c);
+                //     if(map.containsKey(r)){
+                //         PriorityQueue pq = map.get(r);
+                //         pq.add(node.val);
+                //         map.put(r,pq);
+                //     }else{
+                //         PriorityQueue<Integer> pq = new PriorityQueue<>();
+                //         pq.add(node.val);
+                //         map.put(r,pq);
+                //     }
+                // }else{
+                //     Map<Integer,PriorityQueue<Integer>> map = new HashMap<>();
+                //     PriorityQueue<Integer> pq = new PriorityQueue<>();
+                //     pq.add(node.val);
+                //     map.put(r,pq);
+                //     hm.put(c,map);
+                // }
+
+                if(!hm.containsKey(c)){
+                    hm.put(c,new TreeMap<>());
                 }
 
-                if(!map.get(index).containsKey(nodelevel)){
-                    map.get(index).put(nodelevel, new PriorityQueue<>());
+                if(!hm.get(c).containsKey(r)){
+                    hm.get(c).put(r, new PriorityQueue<>());
                 }
 
-                map.get(index).get(nodelevel).offer(node.val);
-               
+                hm.get(c).get(r).add(node.val);
 
                 if(node.left != null){
-                    queue.offer(new Pair(node.left, index-1, level));
+                    q.add(new Pair(r+1,c-1,node.left));
                 }
 
                 if(node.right != null){
-                    queue.offer(new Pair(node.right, index+1,level));
+                    q.add(new Pair(r+1,c+1,node.right));
                 }
             }
         }
 
-        for(Map.Entry<Integer,Map<Integer,PriorityQueue<Integer>>> entry : map.entrySet()){
-            int key = entry.getKey();
-            Map<Integer,PriorityQueue<Integer>> value = entry.getValue();
+        List<List<Integer>> ans = new ArrayList<>();
+    
+        for(Map.Entry<Integer,Map<Integer,PriorityQueue<Integer>>> entry : hm.entrySet()){
+            Map<Integer,PriorityQueue<Integer>> m = entry.getValue();
             List<Integer> list = new ArrayList<>();
-            for(PriorityQueue<Integer> nodes : value.values()){
-                while(!nodes.isEmpty()){
-                    int e = nodes.poll();
+            for(Map.Entry<Integer,PriorityQueue<Integer>> en : m.entrySet()){
+                PriorityQueue<Integer> pq = en.getValue();
+                while(!pq.isEmpty()){
+                    int e = pq.poll();
                     list.add(e);
                 }
             }
-            res.add(list);
+            ans.add(list);
         }
-        return res;
+        return ans;
     }
 }
