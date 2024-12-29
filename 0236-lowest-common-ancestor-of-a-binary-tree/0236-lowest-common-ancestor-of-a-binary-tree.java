9a2295ed08d07f43fc2912f12e0db178b243acda
Ranjan Paul <paulranjan694@gmail.com>
2024-12-29 17:21:27 +0530

    Time: 7 ms (65.51%), Space: 44.6 MB (65.64%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,48 +8,20 @@
  * }
  */
 class Solution {
-    boolean found = false;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Set<TreeNode> path1 = new LinkedHashSet<>();
-        Set<TreeNode> path2 = new LinkedHashSet<>();
-
-        dfs(root,p,path1);
-        found=false;
-        dfs(root,q,path2);
-        TreeNode lca=null;
-        
-        // System.out.println("path2...");
-        // for(TreeNode node : path2){
-        //     System.out.println(node.val);
-        // }
-
-        // System.out.println("path1...");
-        // for(TreeNode node : path1){
-        //     System.out.println(node.val);
-        // }
-
-        for(TreeNode node : path2){
-            if(path1.contains(node)){
-                lca=node;
-            }
+        if(root == null || root == p || root == q){
+            return root;
         }
-        return lca;
-    }
 
-    private void dfs(TreeNode root, TreeNode p, Set<TreeNode> p1){
-        if(root==null) return;
-        if(root==p){
-            found=true;
-            p1.add(root);
-            return;
-        }
-        p1.add(root);
-        dfs(root.left,p,p1);
-        if(found == true) return;
-        dfs(root.right,p,p1);
-        if(found == true) return;
-        if(found==false){
-            p1.remove(root);
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+
+        if(left==null){
+            return right;
+        }else if(right == null){
+            return left;
+        }else{
+            return root;
         }
     }
 }
