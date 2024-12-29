1c31793292c52521492dc57e7691d2eb5870cbfc
Ranjan Paul <paulranjan694@gmail.com>
2024-12-29 17:04:58 +0530

    Time: 13 ms (8.21%), Space: 45.5 MB (7.25%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,27 +8,48 @@
  * }
  */
 class Solution {
-    public TreeNode ans = null;
+    boolean found = false;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        helper(root,p,q);
-        return ans;
+        Set<TreeNode> path1 = new LinkedHashSet<>();
+        Set<TreeNode> path2 = new LinkedHashSet<>();
+
+        dfs(root,p,path1);
+        found=false;
+        dfs(root,q,path2);
+        TreeNode lca=null;
+        
+        // System.out.println("path2...");
+        // for(TreeNode node : path2){
+        //     System.out.println(node.val);
+        // }
+
+        // System.out.println("path1...");
+        // for(TreeNode node : path1){
+        //     System.out.println(node.val);
+        // }
+
+        for(TreeNode node : path2){
+            if(path1.contains(node)){
+                lca=node;
+            }
+        }
+        return lca;
     }
 
-    public boolean helper(TreeNode root, TreeNode p, TreeNode q){
-        if(root == null) return false;
-        
-        if(root == p || root == q){
-            ans = root;
-            return true;
+    private void dfs(TreeNode root, TreeNode p, Set<TreeNode> p1){
+        if(root==null) return;
+        if(root==p){
+            found=true;
+            p1.add(root);
+            return;
         }
-
-        boolean left = helper(root.left, p, q);
-        boolean right = helper(root.right, p, q);
-
-
-        if(left && right){
-            ans = root;
+        p1.add(root);
+        dfs(root.left,p,p1);
+        if(found == true) return;
+        dfs(root.right,p,p1);
+        if(found == true) return;
+        if(found==false){
+            p1.remove(root);
         }
-        return left || right;
     }
 }
