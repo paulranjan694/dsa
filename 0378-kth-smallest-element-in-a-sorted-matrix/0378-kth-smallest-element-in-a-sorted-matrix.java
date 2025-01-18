dee59ca6fe83b4719669f74399d79fa61cf1b1d9
Ranjan Paul <paulranjan694@gmail.com>
2025-01-19 00:19:15 +0530

    Time: 16 ms (52.41%), Space: 48.4 MB (57.82%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,15 +1,37 @@
+class Node{
+    int row,col,value;
+    Node(int row, int col, int value){
+        this.row = row;
+        this.col = col;
+        this.value = value;
+    }
+}
+
+class NodeComparator implements Comparator<Node>{
+    @Override
+    public int compare(Node a, Node b){
+        return a.value-b.value;
+    }
+
+}
+
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<Node> pq = new PriorityQueue<>(new NodeComparator());
         int n = matrix.length;
-        for(int i = 0;i<n;i++){
-            for(int j=0;j<n;j++){
-                pq.add(matrix[i][j]);
-                if(pq.size()>k){
-                    pq.poll();
-                }
+        for(int i = 0;i<Math.min(n,k);i++){
+            pq.offer(new Node(i,0,matrix[i][0]));
+        }
+        Node element = pq.peek();
+        while(k-- > 0){
+            element = pq.poll();
+            int row = element.row;
+            int col = element.col;
+            if(col + 1 < n){
+                pq.offer(new Node(row,col+1,matrix[row][col+1]));
             }
         }
-        return pq.peek();
+
+        return element.value;
     }
 }
