acdb50906be10f3016c0391fbb53e9191a481db0
Ranjan Paul <paulranjan694@gmail.com>
2025-02-06 19:44:44 +0530

    Time: 122 ms (76.37%), Space: 63.8 MB (9.86%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.java
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.java
@@ -11,7 +11,7 @@ class Solution {
             int num = maxHeap.poll();
             score += num;
             num = (int)Math.ceil(num/3.0);
-            maxHeap.offer(num);
+            maxHeap.add(num);
         }
 
         return score;
