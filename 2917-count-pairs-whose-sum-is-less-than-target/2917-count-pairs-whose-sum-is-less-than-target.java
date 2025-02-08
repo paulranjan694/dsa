c010dc2ff75576ad2687f50c0acaa6062b97db62
Ranjan Paul <paulranjan694@gmail.com>
2025-02-09 00:25:52 +0530

    Time: 4 ms (7.99%), Space: 42.8 MB (12.18%) - LeetHub

--- 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.java
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.java
@@ -5,17 +5,17 @@ class Solution {
         int ans=0;
         for(int i=0;i<n;i++){
             int l = i+1,h = n-1;
-            int tempCount=0;
+          
             while(l<=h){
                 int mid = l + (h-l)/2;
                 if(nums.get(i)+ nums.get(mid) < target){
-                    tempCount += mid-l+1;
+                   
                     l = mid +1;
                 }else{
                     h=mid-1;
                 }
             }
-            ans+= tempCount;
+            ans+=  l - (i + 1);;
         }
         return ans;
     }
