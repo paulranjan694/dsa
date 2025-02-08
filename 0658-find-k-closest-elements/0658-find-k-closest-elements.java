58bd5458aed5af25975af791ffd44a76e0edade5
Ranjan Paul <paulranjan694@gmail.com>
2025-02-08 17:33:45 +0530

    Time: 11 ms (39.19%), Space: 46.1 MB (47.57%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,31 +1,57 @@
-class SortCom implements Comparator<int[]>{
-    @Override
-    public int compare(int[] a, int[] b){
-        if(a[0]==b[0]){
-            return b[1]-a[1];
-        }
-        return b[0]-a[0];
-    }
-}
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        Queue<int[]> maxheap = new PriorityQueue<>(new SortCom());
-
-        for(int i=0;i<arr.length;i++){
-            int[] temp = new int[2];
-            temp[0] = Math.abs(arr[i]-x);
-            temp[1]=arr[i];
-            maxheap.add(temp);
-            if(maxheap.size() > k){
-                maxheap.poll();
+        int idx = lowerBound(arr,x);
+        int n=arr.length;
+        List<Integer> ans =  new ArrayList<>();
+        int i=idx-1,j=idx;
+        while(k > 0 && i >= 0 && j < n){
+            int abs1 = Math.abs(arr[i]-x);
+            int abs2 = Math.abs(arr[j]-x);
+            if(abs1 <= abs2){
+                ans.add(arr[i]);
+                i--;
+            }else{
+                ans.add(arr[j]);
+                j++;
             }
+            k--;
         }
 
-        List<Integer> ans = new ArrayList<>();
-        while(!maxheap.isEmpty()){
-            ans.add(maxheap.poll()[1]);
+        while(k > 0 && i >= 0){
+            ans.add(arr[i]);
+            i--;
+            k--;
         }
+
+        while(k > 0 && j < n){     
+            ans.add(arr[j]);
+            j++; 
+            k--;
+        }
+
         Collections.sort(ans);
         return ans;
+    }
+
+    private int lowerBound(int[] arr, int target){
+        int n=arr.length;
+        int l=0,h=n-1;
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            // if(arr[mid] == target) return mid;
+            if(arr[mid] >= target){
+                h = mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        // if(l>=0 && l<n && h>=0 && h<n){
+        //     if(arr[l] < arr[h]){
+        //         return l;
+        //     }else{
+        //         return h;
+        //     }
+        // }
+        return l;
     }
 }
