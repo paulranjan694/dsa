bbb101875a7836380956e46f104fe38d7af14501
Ranjan Paul <paulranjan694@gmail.com>
2025-02-12 01:33:14 +0530

    Time: 5 ms (97.52%), Space: 42.9 MB (11.96%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,49 +1,18 @@
 class Solution {
-    // public boolean isIsomorphic(String s, String t) {
-    //     if(s.length() != t.length()) return false;
-    //     HashMap<Character,Character> mapS = new HashMap<>();
-    //     HashMap<Character,Character> mapT = new HashMap<>();
-    //     for(int i=0;i<s.length();i++){
-    //         char sch = s.charAt(i);
-    //         char tch = t.charAt(i);
-    //         if(mapS.containsKey(sch)){
-    //             if(mapS.get(sch) != tch){
-    //                 return false;
-    //             }
-    //         }else{
-    //             mapS.put(sch,tch);
-    //         }
-
-    //         if(mapT.containsKey(tch)){
-    //             if(mapT.get(tch) != sch){
-    //                 return false;
-    //             }
-    //         }else{
-    //             mapT.put(tch,sch);
-    //         }
-
-
-    //     }
-    //     return true;
-    // }
-
     public boolean isIsomorphic(String s, String t) {
-         if(s.length() != t.length()) return false;
-
-        int[] arrS = new int[256];
-        int[] arrT = new int[256];
-
+        int[] mapping1 = new int[256];
+        int[] mapping2 = new int[256];
         for(int i=0;i<s.length();i++){
-            char sch = s.charAt(i);
-            char tch = t.charAt(i);
-            if(arrS[sch] != arrT[tch]){
-                return false;
+          char ch1 = s.charAt(i);
+          char ch2 = t.charAt(i);
+          if(mapping1[ch1] == 0 && mapping2[ch2]==0){
+            mapping1[ch1]=ch2;
+            mapping2[ch2]=ch1;
+          }else{
+            if(!(mapping1[ch1] == ch2 && mapping2[ch2]==ch1)){
+              return false;
             }
-
-            arrS[sch] = i+1;
-            arrT[tch] = i+1;
- 
-
+          }
         }
         return true;
     }
