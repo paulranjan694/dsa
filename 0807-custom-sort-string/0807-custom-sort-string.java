4a14d7eee9a2456bd45239334526709c1a34601f
Ranjan Paul <paulranjan694@gmail.com>
2025-02-13 01:08:11 +0530

    Time: 0 ms (100%), Space: 41.7 MB (62.36%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.java
+++ 0807-custom-sort-string/0807-custom-sort-string.java
@@ -1,27 +1,28 @@
 class Solution {
     public String customSortString(String order, String s) {
-        Map<Character,Integer> map = new HashMap<>();
+        int[] freq = new int[26];
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            freq[ch-'a']++;
+            //map.put(ch,map.getOrDefault(ch,0)+1);
         }
         
         StringBuilder sb = new StringBuilder();
 
         for(int i=0;i<order.length();i++){
             char c = order.charAt(i);
-            if(!map.containsKey(c)){
+            if(freq[c-'a']==0){
               break;
             }
-           for(int j=0;j<map.get(c);j++){
+           for(int j=0;j<freq[c-'a'];j++){
               sb.append(c);
             }
-            map.remove(c);
+            freq[c-'a']=0;
         }
         
-        for(char key : map.keySet()){
-            for(int i=0;i<map.get(key);i++){
-              sb.append(key);
+        for(int i=0;i<26;i++){
+            for(int j=0;j<freq[i];j++){
+                sb.append((char)(i+'a'));
             }
         }
         return sb.toString();
