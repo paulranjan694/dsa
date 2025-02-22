cadd6079bc12e6c15a9d74979449ec7a7a776a4b
Ranjan Paul <paulranjan694@gmail.com>
2025-02-23 00:04:48 +0530

    Time: 70 ms (59.85%), Space: 45.2 MB (75.57%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
@@ -1,28 +1,35 @@
 class Solution {
     private static final int MODULO = 1000000007;
-    public int rangeSum(int[] nums, int n, int left, int right) {
-        int[] arr = new int[n*(n+1)/2];
-        int[] pre = new int[n];
-        pre[0] = nums[0];
-        arr[0] = nums[0];
-        for(int i=1;i<n;i++){
-            pre[i] = (pre[i-1] + nums[i])% MODULO;
-            arr[i] = pre[i] % MODULO;
-        }
-        int idx=n;
-        for(int i = 0;i<n-1;i++){
-            for(int j = i+1;j<n;j++){
-                arr[idx] = (pre[j]-pre[i])% MODULO;
-                arr[idx] = arr[idx] % MODULO;
-                idx++;
-            }
+    static class Pair{
+        int element,index;
+        Pair(int element,int index){
+            this.element = element;
+            this.index = index;
         }
 
-        Arrays.sort(arr);
+    }
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        Queue<Pair> minheap = new PriorityQueue<>((Pair a, Pair b) -> a.element - b.element);
+
+        for(int i=0;i<n;i++){
+            minheap.add(new Pair(nums[i],i));
+        }
+
         int ans=0;
-        for(int i=left-1;i<right;i++){
-            ans += (arr[i] % MODULO);
-            ans = ans % MODULO;
+        for(int i=1;i<=right;i++){
+            if(minheap.size() > 0){
+                Pair top = minheap.poll();
+                if(i>=left){
+                    ans += (top.element % MODULO);
+                    ans=ans%MODULO;
+                }
+
+                if(top.index +1 < n){
+                    int newelement = (top.element + nums[top.index +1]) % MODULO;;
+                    int newindex = top.index+1;
+                    minheap.add(new Pair(newelement,newindex));
+                }
+            }
         }
         return ans % MODULO;
     }
