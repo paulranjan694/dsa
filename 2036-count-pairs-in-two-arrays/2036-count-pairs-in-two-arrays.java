5f9ba6e5d0353063aa2f983108031192e9111b4b
Ranjan Paul <paulranjan694@gmail.com>
2025-02-22 10:03:30 +0530

    Time: 39 ms (31.03%), Space: 57.8 MB (62.07%) - LeetHub

---
+++ 2036-count-pairs-in-two-arrays/2036-count-pairs-in-two-arrays.java
@@ -0,0 +1,31 @@
+class Solution {
+    public long countPairs(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        long[] a1 = new long[n];
+
+        for(int i=0;i<n;i++){
+            a1[i] = nums1[i]-nums2[i];
+        }
+
+        Arrays.sort(a1);
+        long ans=0;
+        for(int i =0;i<n;i++){
+            if(a1[i]>0){
+                ans+=n-i-1;
+            }else{
+                int left = i+1,right=a1.length-1;
+                while(left<=right){
+                    int mid = left + (right - left)/2;
+                    if(a1[i]+a1[mid] > 0){
+                        right = mid-1;
+                    }else{
+                        left = mid+1;
+                    }
+                }
+                ans += n-left;
+            }
+            
+        }
+        return ans;
+    }
+}
