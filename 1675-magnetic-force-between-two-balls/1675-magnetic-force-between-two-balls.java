0f371c4c436b9e1b6e5152dc986e9afbd5948a81
Ranjan Paul <paulranjan694@gmail.com>
2025-02-23 01:04:35 +0530

    Time: 45 ms (25.06%), Space: 55.8 MB (72.83%) - LeetHub

---
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxDistance(int[] position, int m) {
+        Arrays.sort(position);
+        int n = position.length;
+        int left=1,right=position[n-1]-position[0], minDistance = -1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(isPossible(position,m,mid)){
+                minDistance=mid;
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return minDistance;
+    }
+
+    private boolean isPossible(int[] position, int m, int minDist){
+        int count=1,n=position.length,pos=position[0];
+        for(int i=1;i<n;i++){
+            if(Math.abs(position[i] - pos) >= minDist){
+                count++;
+                pos=position[i];
+            }
+            if(count==m) return true;
+        }
+
+        return false;
+    }
+}
