f04cb2537784ba8240e3a155fa1933dde7830bf8
Ranjan Paul <paulranjan694@gmail.com>
2025-02-22 20:39:51 +0530

    Time: 73 ms (56.63%), Space: 47 MB (52.65%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
@@ -1,28 +1,29 @@
 class Solution {
-    private static final int modulo = 1000000007;
+    private static final int MODULO = 1000000007;
     public int rangeSum(int[] nums, int n, int left, int right) {
-        ArrayList<Integer> subArrSum = new ArrayList<>();
-
-        for(int i=0;i<n;i++){
-            int sum = (nums[i] % modulo);
-            for(int j=i+1;j<n;j++){
-                sum+=(nums[j]% modulo);
-                sum=sum % modulo;
-                subArrSum.add(sum % modulo);
+        int[] arr = new int[n*(n+1)/2+1];
+        int[] pre = new int[n];
+        pre[0] = nums[0];
+        arr[0] = nums[0];
+        for(int i=1;i<n;i++){
+            pre[i] = (pre[i-1] + nums[i])% MODULO;
+            arr[i] = pre[i] % MODULO;
+        }
+        int idx=n;
+        for(int i = 0;i<n-1;i++){
+            for(int j = i+1;j<n;j++){
+                arr[idx] = (pre[j]-pre[i])% MODULO;
+                arr[idx] = arr[idx] % MODULO;
+                idx++;
             }
         }
 
-        for(int i=0;i<n;i++){
-            subArrSum.add(nums[i] % modulo);
-        }
-
-        Collections.sort(subArrSum);
-
+        Arrays.sort(arr);
         int ans=0;
-        for(int i=left-1;i<=right-1;i++){
-            ans += (subArrSum.get(i) % modulo);
-            ans = ans % modulo;
+        for(int i=left;i<=right;i++){
+            ans += (arr[i] % MODULO);
+            ans = ans % MODULO;
         }
-        return ans % modulo;
+        return ans % MODULO;
     }
 }
