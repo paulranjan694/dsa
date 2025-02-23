246ac8b99f021abb4fe3e3c8d272909c4b4dd31d
Ranjan Paul <paulranjan694@gmail.com>
2025-02-23 16:29:17 +0530

    Time: 6 ms (99.69%), Space: 44.5 MB (51.38%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -1,42 +1,29 @@
 class Solution {
-    static class SortComparator implements Comparator<Integer>{
-        @Override
-        public int compare(Integer a, Integer b){
-            return Integer.compare(b, a);
-        }
-    }
     public int smallestDistancePair(int[] nums, int k) {
-        //Queue<Integer> maxheap = new PriorityQueue<>(new SortComparator());
-        //Set<Integer> alreadyExists = new HashSet<>();
-        int[] buckets = new int[1000001];
-        int n=nums.length;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                int dist=Math.abs(nums[i]-nums[j]);
-                buckets[dist]++;
-                // maxheap.add(dist);
-                // if(maxheap.size() > k){
-                //     maxheap.poll();
-                // }
-                // if(!alreadyExists.contains(dist)){
-                // }
-                //alreadyExists.add(dist);
-            }
-        }
-        int ans=-1;
-        for(int i=0;i<1000001;i++){
-            if(buckets[i] > 0){
-                k-=buckets[i];
-            }
-            if(k<=0){
-                ans=i;
-                break;
+        int n = nums.length;
+        Arrays.sort(nums);
+        int left = 0,right=nums[n-1]-nums[0],ans=-1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(countPairs(nums,mid) < k){
+                left=mid+1;
+            }else{
+                ans=mid;
+                right=mid-1;
             }
         }
         return ans;
-        // while(k-->0 && maxheap.size() > 0){
-        //     ans=maxheap.poll();
-        // }
-        //return maxheap.peek();
+    }
+
+    private int countPairs(int[] nums, int D){
+        int i=0,j=1,count=0,n=nums.length;
+        while(j<n){
+            while(nums[j]-nums[i] > D){
+                i++;
+            }
+            count += (j-i);
+            j++;
+        }
+        return count;
     }
 }
