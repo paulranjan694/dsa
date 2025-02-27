6f16a52d4a52e5e336230be2fae0d58ad78335fd
Ranjan Paul <paulranjan694@gmail.com>
2025-02-27 08:56:49 +0530

    Time: 7 ms (92.6%), Space: 45.3 MB (26.25%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,25 +1,24 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int l=0,r=0,maxlen=0,n=s.length(),maxfreq=0;
-        int[] hash = new int[26];
-        while(r<n){
-            char ch = s.charAt(r);
-            hash[ch-'A']++;
-            maxfreq = Math.max(maxfreq,hash[ch-'A']);
-            if((r-l+1) - maxfreq > k){
-                char ch1 = s.charAt(l);
-                hash[ch1-'A']--;
-                // for(int i=0;i<26;i++){
-                //     maxfreq = Math.max(maxfreq,hash[i]);
-                // }
-                l++;
+        int[] freq = new int[26];
+        int maxFreq = 0;
+        int maxLen = 0;
+        int i=0,j=0,n=s.length();
+        while(j<n){
+            freq[s.charAt(j) - 'A']++;
+            maxFreq = Math.max(maxFreq,freq[s.charAt(j) - 'A']);
+            if((j-i+1)-maxFreq > k){
+                freq[s.charAt(i) - 'A']--;
+                i++;
+                for(int l = 0;l<26;l++){
+                    maxFreq = Math.max(freq[l],maxFreq);
+                }
             }
-
-            if((r-l+1) - maxfreq <= k){
-                maxlen = Math.max(maxlen,(r-l+1));
+            if((j-i+1)-maxFreq <= k){
+                maxLen = Math.max(maxLen,(j-i+1));
             }
-            r++;
+            j++;
         }
-        return maxlen;
+        return maxLen;
     }
 }
