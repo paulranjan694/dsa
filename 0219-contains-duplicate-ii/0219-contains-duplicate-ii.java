92974301e20fd8ed211bdfe0a55728600dbd349a
Ranjan Paul <paulranjan694@gmail.com>
2025-03-01 12:13:07 +0530

    Time: 37 ms (13.85%), Space: 56.3 MB (71.71%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,26 +3,27 @@ class Solution {
         int n = nums.length,right=0,left=0;
         if(n==1) return false;
         Map<Integer,Integer> hash = new HashMap<>();
-        while(right <= k && right < n){
-            if(hash.containsKey(nums[right])){
-                return true;
-            }
-            hash.put(nums[right],hash.getOrDefault(nums[right], 0)+1);
-            right++;
-        }
+        // while(right <= k && right < n){
+        //     if(hash.containsKey(nums[right])){
+        //         return true;
+        //     }
+        //     hash.put(nums[right],hash.getOrDefault(nums[right], 0)+1);
+        //     right++;
+        // }
 
         while(right<n){
-            hash.put(nums[left],hash.get(nums[left])-1);
-            if(hash.get(nums[left])==0){
-                hash.remove(nums[left]);
-            }
-            left++;
-
             if(hash.containsKey(nums[right])){
                 return true;
             }
             hash.put(nums[right],hash.getOrDefault(nums[right], 0)+1);
             right++;
+            if(hash.size()>k){
+                hash.put(nums[left],hash.get(nums[left])-1);
+                if(hash.get(nums[left])==0){
+                    hash.remove(nums[left]);
+                }
+                left++;
+            }
         }
         return false;
     }
