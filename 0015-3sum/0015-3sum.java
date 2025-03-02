efc3fd36b307bb1659e8369df56e6800a2775725
Ranjan Paul <paulranjan694@gmail.com>
2025-03-02 21:46:24 +0530

    Time: 38 ms (30.76%), Space: 52.7 MB (16.56%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,28 +1,75 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
         Arrays.sort(nums);
-        for(int i = 0; i< n; i++){
-           //if(i-1 >= 0 && nums[i]==nums[i-1]) continue;
-           while(i>0 && i<n && nums[i-1] == nums[i]) i++;
-            int left = i+1, right = n-1;
-            while(left < right){
-                if(nums[i]+nums[left]+nums[right] == 0){
-                    // List<Integer> list = new ArrayList<>();
-                    // list.add(nums[i]);
-                    // list.add(nums[left]);
-                    // list.add(nums[right]);
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int left=i+1,right=n-1;
+            while(left<right){
+                if(right+1<n && nums[right]==nums[right+1]) {
+                    right--;
+                    continue;
+                }
+
+                int sum = nums[i]+nums[left]+nums[right];
+                if(sum==0){
+                    List<Integer> triplet = new ArrayList<>();
+                    triplet.add(nums[i]);
+                    triplet.add(nums[left]);
+                    triplet.add(nums[right]);
+                    ans.add(triplet);
                     left++;
-                   while(left < right && nums[left -1] == nums[left]) left++;
-                }else if(nums[i]+nums[left]+nums[right] > 0){
+                    right--;
+                }else if(sum > 0){
                     right--;
                 }else{
                     left++;
                 }
             }
         }
-        return res;
+        return ans;
     }
 }
+
+
+/**
+
+-1,0,0,0,0,0,1,1
+
+
+
+(0,1,5)
+
+i-> 0 - n-2
+j-> i+1 - n-1
+k-> j+1 - n
+
+cur != prev ==> duplicate
+
+
+t - n^3
+s - 1
+
+[-1,0,1,2,-1,-4] - > sort --> -1,-1,0,1,2,4
+
+-1,-1,0,1,2,4
+
+i -> 0 - n-2
+left =i+1, right = n-1
+
+-2,-1,-1,0,1,1,1,2,4
+
+(i,j,k)
+
+-1,0,1
+n=4
+r=n-1=3
+r+1 < n
+
+arr[i]+arr[left] + arr[right] -> SummaryAPIListBuilder
+if(sum==0) store the triplrt
+
+ */
