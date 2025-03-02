56d8f3b20a84727ccae20bf2068e07f406c91b5e
Ranjan Paul <paulranjan694@gmail.com>
2025-03-02 19:28:21 +0530

    Time: 4 ms (25.13%), Space: 45.7 MB (14.44%) - LeetHub

---
+++ 1324-where-will-the-ball-fall/1324-where-will-the-ball-fall.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] findBall(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int[] answer = new int[m];
+
+        for(int i=0;i<m;i++){
+            boolean possible = true;
+            int col=i;
+            for(int row=0;row<n;row++){
+                if((row==0 && (col==0 && grid[row][col]==-1) || (col==m-1 && grid[row][col]==1))){
+                    possible = false;
+                    break;
+                }
+                if(col < 0 || col > m){
+                    possible=false;
+                        break;
+                }
+                if(grid[row][col] == 1){
+                    if(col+1 < m && grid[row][col+1] == -1){
+                        possible=false;
+                        break;
+                    }
+                    col++;
+                }else{
+                    if(col-1 >= 0 && grid[row][col-1] == 1){
+                        possible=false;
+                        break;
+                    }
+                    col--;
+                }
+            }
+            if(possible==true){
+                answer[i]=col;
+            }else{
+                answer[i]=-1;
+            }
+        }
+        return answer;
+    }
+}
