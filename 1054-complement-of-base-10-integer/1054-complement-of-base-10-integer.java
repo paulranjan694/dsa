6ab766e764b784ca99b29235bff97b6db736d67c
Ranjan Paul <paulranjan694@gmail.com>
2025-03-08 00:19:51 +0530

    Time: 0 ms (100%), Space: 40.1 MB (97.97%) - LeetHub

---
+++ 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        if(n==0) return 1;
+        int ans=0,bitIndex=0;
+        while(n>1){
+            if((n & 1) == 0){
+                ans += (1<<bitIndex);
+            }
+            n = n>>1;
+            bitIndex++;
+        }
+        return ans;
+    }
+}
