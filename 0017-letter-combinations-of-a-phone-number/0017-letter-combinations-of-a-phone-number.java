0fe4cc31f4f5bb666bc07f8835a84a97fc1a3416
Ranjan Paul <paulranjan694@gmail.com>
2025-03-16 18:07:22 +0530

    Time: 0 ms (100%), Space: 42.1 MB (78.77%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,37 +1,37 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        Map<Character,String> map = new HashMap<>();
-        map.put('2',"abc");
-        map.put('3',"def");
-        map.put('4',"ghi");
-        map.put('5',"jkl");
-        map.put('6',"mno");
-        map.put('7',"pqrs");
-        map.put('8',"tuv");
-        map.put('9',"wxyz");
-
-        return utils(digits,map,0);
+        Map<Character,String> digitMapping = new HashMap<>();
+        List<String> result = new ArrayList<>();
+        if(digits.length()==0){
+            //result.add("");
+            return result;
+        }
+        digitMapping.put('2',"abc");
+        digitMapping.put('3',"def");
+        digitMapping.put('4',"ghi");
+        digitMapping.put('5',"jkl");
+        digitMapping.put('6',"mno");
+        digitMapping.put('7',"pqrs");
+        digitMapping.put('8',"tuv");
+        digitMapping.put('9',"wxyz");
+        solve(digits,digitMapping,result,new StringBuilder(),0);
+        return result;
     }
 
-    public List<String> utils(String s, Map<Character,String> map, int idx){
-        if(idx==s.length()) return new ArrayList<String>();
-        List<String> returnedList = utils(s,map,idx+1);
-        List<String> ansList = new ArrayList<String>();
-        String val = map.get(s.charAt(idx));
-        if(returnedList.size() == 0){
-            for(int i=0;i<val.length();i++){
-                String str = val.charAt(i)+"";
-                ansList.add(str);
-            }
-        }else{
-            for(int i=0;i<val.length();i++){
-                String str = val.charAt(i)+"";
-                for(String r: returnedList){
-                    String s1 = str+r;
-                    ansList.add(s1);
-                }
-            }
+    private void solve(String digits,Map<Character,String> digitMapping,List<String> result, StringBuilder ds, int index){
+        //base cond
+        if(ds.length()==digits.length()){
+            result.add(ds.toString());
+            return;
         }
-        return ansList;
+
+        String digitChars = digitMapping.get(digits.charAt(index));
+        for(int j=0;j<digitChars.length();j++){
+            ds.append(digitChars.charAt(j));
+            solve(digits,digitMapping,result,ds,index+1);
+            ds.deleteCharAt(ds.length()-1);
+
+        }
+        
     }
 }
