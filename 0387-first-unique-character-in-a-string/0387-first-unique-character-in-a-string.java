2144b834c6d9afba37953fd8e820c03f01481ca3
Ranjan Paul <paulranjan694@gmail.com>
2025-03-21 21:48:05 +0530

    Time: 7 ms (63.39%), Space: 45.4 MB (46.28%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -4,12 +4,17 @@ class Solution {
         Set<Character> set = new LinkedHashSet<>();
         for(int i=0;i<s.length();i++){
             freq[s.charAt(i)-'a']++;
-            set.add(s.charAt(i));
+            //set.add(s.charAt(i));
         }
 
-        for(char c : set){
-            if(freq[c-'a']==1) return s.indexOf(c);
+        for(int i=0;i<s.length();i++){
+           if(freq[s.charAt(i)-'a']==1) return i;
+            //set.add(s.charAt(i));
         }
+
+        // for(char c : set){
+        //     if(freq[c-'a']==1) return s.indexOf(c);
+        // }
         return -1;
     }
 }
