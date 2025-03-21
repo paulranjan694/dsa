7ce9f139cb58a1a5547293b31e4dae1e8bd8cea5
Ranjan Paul <paulranjan694@gmail.com>
2025-03-22 01:28:12 +0530

    Time: 8 ms (94.16%), Space: 44.8 MB (82.58%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -2,6 +2,7 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         int k = p.length(),n=s.length(),m=p.length();
         List<Integer> res = new ArrayList<>();
+        if(n<m) return res;
         // Map<Character,Integer> smap = new HashMap<>();
         // Map<Character,Integer> pmap = new HashMap<>();
         int[] smap = new int[26];
@@ -12,18 +13,31 @@ class Solution {
             pmap[c-'a']++;
             //pmap.put(c,pmap.getOrDefault(c, 0) + 1);
         }
-      
-        for(int i=0;i<n;i++){
-            Arrays.fill(smap,0);
-            for(int j=i;j<i+k && j < n;j++){
-                char ch = s.charAt(j);
-                smap[ch-'a']++;
-            }
-            if(areEquals(pmap,smap)){
-                res.add(i);
-            }
+
+        int l=0,r=0;
+        while(r<k){
+            char ch = s.charAt(r);
+            smap[ch-'a']++;
+            r++;
         }
 
+        if(areEquals(pmap,smap)){
+            res.add(0);
+        }
+
+        while(r<n){
+            char ch = s.charAt(r);
+            char c = s.charAt(l);
+            smap[ch-'a']++;
+            smap[c-'a']--;
+            r++;
+            l++;
+
+            if(areEquals(pmap,smap)){
+                res.add(r-k);
+            }
+        }
+      
         return res;
     }
 
