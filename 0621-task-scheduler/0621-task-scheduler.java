bcf7971fa7233a56a093aaaf256d33fd6d24e86a
Ranjan Paul <paulranjan694@gmail.com>
2025-03-28 01:56:34 +0530

    Time: 46 ms (10.23%), Space: 45.4 MB (92.64%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,38 +1,39 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
-
-        for(int e: tasks){
-            map.put(e, map.getOrDefault(e,0)+1);
-        }
-                
-        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int[] chars = new int[26];
+        int t=0;
         Queue<int[][]> queue = new LinkedList<>();
-
-        for(int e : map.values()){
-            pq.add(e);
+        Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for(char c : tasks){
+            chars[c-'A']++;
         }
 
-        int time = 0;
-        while(!pq.isEmpty() || !queue.isEmpty()){
-            if(!pq.isEmpty()){
-                int ele = pq.poll();
-                if(ele - 1 > 0){
-                    int[][] ar = new int[1][2];
-                    ar[0][0] = ele - 1;
-                    ar[0][1] = time + n;
-                    queue.offer(ar);
+        for(int i=0;i<26;i++){
+            if(chars[i] > 0){
+               maxHeap.add(chars[i]);
+            }
+        }
+
+
+
+        while(!queue.isEmpty() || !maxHeap.isEmpty()){
+            if(!maxHeap.isEmpty()){
+                int ele = maxHeap.poll();
+                if(ele -1 > 0){
+                    int[][] temp = new int[1][2];
+                    temp[0][0] = ele-1;
+                    temp[0][1] = t+n;
+                    queue.offer(temp);
                 }
             }
 
-            if(!queue.isEmpty() && queue.peek()[0][1] == time){
-                int[][] arr = queue.poll();
-                pq.add(arr[0][0]);
+            if(!queue.isEmpty() && queue.peek()[0][1] == t){
+                maxHeap.add(queue.poll()[0][0]);
             }
-            time++;
-        }
-        return time;
 
+            t++;
+        }
+        return t;
 
     }
 }
