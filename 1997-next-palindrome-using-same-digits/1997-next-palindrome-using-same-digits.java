0603bf455575da2e41d51cc4666bbcb7686d3a0c
Ranjan Paul <paulranjan694@gmail.com>
2025-03-30 02:48:54 +0530

    Time: 3 ms (91.67%), Space: 45.1 MB (97.22%) - LeetHub

--- 1997-next-palindrome-using-same-digits/1997-next-palindrome-using-same-digits.java
+++ 1997-next-palindrome-using-same-digits/1997-next-palindrome-using-same-digits.java
@@ -1,57 +1,48 @@
 class Solution {
     public String nextPalindrome(String num) {
         int n = num.length();
+        int mid = n/2;
+        if(n==1) return "";
 
-        if (n == 1) {
-            return "";
-        }
+        char[] arr = num.toCharArray();
 
-        int halfLength = n / 2;
-        List<Character> leftHalf = new ArrayList<>();
-        for (int i = 0; i < halfLength; i++) {
-            leftHalf.add(num.charAt(i));
-        }
+        if(!findNextPermutation(arr,mid)) return "";
 
-        if (!findNextPermutation(leftHalf)) {
-            return "";
+         for(int i=0,j=n-1;i<j;i++,j--){
+            arr[j]=arr[i];
         }
-
-        StringBuilder nextPalindrome = new StringBuilder();
-        for (char c : leftHalf) {
-            nextPalindrome.append(c);
-        }
-
-        if (n % 2 == 0) {
-            nextPalindrome.append(new StringBuilder(nextPalindrome).reverse());
-        } else {
-            nextPalindrome.append(num.charAt(halfLength));
-            nextPalindrome.append(new StringBuilder(nextPalindrome.substring(0, halfLength)).reverse());
-        }
-
-        if (nextPalindrome.toString().compareTo(num) > 0) {
-            return nextPalindrome.toString();
-        }
-        return "";
+        return new String(arr);
     }
 
-    private boolean findNextPermutation(List<Character> digits){
-        
-        int i = digits.size() - 2;
-        while (i >= 0 && digits.get(i) >= digits.get(i + 1)) {
+    private boolean findNextPermutation(char[] arr, int len){
+        int flag=-1;
+        int i=len-2;
+        while(i>=0){
+            if(arr[i] < arr[i+1]){
+                flag=0;
+                break;
+            }
             i--;
         }
 
-        if(i==-1){
-            return false;
-        }
+        if(flag==-1) return false;
 
-        int j = digits.size() - 1;
-        while (digits.get(j) <= digits.get(i)) {
+        int j = len-1;
+        while(j>=0 && arr[j]<=arr[i]){
             j--;
         }
 
-        Collections.swap(digits, i, j);
-        Collections.reverse(digits.subList(i + 1, digits.size()));
+       //swap
+       char c = arr[i];
+       arr[i]=arr[j];
+       arr[j]=c;
+        //reverse except first character
+        for(i=i+1,j=len-1;i<j;i++,j--){
+            c = arr[i];
+            arr[i]=arr[j];
+            arr[j]=c;
+        }
+
         return true;
     }
 }
