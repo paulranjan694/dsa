d0ef9a7cea9a724470a70dcc96105694e10247c9
Ranjan Paul <paulranjan694@gmail.com>
2025-03-30 20:32:33 +0530

    Time: 2 ms (37.66%), Space: 44.8 MB (89.56%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,32 +1,31 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> list = new ArrayList<>();
-        int n = intervals.length;
-        if(intervals.length == 0){
-            list.add(newInterval);
-            return list.toArray(new int[][]{});
-        }
- 
-        //list.add(intervals[0]);
-        int i=0;
-        while(i<n && intervals[i][1] < newInterval[0]){
-            list.add(intervals[i]);
-            i++;
+        List<int[]> listInterVals = new ArrayList<>();
+        for(int[] interval : intervals){
+            listInterVals.add(interval);
         }
 
-        while(i<n && intervals[i][0] <= newInterval[1]){
-            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
-            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
-            i++;
+        listInterVals.add(newInterval);
+        Collections.sort(listInterVals,(a,b) -> a[0]-b[0]);
+
+        List<int[]> tempRes = new ArrayList<>();
+        tempRes.add(listInterVals.get(0));
+
+        for(int i=1;i<listInterVals.size();i++){
+            if(tempRes.get(tempRes.size()-1)[1] >= listInterVals.get(i)[0]){
+                tempRes.get(tempRes.size()-1)[0] = Math.min(tempRes.get(tempRes.size()-1)[0],listInterVals.get(i)[0]);
+                tempRes.get(tempRes.size()-1)[1] = Math.max(tempRes.get(tempRes.size()-1)[1],listInterVals.get(i)[1]);
+            }else{
+                tempRes.add(listInterVals.get(i));
+            }
         }
 
-        list.add(newInterval);
-
-        while(i<n){
-            list.add(intervals[i]);
-            i++;
+        int[][] res = new int[tempRes.size()][2];
+        int index=0;
+        for(int[] ar : tempRes){
+            res[index++]=ar;
         }
-    
-        return list.toArray(new int[][]{});
+
+        return res;
     }
 }
