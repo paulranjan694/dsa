43242f8ca7dc9baf5a2e7e2630121e24ac5ab334
Ranjan Paul <paulranjan694@gmail.com>
2025-03-30 16:00:39 +0530

    Time: 3 ms (99.71%), Space: 69 MB (21.1%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,45 +10,34 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode midNode = middle(head);
-
-        ListNode revHead = reverse(midNode.next);
-        ListNode startHead = head;
-        while(revHead != null){
-            if(startHead.val != revHead.val){
-                return false;
-            }
-            startHead = startHead.next;            
-            revHead = revHead.next;
-        }
-        return true;
-    }
-
-    public ListNode reverse(ListNode head){
-        if(head == null || head.next == null){
-            return head;
-        }
-
-        ListNode temp = head;
-        ListNode prev = null;
-        while(temp != null){
-            ListNode t = temp.next;
-            temp.next = prev;
-            prev=temp;
-            temp=t;
-        }
-        head = prev;
-        return head;
-    }
-
-    public ListNode middle(ListNode head){
-        ListNode slow = head, fast = head;
-
-        while(fast!=null && fast.next != null && fast.next.next != null){
-            fast=fast.next.next;
+        if(head != null && head.next == null) return true;
+        ListNode fast=head,slow=head,prev=null,next=null;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            prev=slow;
             slow=slow.next;
         }
+        if(prev == null) return false;
+        ListNode mid=slow;
+        prev.next=null;
+        prev = null;
+        ListNode temp = mid;
+        while(temp!= null){
+           next = temp.next;
+           temp.next=prev;
+           prev=temp;
+           temp=next;
+        }
+        ListNode h1=head,h2=prev;
+        while(h1 != null && h2 != null){
+            if(h1.val != h2.val){
+                return false;
+            }
+            h1=h1.next;
+            h2=h2.next;
+        }
+        return true;
 
-        return slow;
+
     }
 }
