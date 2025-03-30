ec1d562586ad6252ad577a03fbe9021be191b3e5
Ranjan Paul <paulranjan694@gmail.com>
2025-03-30 20:59:30 +0530

    Time: 2 ms (37.66%), Space: 45.1 MB (25.76%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,12 +1,36 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> listInterVals = new ArrayList<>();
-        for(int[] interval : intervals){
-            listInterVals.add(interval);
+        if(intervals.length == 0){
+            int[][] r = new int[1][2];
+            r[0][0] = newInterval[0];
+            r[0][1] = newInterval[1];
+            return r;
         }
 
-        listInterVals.add(newInterval);
-        Collections.sort(listInterVals,(a,b) -> a[0]-b[0]);
+        
+        boolean inserted=false;
+        for(int i=0;i<intervals.length;i++){
+            if(intervals[0][1] >= newInterval[0] && !inserted){
+                listInterVals.add(newInterval);
+                inserted=true;
+            }
+            if(intervals[i][1] <= newInterval[0]){
+                listInterVals.add(intervals[i]);
+            }else{
+                if(!inserted){
+                    listInterVals.add(newInterval);
+                    inserted=true;
+                }
+                listInterVals.add(intervals[i]);
+            }
+        }
+
+        if(listInterVals.get(listInterVals.size()-1)[1] <= newInterval[1]){
+            listInterVals.add(newInterval);
+        }
+
+       // Collections.sort(listInterVals,(a,b) -> a[0]-b[0]);
 
         List<int[]> tempRes = new ArrayList<>();
         tempRes.add(listInterVals.get(0));
