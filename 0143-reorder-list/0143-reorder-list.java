70ec9d614bfd747abe52607153776528e4d2afd7
Ranjan Paul <paulranjan694@gmail.com>
2025-04-02 23:06:52 +0530

    Time: 2 ms (85.08%), Space: 48 MB (81.37%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -25,38 +25,34 @@ class Solution {
         prev.next=null;
         ListNode h2 = reverse(slow);
         ListNode h1 = head;
-        ListNode thead= new ListNode(-1);
-        ListNode th=thead;
+        // ListNode thead= new ListNode(-1);
+        // ListNode th=thead;
         while(h1!=null && h2!=null){
-            ListNode t = h1.next;
-            th.next = h1;
-            h1.next=null;
-            h1=t;
-            th=th.next;
+            ListNode t1 = h1.next;
+            ListNode t2 = h2.next;
+            h1.next = h2;
+            h2.next = t1;
 
-            t = h2.next;
-            th.next = h2;
-            h2.next=null;
-            h2=t;
-            th=th.next;
+            h1 = t1;
+            h2=t2;
         }
 
-         while(h1!=null){
-            ListNode t = h1.next;
-            th.next = h1;
-            h1.next=null;
-            h1=t;
-            th=th.next;
-        }
+        //  while(h1!=null){
+        //     ListNode t = h1.next;
+        //     th.next = h1;
+        //     h1.next=null;
+        //     h1=t;
+        //     th=th.next;
+        // }
 
-         while(h2!=null){
-            ListNode t = h2.next;
-            th.next = h2;
-            h2.next=null;
-            h2=t;
-            th=th.next;
-        }
-        head=thead.next;
+        //  while(h2!=null){
+        //     ListNode t = h2.next;
+        //     th.next = h2;
+        //     h2.next=null;
+        //     h2=t;
+        //     th=th.next;
+        // }
+        // head=thead.next;
 
     }
 
