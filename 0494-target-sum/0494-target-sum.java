821675a242227d5c0296779c118aeee3b67e9265
Ranjan Paul <paulranjan694@gmail.com>
2025-04-11 16:28:56 +0530

    Time: 5 ms (87.08%), Space: 43.9 MB (42.03%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,30 +1,31 @@
 class Solution {
-    public int findTargetSumWays(int[] arr, int d) {
-        int totalSum=0;
-        int n= arr.length;
-        for(int e : arr) totalSum += e;
-        int target = (totalSum - d);
-        if(target < 0 || target % 2 != 0) return 0;
-        target /= 2;
-        int[][] dp = new int[n][target+1];
-        
-        for(int i=0;i<n;i++){
+    public int findTargetSumWays(int[] nums, int target) {
+        int totalSum=0,n=nums.length;
+
+        for(int num : nums){
+            totalSum += num;
+        }
+
+        int t = (target+totalSum);
+        if(t % 2 != 0 || totalSum < target || t < 0) return 0;
+        t /= 2;
+
+        int[][] dp = new int[n+1][t+1];
+
+        for(int i=0;i<=n;i++){
             dp[i][0]=1;
         }
-        
-        if(arr[0]==0) dp[0][0] = 2;
-        else dp[0][0] = 1;
-        
-        if(arr[0] != 0 && arr[0] <= target) dp[0][arr[0]] = 1;
-        
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=target;j++){
-                int pick = 0;
-                if(arr[i] <= j) pick = dp[i-1][j-arr[i]];
-                int notPick = dp[i-1][j];
-                dp[i][j] = pick + notPick;
+
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=t;j++){
+                if(nums[i-1] <=j){
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
-        return dp[n-1][target];
+
+        return dp[n][t];
     }
 }
