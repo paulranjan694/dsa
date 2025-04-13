6dd4501bbe94535d355dffdd880373bf30f3f4cb
Ranjan Paul <paulranjan694@gmail.com>
2025-04-13 19:43:19 +0530

    Time: 15 ms (77.75%), Space: 45.4 MB (18.08%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,32 +1,32 @@
 class Solution {
-    static int pow = (int) Math.pow(10, 9);
+
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
 
         int[][] dp = new int[n+1][amount+1];
 
-        // for(int i=0;i<=n;i++){
-        //     Arrays.fill(dp[i],-1);
-        // }
+        for(int i=0;i<=amount;i++){
+            dp[0][i] = Integer.MAX_VALUE-1;
+        }
 
         for(int i=0;i<=amount;i++){
-            if(i%coins[0] ==0){
-                dp[0][i]=i/coins[0];
+            if(i%coins[0] == 0){
+                dp[1][i] = i/coins[0];
             }else{
-                dp[0][i] = pow;
+                dp[1][i] = Integer.MAX_VALUE-1;
             }
         }
 
-        for(int i=1;i<=n;i++){
+        for(int i=2;i<=n;i++){
             for(int j=1;j<=amount;j++){
-                if(coins[i-1] <= j){
-                    dp[i][j] = Math.min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
+                if(coins[i-1]<=j){
+                    dp[i][j] = Math.min(dp[i-1][j], 1 + dp[i][j-coins[i-1]]);
                 }else{
                     dp[i][j] = dp[i-1][j];
                 }
             }
         }
 
-        return dp[n][amount]==pow?-1:dp[n][amount];
+        return dp[n][amount] == Integer.MAX_VALUE-1 ? -1 : dp[n][amount];
     }
 }
