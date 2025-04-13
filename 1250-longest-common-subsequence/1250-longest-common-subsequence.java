511b956c6f39a2c97226f26c22d2aa250c91cb08
Ranjan Paul <paulranjan694@gmail.com>
2025-04-14 00:06:21 +0530

    Time: 25 ms (28.23%), Space: 50.5 MB (86.72%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,43 +1,31 @@
 class Solution {
-    
-    public int longestCommonSubsequence(String s1, String s2) {
-        int n = s1.length(), m = s2.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(),n=text2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int[] ar : dp){
+            Arrays.fill(ar,-1);
         }
-        for(int j=0;j<=m;j++){
-            dp[0][j]=0;
-        }
-
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] =  1 + dp[i-1][j-1];
-                }else{
-                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
-        return dp[n][m];
+        utils(text1,text2,m,n,dp);
+        return dp[m][n];
     }
 
-    public int longestCommonSubsequence2(String s1, String s2) {
-        int n = s1.length(), m = s2.length();
-        int[][] dp = new int[n][m];
-        for(int[] row:dp) Arrays.fill(row,-1);
-        return utils(s1,s2,n-1,m-1,dp);
-    }
-
-    private int utils(String s1, String s2, int n, int m, int[][] dp){
-        if(n<0 || m <0) return 0;
-        if(dp[n][m] != -1) return dp[n][m];
-        if(s1.charAt(n) == s2.charAt(m)){
-            int match =  1 + utils(s1,s2,n-1,m-1,dp);
-            return dp[n][m] = match;
+    private int utils(String x, String y, int m, int n, int[][] dp){
+        //base cond
+        if(m==0 | n==0){
+            return 0;
         }
-        int notMatch = Math.max(utils(s1,s2,n-1,m,dp),utils(s1,s2,n,m-1,dp));
-        dp[n][m] = notMatch;
-        return notMatch;
+
+        //memoize lookup
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+
+        //recursive tree
+        if(x.charAt(m-1)==y.charAt(n-1)){
+            return dp[m][n] = 1 + utils(x,y,m-1,n-1,dp);
+        }else{
+            return dp[m][n] = Math.max(utils(x,y,m-1,n,dp),utils(x,y,m,n-1,dp));
+        }
+
     }
 }
