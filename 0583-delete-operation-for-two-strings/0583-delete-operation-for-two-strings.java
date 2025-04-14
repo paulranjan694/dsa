1f836c494b20199f95587839fa0e87ba662407bf
Ranjan Paul <paulranjan694@gmail.com>
2025-04-15 01:23:28 +0530

    Time: 5 ms (97.91%), Space: 44.8 MB (72.67%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -7,10 +7,11 @@ class Solution {
     
     private int lcs(String s1, String s2, int n, int m){
         int[][] dp = new int[n+1][m+1];
-
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                if(arr1[i-1]==arr2[j-1]){
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }else{
                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
