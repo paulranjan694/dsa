2243ec9874fb73a985f8cc7dd54e272929173e63
Ranjan Paul <paulranjan694@gmail.com>
2025-04-15 02:22:26 +0530

    Time: 14 ms (96.83%), Space: 45.4 MB (67.96%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,30 +1,27 @@
 class Solution {
     public int minInsertions(String s) {
-        StringBuilder temp = new StringBuilder(s);
-        String t = temp.reverse().toString();
-        int lcsLen = lcs(s,t);//longest palindromic subsequence
-        return s.length() - lcsLen;
+        int n = s.length();
+        String revS = new StringBuilder(s).reverse().toString();
+        int lcsLen =  lcs(s,revS,n,n);
+        return n-lcsLen;
     }
 
-    private int lcs(String s1, String s2) {
-        int n = s1.length(), m = s2.length();
+    public int lcs(String s1,String s2, int n, int m){
         int[][] dp = new int[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-        }
-        for(int j=0;j<=m;j++){
-            dp[0][j]=0;
-        }
+        
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] =  1 + dp[i-1][j-1];
+                if(arr1[i-1] == arr2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }else{
-                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
                 }
             }
         }
         return dp[n][m];
     }
+
 }
