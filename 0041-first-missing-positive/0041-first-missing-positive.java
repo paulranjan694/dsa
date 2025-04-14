67a37724c3e03e5103af9b7f0495a753d7cd4461
Ranjan Paul <paulranjan694@gmail.com>
2025-04-14 22:59:11 +0530

    Time: 1 ms (99.99%), Space: 57.2 MB (57.3%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,24 +1,35 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        boolean isOnePresent = false;
         int n = nums.length;
-        int idx = 0,val=0;
-        
-        while(idx < n){
-            val = nums[idx]-1;
-            if(val<n && val >= 0 && nums[val] != nums[idx]){
-            int t = nums[idx];
-            nums[idx] = nums[val];
-            nums[val] = t;
-            }else{
-            idx++;
-            }
-        }
-        
         for(int i=0;i<n;i++){
-            if(i+1 != nums[i]){
-            return i+1;
+            if(nums[i]==1){
+                isOnePresent = true;
+                break;
             }
         }
+        if(!isOnePresent){
+            return 1;
+        }
+
+        int idx=0;
+        while(idx < n){
+            int correctindex = nums[idx]-1;
+            if(nums[idx] > 0 && nums[idx] <= n && nums[idx]!= nums[correctindex]){
+                int t = nums[idx];
+                nums[idx] = nums[correctindex];
+                nums[correctindex] = t;
+            }else{
+                idx++;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1){
+                return i+1;
+            }
+        }
+
         return n+1;
     }
 }
