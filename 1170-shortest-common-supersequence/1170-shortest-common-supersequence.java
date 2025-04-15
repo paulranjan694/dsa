b7259ba853c3bde94e6ffa4d78a62d068dbe0787
Ranjan Paul <paulranjan694@gmail.com>
2025-04-15 23:52:52 +0530

    Time: 11 ms (88.15%), Space: 50.2 MB (87.74%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -1,50 +1,56 @@
 class Solution {
-    public String shortestCommonSupersequence(String s1, String s2) {
-        int n = s1.length(),m = s2.length();
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int n = str1.length(),m=str2.length();
+
+        char[] ar1 = str1.toCharArray();
+        char[] ar2 = str2.toCharArray();
+
+        int[][] dp = lcs(ar1,ar2,n,m);
+
+        StringBuilder sb = new StringBuilder();
+
+        int i = n, j= m;
+        while(i>0&&j>0){
+            if(ar1[i-1]==ar2[j-1]){
+                sb.append(ar1[i-1]);
+                i--;
+                j--;
+            }else{
+                if(dp[i][j-1] > dp[i-1][j]){
+                    sb.append(ar2[j-1]);
+                    j--;
+                }else{
+                    sb.append(ar1[i-1]);
+                    i--;
+                }
+            }
+        }
+
+        while(i>0){
+            sb.append(ar1[i-1]);
+            i--;
+        }
+
+        while(j>0){
+            sb.append(ar2[j-1]);
+            j--;
+        }
+
+        return sb.reverse().toString();
+    }
+
+    private int[][] lcs(char[] ar1, char[] ar2, int n, int m){
         int[][] dp = new int[n+1][m+1];
 
-        for(int i=0;i<=n;i++) dp[i][0] = 0;
-        for(int i=0;i<=m;i++) dp[0][i] = 0;
-
-        for(int i = 1; i <=n;i++){
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                if(ar1[i-1]==ar2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
                 }else{
                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-
-        StringBuilder sb = new StringBuilder();
-        int i = n,j = m;
-
-        while(i>0 && j>0){
-            if(s1.charAt(i-1) == s2.charAt(j-1)){
-                sb.append(s1.charAt(i-1));
-                i--;
-                j--;
-            }else if(dp[i-1][j] > dp[i][j-1]){
-                sb.append(s1.charAt(i-1));
-                i--;
-            }else{
-                sb.append(s2.charAt(j-1));
-                j--;
-            }
-        }
-
-        while(i>0){
-            sb.append(s1.charAt(i-1));
-            i--;
-        }
-
-        while(j>0){
-            sb.append(s2.charAt(j-1));
-            j--;
-        }
-
-        String ans = sb.reverse().toString();
-        return ans;
+        return dp;
     }
-
 }
