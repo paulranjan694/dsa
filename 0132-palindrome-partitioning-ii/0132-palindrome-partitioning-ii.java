7897991f858db85494ff7d056281e9b47705c681
Ranjan Paul <paulranjan694@gmail.com>
2025-04-17 01:44:01 +0530

    Time: 615 ms (53.3%), Space: 81.7 MB (5.03%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -3,21 +3,30 @@ class Solution {
         int n = s.length();
         char[] arr = s.toCharArray();
         int[][] dp = new int[n+1][n+1];
+        Boolean[][] palin = new Boolean[n+1][n+1];
         for(int[] a : dp){
             Arrays.fill(a,-1);
         }
-        return solve(arr,0,n-1,dp);
+        return solve(arr,0,n-1,dp,palin);
     }
 
-    private int solve(char[] arr, int i, int j, int[][] dp){
-        if(i>j || isPalindrom(arr,i,j)) return 0;
-
+    private static int solve(char[] arr, int i, int j, int[][] dp,Boolean[][] palin){
+        if(i>j) return 0;
+        
         if(dp[i][j] != -1) return dp[i][j];
+
+        if(isPalindrom(arr,i,j,palin)) return dp[i][j] = 0;
 
         int min = Integer.MAX_VALUE;
         for(int k=i;k<=j-1;k++){
-            if(isPalindrom(arr, i, k)){
-                min = Math.min(min,1+solve(arr,k+1,j,dp));
+            if(isPalindrom(arr, i, k,palin)){
+                int right = 0;
+                if(dp[k+1][j] != -1){
+                    right = dp[k+1][j];
+                }else{
+                    right = dp[k+1][j] = solve(arr,k+1,j,dp,palin);
+                }
+                min = Math.min(min,1+right);
             }
             // int left=0;
             // if(dp[i][k] != -1){
@@ -39,11 +48,13 @@ class Solution {
         return dp[i][j] = min;
     }
 
-    private boolean isPalindrom(char[] arr, int i,int j){
+    private static boolean isPalindrom(char[] arr, int i,int j, Boolean[][] palin){
+        if(palin[i][j] != null) return palin[i][j];
+        int a=i,b=j;
         while(i<=j && arr[i]==arr[j]){
             i++;
             j--;
         }
-        return i>=j;
+        return palin[a][b]=(i>=j);
     }
 }
