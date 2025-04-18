fc1ae921cbc4f143baf109acc7bf8865ef3a04f5
Ranjan Paul <paulranjan694@gmail.com>
2025-04-19 02:15:58 +0530

    Time: 3 ms (96.59%), Space: 44.6 MB (19.3%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -15,45 +15,23 @@
  */
 class Solution {
     public int pathSum(TreeNode root, int targetSum) {
-        int[] ans = new int[1];
-        Queue<TreeNode> queue = new LinkedList<>();
-        if(root == null) return 0;
-        queue.add(root);
-        while(!queue.isEmpty()){
-            int len = queue.size();
+        Map<Long,Integer> map = new HashMap<>();
+        map.put(0L,1);
+        return dfs(root,map,0L,targetSum);
 
-            while(len-- > 0){
-                TreeNode node = queue.poll();
-                dfs(node,targetSum,ans,0);
-
-                if(node.left != null){
-                    queue.add(node.left);
-                }
-
-                if(node.right != null){
-                    queue.add(node.right);
-                }
-            }
-        }
-        return ans[0];
+        
     }
 
-    private void dfs(TreeNode root, int targetSum, int[] ans, long sum){
-        if(root == null) return;
+    private int dfs(TreeNode root,  Map<Long,Integer> map, long sum, int targetSum){
+        if(root == null) return 0;
         sum += root.val;
+        int pathsEndingHere = map.getOrDefault(sum-targetSum, 0);
+        map.put(sum, map.getOrDefault(sum, 0) + 1);
+        int left = dfs(root.left,map,sum, targetSum);
+        int right = dfs(root.right,map,sum,targetSum);
+        int totals = pathsEndingHere + left + right;
+        map.put(sum, map.getOrDefault(sum, 0) - 1);
+        return totals;
 
-        if(sum==targetSum){
-            ans[0]++;
-        }
-
-        // if(sum < 0){
-        //    return;
-        // }
-
-        dfs(root.left,targetSum,ans,sum);
-        //dfs(root.left,targetSum,ans,targetSum);
-
-        dfs(root.right,targetSum,ans,sum);
-        //dfs(root.right,targetSum,ans,targetSum);
     }
 }
