6ab2d4b9363171854758406670b5ad2c71e8043c
Ranjan Paul <paulranjan694@gmail.com>
2025-04-20 02:14:57 +0530

    Time: 7 ms (84.03%), Space: 45.3 MB (7.05%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.java
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.java
@@ -5,14 +5,18 @@ constraints - s contains all characters, lower, uper,digit,symbol
 
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        int[] hash = new int[128];
+        //int[] hash = new int[128];
+        Map<Character,Integer> hash = new HashMap<>();
         int n = s.length(),l=0,r=0,max=0;
         char[] arr = s.toCharArray();
 
         while(r<n){
-            hash[arr[r]]++;
-            while(containsMoreThanKDistinctChar(hash,k)){
-                hash[arr[l]]--;
+            hash.put(arr[r],hash.getOrDefault(arr[r],0)+1);
+            while(hash.size() > k){
+                hash.put(arr[l],hash.get(arr[l])-1);
+                if(hash.get(arr[l]) == 0){
+                    hash.remove(arr[l]);
+                }
                 l++;
             }
             max = Math.max(max,r-l+1);
@@ -20,17 +24,17 @@ class Solution {
         }
         return max;
     }
-    private boolean containsMoreThanKDistinctChar(int[] hash, int k){
-        for(int i=0;i<128;i++){
-            if(hash[i] > 0){
-                k--;
-                if(k<0){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+    // private boolean containsMoreThanKDistinctChar(int[] hash, int k){
+    //     for(int i=0;i<128;i++){
+    //         if(hash[i] > 0){
+    //             k--;
+    //             if(k<0){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
 
 
 }
