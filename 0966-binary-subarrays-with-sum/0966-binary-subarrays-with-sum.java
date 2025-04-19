f67ef0c3dd89aed73d0ab9dfc4d35356f8c23585
Ranjan Paul <paulranjan694@gmail.com>
2025-04-19 22:04:14 +0530

    Time: 22 ms (32.87%), Space: 49.9 MB (19.07%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,42 +1,13 @@
 class Solution {
-
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int n = nums.length, l = 0, r=0, sum=0,count=0;
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        while(r<n){
-            sum += nums[r];
-            if(sum == goal){
-               count++;
-            }
+        int count = 0,sum=0;
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
 
-            if(hm.containsKey(sum-goal)){
-                count += hm.get(sum-goal);
-            }
-            hm.put(sum, hm.getOrDefault(sum,0)+1);
-            
-            r++;
-        }
-        return count;
-    }
-
-    public int numSubarraysWithSum2(int[] nums, int goal) {
-        return (numberSubArr(nums,goal) - numberSubArr(nums, goal-1));
-    }
-
-    public int numberSubArr(int[] nums, int goal){
-        if(goal < 0) return 0;
-        int n = nums.length, l = 0, r=0, sum=0,count=0;
-
-        while(r<n){
-            sum += nums[r];
-            while(sum > goal){
-                sum -= nums[l];
-                l++;
-            }
-            if(sum <= goal){
-                count += (r-l+1);
-            }
-            r++;
+        for(int num:nums){
+            sum+=num;
+            count+=map.getOrDefault(sum-goal, 0);
+            map.put(sum,map.getOrDefault(sum, 0)+1);
         }
         return count;
     }
