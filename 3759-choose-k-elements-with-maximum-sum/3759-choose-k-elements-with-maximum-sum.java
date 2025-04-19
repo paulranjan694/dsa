ae1e3ed67cd1f856ff3a5e486fb7ac5332e964c8
Ranjan Paul <paulranjan694@gmail.com>
2025-04-19 19:36:59 +0530

    Time: 145 ms (83.02%), Space: 72.8 MB (11.44%) - LeetHub

---
+++ 3759-choose-k-elements-with-maximum-sum/3759-choose-k-elements-with-maximum-sum.java
@@ -0,0 +1,54 @@
+class Pair implements Comparable<Pair>{
+    int a,b,idx;
+    Pair(int a, int b, int idx){
+        this.a=a;
+        this.b=b;
+        this.idx=idx;
+    }
+
+    @Override
+    public int compareTo(Pair p){
+        if(this.a == p.a){
+            return this.idx-p.idx;
+        }
+        return this.a - p.a;
+    }
+
+
+}
+class Solution {
+    public long[] findMaxSum(int[] nums1, int[] nums2, int k) {
+        List<Pair> list = new ArrayList<>();
+        int n = nums1.length;
+
+        for(int i=0;i<n;i++){
+            list.add(new Pair(nums1[i],nums2[i],i));
+        }
+
+        Collections.sort(list);
+
+        long[] res = new long[n];
+        Queue<Integer> pq = new PriorityQueue<>();
+        long sum=0;
+        for(int i=0;i<n;i++){
+            Pair p = list.get(i);
+            if(i==0){
+                pq.add(list.get(i).b);
+                res[p.idx]=0;
+                sum+=list.get(i).b;
+            }else{   
+                    res[p.idx] = sum;
+                    pq.add(list.get(i).b);
+                    sum += list.get(i).b;
+                if(list.get(i-1).a == p.a){
+                    res[p.idx] = res[list.get(i-1).idx];
+                }
+                while(pq.size() > k){
+                    sum-=pq.poll();
+                }    
+            }
+        }
+
+        return res;
+    }
+}
