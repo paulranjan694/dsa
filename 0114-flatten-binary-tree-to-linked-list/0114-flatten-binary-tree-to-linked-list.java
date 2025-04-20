376e466c40f82b0231431ef87032fb2072dd32f2
Ranjan Paul <paulranjan694@gmail.com>
2025-04-20 23:53:44 +0530

    Time: 0 ms (100%), Space: 41.9 MB (91.49%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,15 +14,32 @@
  * }
  */
 class Solution {
-    TreeNode prev = null;
-    public void flatten(TreeNode root) {
-        if(root==null) return;
 
-        flatten(root.right);
-        flatten(root.left);
-
-        root.right = prev;
-        root.left = null;
-        prev=root;
+    public void flatten(TreeNode root) { //moris traversal -> tc - O(n) sc - O(1)
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode rightmost = curr.left;
+                while(rightmost.right != null){
+                    rightmost=rightmost.right;
+                }
+                rightmost.right = curr.right;
+                curr.right = curr.left;
+                curr.left=null;
+            }
+            curr=curr.right;
+        }
     }
+    
+    // TreeNode prev = null;
+    // public void flatten_reverse_postorder(TreeNode root) {//reverse post-order -> tc - O(n) sc - O(n) => stack space
+    //     if(root==null) return;
+
+    //     flatten(root.right);
+    //     flatten(root.left);
+
+    //     root.right = prev;
+    //     root.left = null;
+    //     prev=root;
+    // }
 }
