99242d298df02d1a211932ad07824b8d04544ede
Ranjan Paul <paulranjan694@gmail.com>
2025-05-10 17:55:03 +0530

    Time: 3 ms (88.73%), Space: 61.7 MB (6.57%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
@@ -5,6 +5,7 @@ class Solution {
         for(int i=0;i<nums1.length;i++){
             s1+=nums1[i];
             if(nums1[i]==0){
+                s1++;
                 z1++;
             }
         }
@@ -12,42 +13,14 @@ class Solution {
         for(int i=0;i<nums2.length;i++){
             s2+=nums2[i];
             if(nums2[i]==0){
+                s2++;
                 z2++;
             }
         }
 
-
-        // if((z1==0 && z2!=0) || (z1!=0 && z2==0)){
-        //     return -1;
-        // }
-        long ans=0;
-        long ts1=s1+z1,ts2=s2+z2;
-        if(s1==s2){
-            if((z1==0 && z2!=0) || (z1!=0 && z2==0)){
-                return -1;
-            }else{
-                s1 += Math.max(z1,z2);
-                ans=s1;
-            }
-        }else if(ts1>ts2){
-            s1 += z1;
-            if(z2==0){
-                ans=-1;
-            }else if(z2!=0 && (s1-s2)/z2 == 0){
-                ans=-1;
-            }else{
-                ans=s1;
-            }
-        }else{
-            s2 += z2;
-            if(z1==0){
-                ans=-1;
-            }else if(z1!=0 && (s2-s1)/z1 == 0){
-                ans=-1;
-            }else{
-                ans=s2;
-            }
+        if((z1 == 0 && s2>s1) || (z2==0 && s1>s2)){
+            return -1;
         }
-        return ans;
+        return Math.max(s1,s2);
     }
 }
