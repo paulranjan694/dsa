7c78976c056cf00a4ddcd25d21bfe2b3f72fa7fe
Ranjan Paul <paulranjan694@gmail.com>
2025-06-07 20:47:46 +0530

    Time: 0 ms (100%), Space: 41.3 MB (35.78%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,39 +1,27 @@
 class Solution {
-
     public int rob(int[] nums) {
         int n = nums.length;
-        //int[] dp = new int[n];
-        //Arrays.fill(dp,-1);
-        //dp[0] = nums[0];
-        int prev1=nums[0],prev2=0,curr=0;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
         for(int i=1;i<n;i++){
-            int take = nums[i];
-            if(i>1) take += prev2;
-            int notTake =prev1;
-            curr = Math.max(take,notTake);
-            prev2=prev1;
-            prev1=curr;
+            int take=nums[i];
+            if(i>1) take += dp[i-2];
+            int nottake = dp[i-1];
+            dp[i] = Math.max(take, nottake);
         }
-        return prev1;//n-1
-
+        return dp[n-1];
     }
 
-    // public int rob(int[] nums) {
-    //     int n = nums.length;
-    //     int[] dp = new int[n+1];
-    //     Arrays.fill(dp,-1);
-    //    return utils(nums,n-1, dp);
-    // }
+    public int rob2(int[] nums) {
+        int n = nums.length;
+        return utils(nums,n-1);
+    }
 
-    // private int utils(int[] nums, int idx, int[] dp){
-    //     if(idx == 0) return nums[idx];
-    //     if(idx < 0) return 0;
-    //     if(dp[idx] != -1)
-    //         return dp[idx];
-
-    //     int take = nums[idx] + utils(nums, idx-2,dp);
-    //     int notTake = 0 + utils(nums, idx-1,dp);
-    //     dp[idx] = Math.max(take, notTake);
-    //     return dp[idx];
-    // }
+    private int utils(int[] nums, int idx){
+        if(idx<0) return 0;
+        if(idx==0) return nums[idx];
+        int take = nums[idx] + utils(nums,idx-2);
+        int nottake = utils(nums,idx-1);
+        return Math.max(take,nottake);
+    }
 }
