009e1b5e0e4dd32b3d2d78d56a7a006803fa8af5
Ranjan Paul <paulranjan694@gmail.com>
2025-06-07 21:19:39 +0530

    Time: 0 ms (100%), Space: 41.3 MB (29.15%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -2,30 +2,30 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n==1) return nums[0];
-        
         int[] temp1 = new int[n];
         int[] temp2 = new int[n];
-        for(int i = 0; i < n; i++){
-            if(i!=0) temp1[i] = nums[i];
-            if(i!=n-1) temp2[i] = nums[i];
+        for(int i=0;i<n;i++){
+            if(i!=n-1){
+                temp1[i]=nums[i];
+            }
+
+            if(i!=0){
+                temp2[i]=nums[i];
+            }
         }
-        return Math.max(robUtils(temp1, 1),robUtils(temp2,0));
+        return Math.max(helper(temp1), helper(temp2));
     }
 
-    private int robUtils(int[] arr, int si){
-        int n = arr.length;
-        //int[] dp = new int[n];
-        //Arrays.fill(dp,-1);
-        //dp[0] = nums[0];
-        int prev1=arr[si],prev2=0,curr=0;
-        for(int i=si+1;i<n;i++){
-            int take = arr[i];
-            if(i>1) take += prev2;
-            int notTake =prev1;
-            curr = Math.max(take,notTake);
-            prev2=prev1;
-            prev1=curr;
+    private int helper(int[] nums){
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1) take += dp[i-2];
+            int nottake = dp[i-1];
+            dp[i] = Math.max(take, nottake);
         }
-        return prev1;//n-1
+        return dp[n-1];
     }
 }
