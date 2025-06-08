0229bcfcc857d0766f2fe46ab8cc46d538b1c7f6
Ranjan Paul <paulranjan694@gmail.com>
2025-06-08 19:40:24 +0530

    Time: 182 ms (26.25%), Space: 55.9 MB (5.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -3,9 +3,15 @@ class Solution {
         char[] arr = s.toCharArray();
         int start=0,end=0,n=arr.length;
         if(n==1) return s;
+        int[][] dp = new int[n+1][n+1];
+
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                if(isPalindrome(arr,i,j)){
+                if(isPalindrome(arr,i,j,dp) == 1){
                     if(end-start < j-i){
                         start = i;
                         end = j;
@@ -17,11 +23,12 @@ class Solution {
         return s.substring(start,end+1);
     }
 
-    private boolean isPalindrome(char[] arr, int s, int e){
-        if(s>=e) return true;
+    private int isPalindrome(char[] arr, int s, int e, int[][] dp){
+        if(s>=e) return 1;
+        if(dp[s][e] != -1) return dp[s][e];
         if(arr[s]==arr[e]){
-            return isPalindrome(arr, s+1, e-1);
+            return dp[s][e] = isPalindrome(arr, s+1, e-1,dp);
         }
-        else return false;
+        else return dp[s][e] = 0;
     }
 }
