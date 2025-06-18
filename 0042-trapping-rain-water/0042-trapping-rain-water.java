6c0b01cd6de6db1e927fd4024fbe628fe826ed14
Ranjan Paul <paulranjan694@gmail.com>
2025-06-19 02:02:12 +0530

    Time: 1 ms (62.45%), Space: 46 MB (88.52%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,50 +1,25 @@
 class Solution {
-    //optimise - TC - O(n) SC - O(1)
     public int trap(int[] height) {
         int n = height.length;
-        int lmax=0,rmax=0, l=0,r=n-1, total = 0;
-        while(l < r){
-            if(height[l] <= height[r]){
-                int curr = lmax - height[l];
-                if(curr > 0) {
-                    total += curr;
-                }else{
-                    lmax = height[l];
-                }
-                l++;
-            }else{
-                int curr = rmax - height[r];
-                if(curr > 0) {
-                    total += curr;
-                }else{
-                    rmax = height[r];
-                }
-                r--;
-            }
-        }
-        return total;
-    }
-
-    //TC - O(n). SC - O(n) => prefix and suffix arrays take n space
-    public int trap2(int[] height) {
-        int n = height.length, total = 0;
-        int[] prefixmax = new int[n];
-        int[] suffixmax = new int[n];
-        prefixmax[0] = height[0];
-        suffixmax[n-1] = height[n-1];
-
-        for(int i = 1; i < n; i++){
-            prefixmax[i] = Math.max(prefixmax[i-1],height[i]);
+        int[] rightNearestMax = new int[n]; 
+        int[] leftNearestMax = new int[n];
+        int maxSoFar = -1;
+        //leftNearestMax[0] = 0;
+        for(int i=1;i<n;i++){
+            maxSoFar = Math.max(height[i-1],maxSoFar);
+            leftNearestMax[i] = maxSoFar;
         }
 
-        for(int i = n-2; i >= 0; i--){
-            suffixmax[i] = Math.max(suffixmax[i+1],height[i]);
+        maxSoFar = -1;
+        for(int i=n-2;i>=0;i--){
+             maxSoFar = Math.max(height[i+1],maxSoFar);
+            rightNearestMax[i] = maxSoFar;
         }
 
-        for(int i=0;i<n;i++){
-            total += Math.min(prefixmax[i],suffixmax[i]) - height[i];
+        int maxWater=0;
+        for(int i=1;i<n-1;i++){
+            maxWater += Math.max(Math.min(leftNearestMax[i],rightNearestMax[i])-height[i],0);
         }
-
-        return total;
+        return maxWater;
     }
 }
