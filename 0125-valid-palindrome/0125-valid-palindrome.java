8c19643c5543aaa95da1546ae4ff3095752f4aa2
Ranjan Paul <paulranjan694@gmail.com>
2025-06-19 22:35:52 +0530

    Time: 5 ms (47.15%), Space: 45 MB (28.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,27 +1,56 @@
 class Solution {
+    private Set<Character> numbers = new HashSet<>();
+    private Set<Character> alphabets = new HashSet<>();
     public boolean isPalindrome(String s) {
-        int i=0,j=s.length()-1;
-        while(i<=j){
-            if(!isAlphaNumeric(s.charAt(i))){
-                i++;
-                continue;
-            }
-            if(!isAlphaNumeric(s.charAt(j))){
-                j--;
-                continue;
-            }
-            char lch=Character.toLowerCase(s.charAt(i)), rch=Character.toLowerCase(s.charAt(j));
 
-            if(lch==rch){
-                i++;j--;
-            }else{
+        for(char i = '0';i<='9';i++){
+            numbers.add(i);
+        }
+
+        for(char i = 'a';i<='z';i++){
+            alphabets.add(i);
+        }
+
+        for(char i = 'A';i<='Z';i++){
+            alphabets.add(i);
+        }
+
+        // System.out.println(numbers);
+        // System.out.println(alphabets);
+
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int left = 0,right=n-1;
+
+        while(left<=right){
+            while(left < n && isValid(arr[left])){
+                left++; 
+            }
+
+            while(right>=0 && isValid(arr[right])){
+                right--;
+            }
+
+            if(left < n && arr[left] >= 'A' && arr[left] <= 'Z'){
+                arr[left] += 32;
+            }
+
+            if(right >= 0 && arr[right] >= 'A' && arr[right] <= 'Z'){
+                arr[right] += 32;
+            }
+
+
+            if(left < n && right >= 0 && arr[left]!=arr[right]){
                 return false;
             }
+            left++;
+            right--;
         }
+
         return true;
     }
 
-    public boolean isAlphaNumeric(char ch){
-        return ('A' <= ch && ch <= 'Z') || ('a' <= ch && ch <= 'z') || ('0' <= ch && ch <= '9');
+    private boolean isValid(char c){
+        return !(alphabets.contains(c) || numbers.contains(c));
     }
 }
