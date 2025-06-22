29ab9eeffd7145703a0915bfde9763e3ac4024a4
Ranjan Paul <paulranjan694@gmail.com>
2025-06-22 22:16:20 +0530

    Time: 10 ms (12.2%), Space: 44.7 MB (60.48%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,20 +9,47 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q) {
-            return root;
+        List<TreeNode> path1 = new ArrayList<>();
+        List<TreeNode> path2 = new ArrayList<>();
+
+        findNode(root,p,path1);
+        findNode(root,q,path2);
+
+        TreeNode ans=null;
+
+        int i = 0;
+    //    System.out.println(path1.toString());
+    //    System.out.println(path2.toString());
+        while(i<path1.size() && i < path2.size()){
+            //System.out.println(path1.get(i).val+" --- "+path2.get(i).val);
+            if(path1.get(i).val != path2.get(i).val){
+                //System.out.println("inside non match");
+                break;
+            }
+            ans=path1.get(i);
+            i++;
+        }
+        return ans;
+    }
+    private boolean findNode(TreeNode root, TreeNode toFind, List<TreeNode> path){
+        if(root == null){
+            return false;
         }
 
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-
-        if(left == null){
-            return right;
-        }else if(right == null){
-            return left;
-        }else{
-            return root;
+        path.add(root);
+        if(root.val==toFind.val){
+            return true;
         }
 
+        boolean findinLeftSubTree = findNode(root.left, toFind, path);
+        if(findinLeftSubTree){
+            return true;
+        }
+        boolean findinRightSubTree = findNode(root.right, toFind, path);
+        if(findinRightSubTree){
+            return true;
+        }
+        path.remove(path.size()-1);
+        return false;
     }
 }
