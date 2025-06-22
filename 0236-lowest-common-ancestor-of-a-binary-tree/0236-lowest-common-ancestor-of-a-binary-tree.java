3843543cd249b4616949c99e4b9a438ed075d361
Ranjan Paul <paulranjan694@gmail.com>
2025-06-22 22:28:44 +0530

    Time: 6 ms (100%), Space: 45.1 MB (20.4%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,47 +9,19 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> path1 = new ArrayList<>();
-        List<TreeNode> path2 = new ArrayList<>();
-
-        findNode(root,p,path1);
-        findNode(root,q,path2);
-
-        TreeNode ans=null;
-
-        int i = 0;
-    //    System.out.println(path1.toString());
-    //    System.out.println(path2.toString());
-        while(i<path1.size() && i < path2.size()){
-            //System.out.println(path1.get(i).val+" --- "+path2.get(i).val);
-            if(path1.get(i).val != path2.get(i).val){
-                //System.out.println("inside non match");
-                break;
-            }
-            ans=path1.get(i);
-            i++;
+        if(root == null) return root;
+        if(root.val == p.val || root.val == q.val){
+            return root;
         }
-        return ans;
-    }
-    private boolean findNode(TreeNode root, TreeNode toFind, List<TreeNode> path){
-        if(root == null){
-            return false;
-        }
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
 
-        path.add(root);
-        if(root.val==toFind.val){
-            return true;
+        if(left != null && right != null){
+            return root;
+        }else if(left != null && right == null){
+            return left;
+        }else{
+            return right;
         }
-
-        boolean findinLeftSubTree = findNode(root.left, toFind, path);
-        if(findinLeftSubTree){
-            return true;
-        }
-        boolean findinRightSubTree = findNode(root.right, toFind, path);
-        if(findinRightSubTree){
-            return true;
-        }
-        path.remove(path.size()-1);
-        return false;
     }
 }
