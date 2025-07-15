d6086a871b9c7e616936e143e0c67f25ea34e395
Ranjan Paul <paulranjan694@gmail.com>
2025-07-15 21:48:50 +0530

    Time: 1 ms (99.82%), Space: 45.7 MB (56.05%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,5 +1,18 @@
 class Solution {
+
     public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int goal=n-1;
+        for(int i = n-2;i>=0;i--){
+            if(goal <= i+nums[i]){
+               goal = i;
+            }
+            
+        }
+        return goal==0;
+    }
+
+    public boolean canJump_dp(int[] nums) {
         int n = nums.length;
         boolean[] dp = new boolean[n];
         dp[n-1] = true;
@@ -14,19 +27,19 @@ class Solution {
         return dp[0];
     }
 
-    private boolean dfs(int[] nums, int idx,int[] dp){
-        if(idx==nums.length-1) return true;
-        if(dp[idx] != -1) return dp[idx] == 1;
-        int end = Math.min(nums.length-1,idx+nums[idx]);
-        for(int i = idx+1;i<=end;i++){
-            if(dp[i] != -1) return dp[idx] == 1;
-            boolean found = dfs(nums,i,dp);
-            if(found==true) dp[i] = 1;
-            else dp[i]=0;
-            if(found==true){
-                return true;
-            }
-        }
-        return false;
-    }
+    // private boolean dfs(int[] nums, int idx,int[] dp){
+    //     if(idx==nums.length-1) return true;
+    //     if(dp[idx] != -1) return dp[idx] == 1;
+    //     int end = Math.min(nums.length-1,idx+nums[idx]);
+    //     for(int i = idx+1;i<=end;i++){
+    //         if(dp[i] != -1) return dp[idx] == 1;
+    //         boolean found = dfs(nums,i,dp);
+    //         if(found==true) dp[i] = 1;
+    //         else dp[i]=0;
+    //         if(found==true){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 }
