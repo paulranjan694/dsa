68ec7ff33f92345ee9de6680c25979cda3eed39b
Ranjan Paul <paulranjan694@gmail.com>
2025-07-20 20:25:09 +0530

    Time: 20 ms (6.15%), Space: 49 MB (85.77%) - LeetHub

---
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.java
@@ -0,0 +1,59 @@
+class Solution {
+    private static final int MOD = 1000000007;
+    public int countPaths(int n, int[][] roads) {
+        int[] count = new int[n];
+        long[] times = new long[n];
+
+        List<List<int[]>> adj = constructAdj(n,roads);
+
+        Arrays.fill(times,Long.MAX_VALUE);
+
+        times[0]=0;
+        count[0]=1;
+
+        PriorityQueue<long[]> minheap = new PriorityQueue<>((long[] a, long[] b) -> Long.compare(a[1],b[1])); // 0 -> node,1 -> time
+
+        minheap.add(new long[]{0,0});
+
+        while(!minheap.isEmpty()){
+            long[] pair = minheap.poll();
+            int node = (int)pair[0];
+            long time = pair[1];
+      
+            if (time > times[node]) continue;
+
+            for(int[] ngrs : adj.get(node)){
+                int v = ngrs[0];
+                int t = ngrs[1];
+
+                long newTime = t + time;
+               
+                if(times[v] > newTime){
+                    times[v] = newTime;
+                    minheap.add(new long[]{v,newTime});
+                    count[v]=count[node];
+                }else if(times[v] == newTime){
+                    count[v]=(count[v]+count[node]) % MOD;
+                }
+            }
+        }
+        return count[n-1] % MOD;
+    }
+
+    private List<List<int[]>> constructAdj(int n, int[][] roads){
+        List<List<int[]>> adj = new ArrayList<>();
+        for(int i=0; i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] road : roads){
+            int u = road[0];
+            int v = road[1];
+            int time = road[2];
+
+            adj.get(u).add(new int[]{v,time});
+            adj.get(v).add(new int[]{u,time});
+        }
+        return adj;
+    }
+}
