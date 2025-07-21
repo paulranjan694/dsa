e68f624b877c4b2b0fea5a0a537663a81bb8cd1f
Ranjan Paul <paulranjan694@gmail.com>
2025-07-21 10:34:19 +0530

    Time: 380 ms (11.75%), Space: 81.7 MB (5.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,52 +1,28 @@
 class Solution {
-
-    //using binary search, just to find the length of LIS, we are not storing the LIS.
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(nums[0]);
-
-        for(int i = 1; i < n; i++){
-            if(nums[i]>list.get(list.size()-1)){
-                list.add(nums[i]);
-            }else{
-                int idx = lowerBound(nums[i],list,0, list.size()-1);
-                list.set(idx,nums[i]);
-            }
-        }
-        return list.size();
-    }
-
-    private int lowerBound(int ele, ArrayList<Integer> list, int s, int e){
-        while(s<e){
-            int mid = (s+e) / 2;
-            if(list.get(mid)== ele){
-                return mid;
-            }
-            if(list.get(mid)> ele){
-                e = mid;
-            }else{
-                s = mid+1;
-            }
-        }
-        return s;
-    }
-
-    public int lengthOfLIS2(int[] nums) {
-        int n = nums.length;
         int[][] dp = new int[n][n+1];
-        for(int[] row : dp) Arrays.fill(row,-1);
-        return utils(nums, 0,-1,dp);
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+        return lis(nums,0,-1,dp);
     }
 
-    private int utils(int[] nums, int idx, int prevIdx, int[][] dp){
-        //base case
-        if(idx >= nums.length) return 0;
-        if(dp[idx][prevIdx+1] != -1) return dp[idx][prevIdx+1];
+    private int lis(int[] nums, int idx, int prev, int[][] dp){
+        //base condition
+        if(idx==nums.length){
+            return 0;
+        }
+        if(dp[idx][prev+1]!=-1){
+            return dp[idx][prev+1];
+        }
+
         int take = 0;
-        if(prevIdx == -1 || nums[prevIdx] < nums[idx])
-            take = 1 + utils(nums, idx+1, idx,dp);
-        int notTake = utils(nums,idx+1,prevIdx,dp);
-        return dp[idx][prevIdx+1] =  Math.max(take,notTake);
+        if(prev == -1 || nums[prev] < nums[idx]){
+            take = 1 + lis(nums, idx+1, idx, dp);
+        }
+        int notTake = lis(nums, idx+1, prev, dp);
+
+        return dp[idx][prev+1]=Math.max(take,notTake);
     }
-}         
+}
