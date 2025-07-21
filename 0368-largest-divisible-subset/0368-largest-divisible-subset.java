75a706143dfc6f16fdd567b5085eb70412bc05fe
Ranjan Paul <paulranjan694@gmail.com>
2025-07-21 11:10:43 +0530

    Time: 92 ms (5.01%), Space: 55.4 MB (5.01%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,38 +1,39 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        Arrays.sort(nums);
-
         int n = nums.length;
-        int[] dp = new int[n];
-        int[] hash = new int[n];
-
-        Arrays.fill(dp,1);
-
-        for(int i=0;i<n;i++){
-            hash[i] = i;
-        }
-        int maxIdx = 0;
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1){
-                    dp[i] = dp[j] + 1;
-                    hash[i] = j;
-                }
-            }
-
-            if(dp[i] > dp[maxIdx]){
-                maxIdx = i;
-            }
-        }
-
-        List<Integer> ans = new ArrayList<Integer>();
-        ans.add(nums[maxIdx]);
-        while(maxIdx != hash[maxIdx]){
-            maxIdx = hash[maxIdx];
-            ans.add(nums[maxIdx]);
-        }
-
-        Collections.reverse(ans);
-        return ans;
+        
+        Arrays.sort(nums);
+        List<Integer>[][] dp = new ArrayList[n][n+1];
+        
+        return lis(nums,0,-1,dp);
     }
+
+    private List<Integer> lis(int[] nums, int idx, int prev, List<Integer>[][] dp ){
+        if(idx==nums.length) {
+            return new ArrayList<>();
+        }
+        
+        if (dp[idx][prev + 1] != null) {
+            return dp[idx][prev + 1];
+        }
+
+        List<Integer> take = new ArrayList<>();
+        if(prev == -1 || nums[idx] % nums[prev]==0){
+           take = new ArrayList<>();
+            take.add(nums[idx]);
+            take.addAll(lis(nums, idx + 1, idx, dp));
+        }
+
+
+        List<Integer> skip = lis(nums, idx + 1, prev, dp);
+
+        // Take the longer one
+        if (take.size() > skip.size()) {
+            dp[idx][prev + 1] = take;
+        } else {
+            dp[idx][prev + 1] = skip;
+        }
+
+        return dp[idx][prev + 1];
+    } 
 }
