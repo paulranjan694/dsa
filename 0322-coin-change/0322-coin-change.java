ae012584d35378c2b328288a2b23c3b1a96e8867
Ranjan Paul <paulranjan694@gmail.com>
2025-10-02 13:05:45 +0530

    Time: 18 ms (41.65%), Space: 44.8 MB (47.98%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,32 +1,33 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[amount+1][coins.length+1];
-        for(int[] d : dp)
-            Arrays.fill(d, -1);
-        int min = utils(coins, amount, 0, dp);
-        return min == Integer.MAX_VALUE ? -1 : min;
-    }
+        int n = coins.length;
+        int[][] dp = new int[n+1][amount+1];
 
-    private int utils(int[] coins, int amount, int index, int[][] dp){
-        //base condition
+        //base cond - amount > 0 and no coins left
+        for(int a=1;a<=amount;a++){
+            dp[n][a] = Integer.MAX_VALUE;
+        }
 
-        if(index >= coins.length){
-            if(amount > 0){
-                return Integer.MAX_VALUE;
+        //base cond - amount = 0
+        for(int i = 0;i<n+1;i++){
+            dp[i][0] = 0;
+        }
+
+        for(int i = n-1;i>=0;i--){
+            for(int a = 1;a<=amount;a++){
+                int taken = Integer.MAX_VALUE;
+                if(a >= coins[i]){
+                    taken = dp[i][a-coins[i]];
+                    if(taken != Integer.MAX_VALUE){
+                        taken += 1;
+                    }
+                }
+                
+                int nottaken = dp[i+1][a];
+                dp[i][a] = Math.min(taken, nottaken);
             }
-            return 0;
         }
 
-        if(dp[amount][index] != -1) return dp[amount][index];
-
-        int taken = Integer.MAX_VALUE;
-        if(amount >= coins[index]){
-            taken = utils(coins, amount-coins[index], index, dp);
-            if(taken != Integer.MAX_VALUE) 
-                taken += 1;
-        }
-
-        int notTaken = utils(coins, amount, index+1,dp);
-        return dp[amount][index] = Math.min(taken, notTaken);
+        return dp[0][amount] == Integer.MAX_VALUE ? -1 : dp[0][amount];
     }
 }
