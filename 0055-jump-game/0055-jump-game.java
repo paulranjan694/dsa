eb682d052e4df5106755c906eaf2210439cbd9f3
Ranjan Paul <paulranjan694@gmail.com>
2025-10-04 20:33:12 +0530

    Time: 3 ms (39.51%), Space: 45.7 MB (72%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,39 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        //return utils(nums, 0);
-
-        int n = nums.length;
-        boolean[] dp = new boolean[n];
-        dp[n-1] = true;
-
-        for(int i=n-2;i>=0;i--){
-            if(nums[i] == 0) continue;
-            int limit = Math.min(n-1, nums[i]);
-            for(int j=1;j<=limit;j++){
-                dp[i] = dp[i] || dp[i+j];
-            }
+        int farthest = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > farthest) return false;   // stuck before reaching i
+            farthest = Math.max(farthest, i + nums[i]);
         }
-        return dp[0];
-    }
-
-    private boolean utils(int[] nums, int idx){
-        if(idx == nums.length-1){
-            return true;
-        }
-        if(idx >= nums.length){
-            return false;
-        }
-        boolean result = false;
-        for(int i=1;i<=nums[idx];i++){
-            result = result || utils(nums, idx+i);
-            if(result == true){
-                return true;
-            }
-        }
-        return result;
+        return true;
     }
 }
-
-/**
-
- */
