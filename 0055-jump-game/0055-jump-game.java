0784a1ab32d88e34e3de594c9b237635bdaee835
Ranjan Paul <paulranjan694@gmail.com>
2025-10-04 19:46:03 +0530

    Time: 759 ms (5.02%), Space: 45.6 MB (72%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,45 +1,39 @@
 class Solution {
-
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int goal=n-1;
-        for(int i = n-2;i>=0;i--){
-            if(goal <= i+nums[i]){
-               goal = i;
-            }
-            
-        }
-        return goal==0;
-    }
+        //return utils(nums, 0);
 
-    public boolean canJump_dp(int[] nums) {
         int n = nums.length;
         boolean[] dp = new boolean[n];
         dp[n-1] = true;
+
         for(int i=n-2;i>=0;i--){
-            int end = Math.min(n-1,i+nums[i]);
-            for(int j = i+1;j<=end;j++){
-                if(dp[j]){
-                    dp[i] = true;
-                }
+            if(nums[i] == 0) continue;
+            int limit = Math.min(n-1, nums[i]);
+            for(int j=1;j<=limit;j++){
+                dp[i] = dp[i] || dp[i+j];
             }
         }
         return dp[0];
     }
 
-    // private boolean dfs(int[] nums, int idx,int[] dp){
-    //     if(idx==nums.length-1) return true;
-    //     if(dp[idx] != -1) return dp[idx] == 1;
-    //     int end = Math.min(nums.length-1,idx+nums[idx]);
-    //     for(int i = idx+1;i<=end;i++){
-    //         if(dp[i] != -1) return dp[idx] == 1;
-    //         boolean found = dfs(nums,i,dp);
-    //         if(found==true) dp[i] = 1;
-    //         else dp[i]=0;
-    //         if(found==true){
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
+    private boolean utils(int[] nums, int idx){
+        if(idx == nums.length-1){
+            return true;
+        }
+        if(idx >= nums.length){
+            return false;
+        }
+        boolean result = false;
+        for(int i=1;i<=nums[idx];i++){
+            result = result || utils(nums, idx+i);
+            if(result == true){
+                return true;
+            }
+        }
+        return result;
+    }
 }
+
+/**
+
+ */
