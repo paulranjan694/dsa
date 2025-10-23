5626019b7659b6d6a14b871d9c0f6eb133ec5f69
Ranjan Paul <paulranjan694@gmail.com>
2025-10-23 23:56:32 +0530

    Time: 7 ms (41.06%), Space: 42.1 MB (70.53%) - LeetHub

--- 1216-print-zero-even-odd/1216-print-zero-even-odd.java
+++ 1216-print-zero-even-odd/1216-print-zero-even-odd.java
@@ -1,70 +1,74 @@
 class ZeroEvenOdd {
     private int n;
+    private int num=1;
+    ReentrantLock lock = new ReentrantLock();
+    Condition cond1 = lock.newCondition();
+    Condition cond2 = lock.newCondition();
+    Condition cond3 = lock.newCondition();
 
-    private int current=1;
+    private boolean pzero=true;
 
-    private ReentrantLock lock = new ReentrantLock();
-    private Condition oddCondition = lock.newCondition();
-    private Condition evenCondition = lock.newCondition();
-    private Condition zeroCondition = lock.newCondition();
-
-    private boolean zeroTurn=true;
-
-    
     public ZeroEvenOdd(int n) {
         this.n = n;
     }
 
     // printNumber.accept(x) outputs "x", where x is an integer.
     public void zero(IntConsumer printNumber) throws InterruptedException {
-        for(int i=1;i<=n;i++){
-            lock.lock();
-            try{
+       for(int i=0;i<n;i++){
+            try {
+                lock.lock();
+                while(pzero == false){
+                    if(num > n){
+                        cond1.signal();
+                        cond2.signal();
+                        cond3.signal();
+                        return;
+                    }
+                    cond1.await();
+                }
 
-                while(!zeroTurn){
-                    zeroCondition.await();
+                if(num > n){
+                    cond1.signal();
+                    cond2.signal();
+                    cond3.signal();
+                    return;
                 }
+
                 printNumber.accept(0);
-                zeroTurn=false;
-                if(current % 2 == 0){
-                    evenCondition.signal();
-                }else{
-                    oddCondition.signal();
-                }
+                pzero = false;
+                if (num % 2 == 0)
+                    cond2.signal();
+                else
+                    cond3.signal();
             }finally{
                 lock.unlock();
             }
-        }
+       }
     }
 
     public void even(IntConsumer printNumber) throws InterruptedException {
         while(true){
-            lock.lock();
             try{
-                if(current > n) {
-                    zeroCondition.signal();
-                    oddCondition.signal();
-                    evenCondition.signal();
+            lock.lock();
+            while(pzero == true || num % 2 != 0){
+                if(num > n){
+                    cond1.signal();
+                    cond2.signal();
+                    cond3.signal();
                     return;
                 }
-                while(zeroTurn || current % 2 != 0){
-                    if(current > n) {
-                        zeroCondition.signal();
-                        oddCondition.signal();
-                        evenCondition.signal();
-                        return;
-                    }
-                    evenCondition.await();
-                }
-                if(current > n) {
-                    zeroCondition.signal();
-                    oddCondition.signal();
-                    evenCondition.signal();
-                    return;
-                }
-                printNumber.accept(current++);
-                zeroTurn=true;
-                zeroCondition.signal();
+                cond2.await();
+            }
+            if(num > n){
+                cond1.signal();
+                cond2.signal();
+                cond3.signal();
+                return;
+            }
+
+            printNumber.accept(num++);
+            pzero = true;
+            cond1.signal();
             }finally{
                 lock.unlock();
             }
@@ -72,36 +76,32 @@ class ZeroEvenOdd {
     }
 
     public void odd(IntConsumer printNumber) throws InterruptedException {
-        while(true){
-            lock.lock();
+         while(true){
             try{
-                if(current > n) {
-                    zeroCondition.signal();
-                    oddCondition.signal();
-                    evenCondition.signal();
-                    return;
-                }
-                while(zeroTurn || current % 2 == 0){
-                    if(current > n) {
-                        zeroCondition.signal();
-                        oddCondition.signal();
-                        evenCondition.signal();
+                lock.lock();
+                while(pzero == true || num % 2 == 0){
+                    if(num > n){
+                        cond1.signal();
+                        cond2.signal();
+                        cond3.signal();
                         return;
                     }
-                    oddCondition.await();
+                    cond3.await();
                 }
-                printNumber.accept(current++);
-                if(current > n) {
-                    zeroCondition.signal();
-                    oddCondition.signal();
-                    evenCondition.signal();
+
+                if(num > n){
+                    cond1.signal();
+                    cond2.signal();
+                    cond3.signal();
                     return;
                 }
-                zeroTurn=true;
-                zeroCondition.signal();
+
+                printNumber.accept(num++);
+                pzero = true;
+                cond1.signal();
             }finally{
                 lock.unlock();
             }
-        }
+         }
     }
 }
