d3c2932e0ee22c57875c5ce8fb4f59a4330ebf0c
Ranjan Paul <paulranjan694@gmail.com>
2025-10-23 20:57:19 +0530

    Time: 520 ms (92.94%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,9 @@
-# Write your MySQL query statement below
--- delete from Person where (select id from Person group by email having count(*) > 1);
-
-delete p1 from Person p1 join Person p2 on p1.email=p2.email and p1.id > p2.id;  
-
+WITH ranked AS (
+    SELECT Id,
+           ROW_NUMBER() OVER(PARTITION BY email ORDER BY id ASC) AS row_num
+    FROM Person
+)
+DELETE FROM Person
+WHERE Id IN (
+    SELECT Id FROM ranked WHERE row_num > 1
+);
