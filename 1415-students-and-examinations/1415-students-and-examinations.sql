bce73d66493ee0b5e86e2e74e9fbb3b47e02a2bc
Ranjan Paul <paulranjan694@gmail.com>
2025-10-23 12:44:07 +0530

    Time: 1201 ms (12.66%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -3,6 +3,5 @@
 
 select s.student_id, s.student_name, s1.subject_name, COALESCE(j.attended_exams, 0) AS attended_exams from Students s cross join Subjects s1 left join (
     select student_id, subject_name, count(*) as attended_exams from Examinations 
-    group by student_id, subject_name 
-    order by student_id, subject_name)as j 
+    group by student_id, subject_name)as j 
     on s.student_id = j.student_id and s1.subject_name = j.subject_name order by s.student_id, s1.subject_name;
