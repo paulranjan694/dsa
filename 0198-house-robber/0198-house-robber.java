175d8547090c39c648ecee3ab09286a5d395917a
Ranjan Paul <paulranjan694@gmail.com>
2025-12-22 23:48:57 +0530

    Time: 0 ms (100%), Space: 42.7 MB (42.12%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,19 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        dp[0] = nums[0];
+        //int[] dp = new int[n];
+       // dp[0] = nums[0];
+
+       int prev1 = nums[0],prev2=0,curr=0;
 
         for(int i=1;i<n;i++){
             int take = nums[i];
-            if(i-2>=0) take += dp[i-2];
+            if(i-2>=0) take += prev2;
 
             int nottake=0;
-            if(i-1 >= 0) nottake = dp[i-1];
+            if(i-1 >= 0) nottake = prev1;
 
-            dp[i] = Math.max(take,nottake);
+            curr = Math.max(take,nottake);
+            prev2=prev1;
+            prev1=curr;
         }
-        return dp[n-1];
+        return prev1;
     }
 
     private int utils(int[] nums, int idx, int[] dp){
