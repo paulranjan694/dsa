58c56850ea90f8ea97cba360a57a3075bb31db24
Ranjan Paul <paulranjan694@gmail.com>
2025-12-25 16:56:13 +0530

    Time: 1 ms (17.49%), Space: 43.6 MB (34.2%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,54 +1,33 @@
 class Solution {
-
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n = obstacleGrid.length;
-        int m = obstacleGrid[0].length;
+    public int uniquePathsWithObstacles(int[][] mat) {
+        int n = mat.length,m=mat[0].length;
         int[][] dp = new int[n][m];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],0);
+        for(int[] d : dp){
+            Arrays.fill(d,-1);
         }
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
-                else if(i == 0 && j == 0) dp[i][j] = 1;
-                else{
-                    int up=0,left=0;
-                    if(i-1 >= 0) up = dp[i-1][j];
-                    if(j - 1 >= 0) left = dp[i][j-1];
-                    dp[i][j] = up + left;
-                }
+        return utils(mat, n,m,n-1,m-1,dp);
+    }
+
+    private int utils(int[][] mat, int n, int m, int i, int j, int[][] dp){
+        if(i==0 && j==0) {
+            if(mat[i][j] == 1){
+                return dp[i][j]=0;
+            }else{
+                return dp[i][j]=1;
             }
         }
-        return dp[n-1][m-1];
-    }
 
-    /*
-    ******---Memoization---*******
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n = obstacleGrid.length;
-        int m = obstacleGrid[0].length;
-        int[][] dp = new int[n][m];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return utils(obstacleGrid,n-1,m-1,dp);
-    }
-
-    private int utils(int[][] grid, int n, int m, int[][] dp){
-        if(n==0 && m == 0 && grid[n][m] != 1) {
-            dp[n][m] = 1;
-            return 1;
+        if(i<0 || j <0 ) {
+            return 0;
         }
 
-        if(n < 0 || m < 0 || grid[n][m] == 1) return 0;
+        if(mat[i][j] == 1) {
+            return dp[i][j]=0;
+        }
 
-        if(dp[n][m] != -1) return dp[n][m];
 
-        int up = utils(grid,n-1,m, dp);
-        int left = utils(grid, n, m-1, dp);
-        dp[n][m] = up + left;
+        if(dp[i][j] != -1) return dp[i][j];
 
-        return dp[n][m];
-    }*/
+        return dp[i][j] = utils(mat, n,m,i-1,j, dp) + utils(mat, n,m,i,j-1, dp);
+    }
 }
