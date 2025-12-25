965233864ec7fc75428b40eb28c018ad8e72237c
Ranjan Paul <paulranjan694@gmail.com>
2025-12-25 17:02:24 +0530

    Time: 0 ms (100%), Space: 43.2 MB (85.32%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,32 +2,33 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] mat) {
         int n = mat.length,m=mat[0].length;
         int[][] dp = new int[n][m];
-        for(int[] d : dp){
-            Arrays.fill(d,-1);
-        }
-        return utils(mat, n,m,n-1,m-1,dp);
-    }
 
-    private int utils(int[][] mat, int n, int m, int i, int j, int[][] dp){
-        if(i==0 && j==0) {
-            if(mat[i][j] == 1){
-                return dp[i][j]=0;
-            }else{
-                return dp[i][j]=1;
+        if(mat[0][0] != 1){
+            dp[0][0] = 1;
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0){
+                   continue;
+                }
+
+                if(mat[i][j] != 1){
+                    int left =0, up=0;
+
+                    if(i-1 >= 0){
+                        left = dp[i-1][j];
+                    }
+
+                    if(j-1 >= 0){
+                        up = dp[i][j-1];
+                    }
+
+                    dp[i][j] = left + up;
+                }
             }
         }
 
-        if(i<0 || j <0 ) {
-            return 0;
-        }
-
-        if(mat[i][j] == 1) {
-            return dp[i][j]=0;
-        }
-
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        return dp[i][j] = utils(mat, n,m,i-1,j, dp) + utils(mat, n,m,i,j-1, dp);
+        return dp[n-1][m-1];
     }
 }
