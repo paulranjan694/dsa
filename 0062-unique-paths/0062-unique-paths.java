c4cddf8a49919599581347201b7b1269a1f45a29
Ranjan Paul <paulranjan694@gmail.com>
2025-12-25 16:25:04 +0530

    Time: 0 ms (100%), Space: 41.9 MB (81.94%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,64 +1,24 @@
 class Solution {
-    //tc - O(m-1)
-    //sc - O(1)
+    
     public int uniquePaths(int m, int n) {
-        int tempN = m+n-2;
-        return nCr(tempN,m-1);
-    }
+        int[][] dp = new int[m][n];
 
-    public int nCr(int n , int r){
-        double ans = 1;
-        for(int i = 1; i <= r; i++){
-            ans*= (n-r+i);
-            ans/=(i);
+        for(int[] d : dp){
+            Arrays.fill(d,-1);
         }
-        return (int)ans;
+
+        return utils(m,n,0,0,dp);
+       
     }
 
-    //tc - O(n^2)
-    //sc - O(n^2)
-    //dp - tabulation soln
-    public int uniquePaths_dp(int m, int n) {
-    int dp[][] = new int[m+1][n+1];
-
-    //base case
-    dp[m-1][n-1]=1;
-
-    for(int r = m-1;r>=0;r--){
-        for(int c = n-1;c>=0;c--){
-            if(r == m-1 && c==n-1) continue;
-            int temp=0;
-            temp+= dp[r][c+1];
-            temp+= dp[r+1][c];
-            dp[r][c] = temp;
-        }
-    }
-    return dp[0][0];
-    }
-
-     //tc - O(n^2)
-     //sc - O(n^2) + recursion stack space O(n^2)
-     //dp - memo soln
-    public int uniquePaths2(int m, int n) {
-        int dp[][] = new int[m][n];
-        for(int[] a : dp){
-            Arrays.fill(a,-1);
-        }
-        return utils(0,0,m,n,dp);
-    }
-
-    public int utils(int r,int c, int m ,int n,int[][] dp){
-        //base case
-        if(r==m-1 && c==n-1){
+    private int utils(int m, int n, int i, int j, int[][] dp){
+        if(i==m-1 && j==n-1){
             return 1;
         }
-        if(r >= m || c >= n){
-            return 0;
-        }
-        int res = 0;
-        if(dp[r][c] != -1) return dp[r][c];
-        res += utils(r,c+1,m,n,dp);
-        res += utils(r+1,c,m,n,dp);
-        return dp[r][c] = res;
+
+        if(i>=m || j>=n) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        return dp[i][j] = utils(m,n,i+1,j, dp) + utils(m,n,i,j+1, dp);
     }
 }
