589e2c021b91301923475c4d4d1af137ab2bfa71
Ranjan Paul <paulranjan694@gmail.com>
2025-12-25 23:06:10 +0530

    Time: 3 ms (83.48%), Space: 46.3 MB (27.25%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,52 +1,29 @@
 class Solution {
-
-    //tab
+    int NEG = -1_000_00;
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[] prev = new int[n];
-        Arrays.fill(prev,-1);
-
-        for(int i=0;i<triangle.get(n-1).size();i++){
-            prev[i] = triangle.get(n-1).get(i);
-        }
-
-        for(int i = n-2;i>=0;i--){
-            int[] curr = new int[n];
-            Arrays.fill(curr,-1);
-            for(int j = i;j >= 0;j--){
-                int down = triangle.get(i).get(j) + prev[j];
-                int diagonal = triangle.get(i).get(j) + prev[j+1];
-
-                int min = Math.min(down,diagonal);
-                curr[j] = min;
+        List<List<Integer>> dp = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            List<Integer> list = new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                list.add(NEG);
             }
-            prev=curr;
+            dp.add(list);
         }
-
-        return prev[0];
+        return utils(triangle, n,0,0, dp);
     }
 
-    public int minimumTotal_memoization(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int[][] dp = new int[n][n];
-        for(int i=0;i<n;i++)
-            Arrays.fill(dp[i],-1);
-        return utils(triangle, 0,0,dp);
-    }
-
-    private int utils(List<List<Integer>> triangle, int r, int c,int[][] dp){
-        if(r == triangle.size()-1){
-             dp[r][c] = triangle.get(r).get(c);
-            return triangle.get(r).get(c);
+    private int utils(List<List<Integer>> triangle, int n, int r, int c, List<List<Integer>> dp){
+        if(r==n-1){
+            dp.get(r).set(c,triangle.get(r).get(c));
+            return triangle.get(r).get(c);  
         }
+        
+        if(dp.get(r).get(c) != NEG) return dp.get(r).get(c);
 
-        if(dp[r][c] != -1) return dp[r][c];
 
-        int down = triangle.get(r).get(c) + utils(triangle, r+1, c,dp);
-        int diagonal = triangle.get(r).get(c) + utils(triangle, r+1, c+1,dp);
-
-        int min = Math.min(down,diagonal);
-        dp[r][c] = min;
-        return dp[r][c];
+        int mini = Math.min(utils(triangle, n, r+1, c, dp), utils(triangle, n, r+1, c+1, dp)) + triangle.get(r).get(c);
+        dp.get(r).set(c,mini);
+        return mini;
     }
 }
