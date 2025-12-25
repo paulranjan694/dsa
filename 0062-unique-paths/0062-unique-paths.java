ec3e92ddd903809c1169a426451a178673dad388
Ranjan Paul <paulranjan694@gmail.com>
2025-12-25 16:26:08 +0530

    Time: 0 ms (100%), Space: 41.9 MB (81.94%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -7,18 +7,18 @@ class Solution {
             Arrays.fill(d,-1);
         }
 
-        return utils(m,n,0,0,dp);
+        return utils(m,n,m-1,n-1,dp);
        
     }
 
     private int utils(int m, int n, int i, int j, int[][] dp){
-        if(i==m-1 && j==n-1){
+        if(i==0 && j==0){
             return 1;
         }
 
-        if(i>=m || j>=n) return 0;
+        if(i<0 || j<0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
 
-        return dp[i][j] = utils(m,n,i+1,j, dp) + utils(m,n,i,j+1, dp);
+        return dp[i][j] = utils(m,n,i-1,j, dp) + utils(m,n,i,j-1, dp);
     }
 }
