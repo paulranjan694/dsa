0ad6f7e4ce1c8822c504d73da8a4d0a8fe0fef37
Ranjan Paul <paulranjan694@gmail.com>
2025-12-26 09:12:23 +0530

    Time: 11 ms (5.34%), Space: 47.8 MB (7.62%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -10,7 +10,21 @@ class Solution {
             }
             dp.add(list);
         }
-        return utils(triangle, n,0,0, dp);
+        
+        for(int j=0;j<n;j++){
+            dp.get(n-1).add(j, triangle.get(n-1).get(j));
+        }
+
+        for(int i = n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                int down = dp.get(i+1).get(j);
+                int diagonal = dp.get(i+1).get(j+1);
+
+                int mini = Math.min(down,diagonal) + triangle.get(i).get(j);
+                dp.get(i).add(j,mini);
+            }
+        }
+        return dp.get(0).get(0);
     }
 
     private int utils(List<List<Integer>> triangle, int n, int r, int c, List<List<Integer>> dp){
