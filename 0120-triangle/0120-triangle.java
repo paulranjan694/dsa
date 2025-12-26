2dc70f6a5dc8a2ab99682362c8df33986312ddf6
Ranjan Paul <paulranjan694@gmail.com>
2025-12-26 09:13:11 +0530

    Time: 8 ms (21.22%), Space: 46.4 MB (18.55%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,13 +1,9 @@
 class Solution {
-    int NEG = -1_000_00;
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         List<List<Integer>> dp = new ArrayList<>();
         for(int i=0;i<n;i++){
             List<Integer> list = new ArrayList<>();
-            for(int j=0;j<=i;j++){
-                list.add(NEG);
-            }
             dp.add(list);
         }
         
@@ -25,19 +21,5 @@ class Solution {
             }
         }
         return dp.get(0).get(0);
-    }
-
-    private int utils(List<List<Integer>> triangle, int n, int r, int c, List<List<Integer>> dp){
-        if(r==n-1){
-            dp.get(r).set(c,triangle.get(r).get(c));
-            return triangle.get(r).get(c);  
-        }
-        
-        if(dp.get(r).get(c) != NEG) return dp.get(r).get(c);
-
-
-        int mini = Math.min(utils(triangle, n, r+1, c, dp), utils(triangle, n, r+1, c+1, dp)) + triangle.get(r).get(c);
-        dp.get(r).set(c,mini);
-        return mini;
     }
 }
