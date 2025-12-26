a28bcc793a45436108cc1e8ea2f56927d498cc65
Ranjan Paul <paulranjan694@gmail.com>
2025-12-26 23:59:48 +0530

    Time: 3 ms (87.65%), Space: 55.1 MB (23.95%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -1,28 +1,38 @@
 class Solution {
     public int minFallingPathSum(int[][] mat) {
-        int n = mat.length,m=mat[0].length;
-        int[][]dp = new int[n][m];
+        int n = mat.length, m = mat[0].length;
+        int[][] dp = new int[n][m];
 
-        //base case
-        for(int i=0;i<m;i++){
-            dp[n-1][i] = mat[n-1][i];
+        // Base case
+        for (int c = 0; c < m; c++) {
+            dp[n - 1][c] = mat[n - 1][c];
         }
 
-        for(int r = n-2;r>=0;r--){
-            for(int last=0;last<m;last++){
-                    int min=Integer.MAX_VALUE;
-                for(int c=0;c<m;c++){
-                    if(c == last) continue;
-                    min = Math.min(min,dp[r+1][c]);
+        for (int r = n - 2; r >= 0; r--) {
+            int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+            int minIdx = -1;
+
+            // Find smallest & second smallest in next row
+            for (int c = 0; c < m; c++) {
+                if (dp[r + 1][c] < min1) {
+                    min2 = min1;
+                    min1 = dp[r + 1][c];
+                    minIdx = c;
+                } else if (dp[r + 1][c] < min2) {
+                    min2 = dp[r + 1][c];
                 }
-                dp[r][last]=min + mat[r][last];
+            }
+
+            // Fill current row
+            for (int c = 0; c < m; c++) {
+                dp[r][c] = mat[r][c] + (c == minIdx ? min2 : min1);
             }
         }
-        int mini = Integer.MAX_VALUE;
-        for(int i=0;i<mat[0].length;i++){
-            mini = Math.min(mini, dp[0][i]);
+
+        int ans = Integer.MAX_VALUE;
+        for (int c = 0; c < m; c++) {
+            ans = Math.min(ans, dp[0][c]);
         }
-        
-        return mini;
+        return ans;
     }
 }
