957e247ac21954e64fbe68caee587f8496bb3371
Ranjan Paul <paulranjan694@gmail.com>
2025-12-26 10:24:41 +0530

    Time: 9 ms (83.1%), Space: 46.8 MB (74.14%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -3,10 +3,9 @@ class Solution {
         int n = customers.length();
         //int[] prefixTime = new int[n+1];
 
-        //char[] arr = customers.toCharArray();
+        char[] arr = customers.toCharArray();
         int penality=0;
-        for(int i=0;i<customers.length();i++){
-            char c = customers.charAt(i);
+        for(char c : arr){
             if(c == 'N'){
                 penality++;
             }
@@ -15,8 +14,7 @@ class Solution {
         
         int mini = penality,idx=n;
         for(int i=n-1;i>=0;i--){
-            char c = customers.charAt(i);
-            if(c=='Y'){
+            if(arr[i]=='Y'){
                 penality = penality+1;
             }else{
                 penality = penality-1;
