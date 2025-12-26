956a15597423687e9e0e7affad73719ea30b165e
Ranjan Paul <paulranjan694@gmail.com>
2025-12-26 23:40:00 +0530

    Time: 8 ms (32.54%), Space: 47.3 MB (24.83%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,15 +1,38 @@
 class Solution {
     int neg = -1_00_000;
     public int minFallingPathSum(int[][] mat) {
-        int n = mat.length,m=mat[0].length, mini = Integer.MAX_VALUE;
+        int n = mat.length,m=mat[0].length;
         int[][]dp = new int[n][m];
 
-        for(int[] d: dp){
-            Arrays.fill(d,neg);
+        //base case
+        for(int i=0;i<m;i++){
+            dp[n-1][i] = mat[n-1][i];
         }
+
+        for(int r = n-2;r>=0;r--){
+            for(int c=0;c<m;c++){
+                int leftDia = Integer.MAX_VALUE, down=Integer.MAX_VALUE, rightDia=Integer.MAX_VALUE;
+
+                if(c-1>=0){
+                    leftDia = dp[r+1][c-1];
+                }
+
+                down = dp[r+1][c];
+
+                if(c+1< m){
+                    rightDia = dp[r+1][c+1];
+                }
+
+                int min = Math.min(leftDia, Math.min(down,rightDia));
+   
+                dp[r][c]=min + mat[r][c];
+            }
+        }
+        int mini = Integer.MAX_VALUE;
         for(int i=0;i<mat[0].length;i++){
-            mini = Math.min(mini, utils(mat,0,i, dp));
+            mini = Math.min(mini, dp[0][i]);
         }
+        
         return mini;
     }
 
