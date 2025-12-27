43f4442979535c61f5d88e5ae099da35a4c7765b
Ranjan Paul <paulranjan694@gmail.com>
2025-12-27 11:58:54 +0530

    Time: 50 ms (49.05%), Space: 47.7 MB (48.59%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,55 +6,40 @@ class Solution {
             sum += num;
         }
 
-        if(sum %2 != 0) return false;
-
-        int target = sum / 2, n= nums.length;
-        Boolean[][] dp = new Boolean[n][target+1];
-
-        
-        // for(Boolean[] d : dp){
-        //     Arrays.fill(d, null);
-        // }
-
-        
-        for(int s=1;s<=target;s++){
-            if(s-nums[0] == 0) dp[0][s] = true;
-            else dp[0][s] = false; 
+        if(sum % 2 != 0){
+            return false;
         }
+
+        int target = sum/2;
+
+        return isTargetPresent(nums,target);
+    }
+
+    private boolean isTargetPresent(int[] nums, int target){
+        int n = nums.length;
+
+        boolean[][] dp = new boolean[n][target+1];
 
         for(int i=0;i<n;i++){
             dp[i][0] = true;
         }
 
-        for(int i=1;i<n;i++){
-            for(int s=1;s<=target;s++){
-                boolean taken = false;
-                if(s-nums[i] >= 0){
-                    taken = dp[i-1][s-nums[i]];
+        if(nums[0]<=target){
+            dp[0][nums[0]] = true;
+        }
+
+        for(int idx=1;idx<n;idx++){
+            for(int tar = 0;tar <= target;tar++){
+                boolean take= false;
+                if(nums[idx] <= tar){
+                    take = dp[idx-1][tar-nums[idx]];
                 }
 
-                boolean notTaken = dp[i-1][s];
+                boolean notTake = dp[idx-1][tar];
 
-                dp[i][s] = taken || notTaken;
+                dp[idx][tar] = take || notTake;
             }
         }
-
         return dp[n-1][target];
     }
-
-    private Boolean isSubsetSum(int arr[], int sum, int idx, Boolean[][] dp) {
-        if(sum == 0) return true;
-        if(sum < 0) return false;
-        if(idx==0){
-            if(sum - arr[0] == 0) return true;
-            return false;
-        }
-
-        if(dp[idx][sum] != null) return dp[idx][sum];
-        
-        boolean taken = isSubsetSum(arr, sum-arr[idx], idx-1, dp);
-        if(taken == true) return true;
-        boolean notTaken = isSubsetSum(arr, sum, idx-1, dp);
-        return dp[idx][sum]= taken || notTaken;
-    } 
 }
