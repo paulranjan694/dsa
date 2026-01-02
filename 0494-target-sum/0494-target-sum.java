7f269c979fcf1bf34bda395e5b5e259f9a74c4ed
Ranjan Paul <paulranjan694@gmail.com>
2026-01-03 01:58:57 +0530

    Time: 8 ms (60.18%), Space: 44.9 MB (50.19%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,29 +1,36 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int n = nums.length;
-        // int[][] dp = new int[n][target+1];
+        int n = nums.length, totalSum = 0;
 
-        // for()
-        return solve(nums, target, n-1, 0);
-    }
-
-    private int solve(int[] nums, int target, int idx, int sum){
-        if(idx == 0){
-            int cnt = 0;
-            if(sum+nums[idx] == target){
-                cnt++;
-            }
-
-            if(sum-nums[idx] == target){
-                cnt++;
-            }
-
-            return cnt;
+        for(int num : nums){
+            totalSum+=num;
         }
 
-        int positive = solve(nums, target, idx-1, sum+nums[idx]);
-        int negative = solve(nums, target, idx-1, sum-nums[idx]);
+        int tar = totalSum - target;
+        if(tar < 0 || tar % 2 != 0) return 0;
+        tar = tar/2;
 
-        return positive + negative;
+        int[] dp = new int[tar+1];
+
+        // Base case
+        if (nums[0] == 0) {
+            dp[0] = 2; // take or not take
+        } else {
+            dp[0] = 1;
+            if (nums[0] <= tar) dp[nums[0]] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            int[] curr = new int[tar + 1];
+
+            for (int sum = 0; sum <= tar; sum++) {
+                int notTake = dp[sum];
+                int take = (nums[i] <= sum) ? dp[sum - nums[i]] : 0;
+                curr[sum] = (take + notTake);
+            }
+            dp = curr;
+        }
+
+        return dp[tar];
     }
 }
