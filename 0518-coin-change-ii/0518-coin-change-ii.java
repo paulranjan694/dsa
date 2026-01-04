3b25d159dd154050ddf0cdfb05e724acf029674e
Ranjan Paul <paulranjan694@gmail.com>
2026-01-04 13:17:55 +0530

    Time: 11 ms (62.17%), Space: 46.7 MB (63.6%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,56 +1,35 @@
 class Solution {
-
     public int change(int amount, int[] coins) {
+        int[] prev = new int[amount+1];
+        
         int n = coins.length;
-        int[][] dp = new int[n][amount+1];
 
-        for(int a = 1;a<=amount;a++){
-            if(a % coins[0] == 0) dp[0][a]=1;
-        }
-
-        for(int i=0;i<n;i++) dp[i][0] = 1;
-
-        for(int i=1;i<n;i++){
-            for(int a=1;a<=amount;a++){
-                int taken=0;
-                if(a - coins[i] >= 0){
-                    taken = dp[i][a - coins[i]];
-                }
-                int notTaken = dp[i-1][a];
-
-                dp[i][a] = taken+notTaken;
+        //base case
+        prev[0] = 1;
+        for(int A = 1; A<=amount;A++){
+            if(A % coins[0] == 0){
+                prev[A] = 1;
             }
         }
 
-        return dp[n-1][amount];
-    }
+        
 
+        for(int idx=1;idx<n;idx++){
+            int[] curr = new int[amount+1];
+            curr[0] = 1;
+            for(int A = 1;A<=amount;A++){
+                int take = 0;
+                if(A>= coins[idx]){
+                    take = curr[A-coins[idx]];
+                }
 
-    public int change2(int amount, int[] coins) {
-        int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
-        for(int[] d : dp){
-            Arrays.fill(d,-1);
-        }
-        return solve(amount, coins, n-1, dp);
-    }
+                int notTake = prev[A];
 
-    private int solve(int amount, int[] coins, int idx, int[][] dp){
-
-        if(idx == 0){
-            if(amount % coins[idx] == 0) return 1;
-            return 0;
+                curr[A] = take + notTake;
+            }
+            prev = curr;
         }
 
-        if(dp[idx][amount] != -1) return dp[idx][amount];
-
-        int taken = 0;
-        if(amount - coins[idx] >= 0){
-            taken = solve(amount - coins[idx], coins,idx,dp);
-        }
-
-        int notTaken = solve(amount,coins,idx-1,dp);
-
-        return dp[idx][amount] = taken + notTaken;
+        return prev[amount];
     }
 }
