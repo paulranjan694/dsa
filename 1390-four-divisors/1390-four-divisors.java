ece11f7256901438bae9259e4118d8543f67b2ce
Ranjan Paul <paulranjan694@gmail.com>
2026-01-04 10:36:19 +0530

    Time: 43 ms (20.96%), Space: 46.3 MB (51.01%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int sum=0;
+
+        for(int num : nums){
+            int divSum=0,divCount=0;
+            for(int div = 1; div <= Math.sqrt(num);div++){
+                if(num % div == 0){
+                    divSum += div;
+                    int q = num/div;
+                    if(div!=q){
+                        divSum += q;
+                        divCount++;
+                    }
+                    divCount++;
+                }
+            }
+
+            if(divCount == 4){
+                sum += divSum;
+            }
+        }
+
+        return sum;
+    }
+}
