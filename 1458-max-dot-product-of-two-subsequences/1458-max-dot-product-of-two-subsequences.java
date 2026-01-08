0b50b516daedc61473781f8f93700d4adb14ec64
Ranjan Paul <paulranjan694@gmail.com>
2026-01-09 00:17:55 +0530

    Time: 11 ms (59.26%), Space: 46.7 MB (31.85%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -2,10 +2,30 @@ class Solution {
     public int maxDotProduct(int[] nums1, int[] nums2) {
         int n = nums1.length, m=nums2.length;
         int[][] dp = new int[n+1][m+1];
-        for(int[] a: dp){
-            Arrays.fill(a,Integer.MIN_VALUE);
+        
+        for(int i=0;i<=n;i++){
+            dp[i][m] = -1_000_000_00;
         }
-        return utils(nums1,nums2, n,m,0,0, dp);
+
+        for(int i=0;i<=m;i++){
+            dp[n][i] = -1_000_000_00;
+        }
+
+        for(int i = n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int max = nums1[i] * nums2[j];
+                int takeBoth =  dp[i+1][j+1];
+                int take_i_j = (nums1[i] * nums2[j]);
+                if(takeBoth != -1_000_000_00){
+                    take_i_j += takeBoth;
+                }
+                int take_i = dp[i][j+1];
+                int take_j = dp[i+1][j];
+                dp[i][j]=Math.max(Math.max(max, take_i_j), Math.max(take_i, take_j));
+            }
+        }
+
+        return dp[0][0];
     }
 
     private int utils(int[] nums1, int[] nums2, int n, int m, int i, int j, int[][] dp){
