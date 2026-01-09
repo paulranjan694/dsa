37b257196baadf7d005ec0521b5d45697c4e4b98
Ranjan Paul <paulranjan694@gmail.com>
2026-01-10 01:24:30 +0530

    Time: 0 ms (100%), Space: 43.8 MB (9.46%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.java
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.java
@@ -15,67 +15,24 @@
  */
 class Solution {
     public TreeNode subtreeWithAllDeepest(TreeNode root) {
-        List<TreeNode> leafNodes = bfs(root);
-
-        List<List<TreeNode>> paths = new ArrayList();
-        for(TreeNode leaf : leafNodes){
-            List<TreeNode> path = new ArrayList<>();
-            if(findPath(root,leaf,path))
-                paths.add(path);
-        }
-
-        TreeNode lca = null;
-        int minLen = paths.stream().mapToInt(List::size).min().getAsInt();
-
-        for (int i = 0; i < minLen; i++) {
-            TreeNode curr = paths.get(0).get(i);
-            for (List<TreeNode> p : paths) {
-                if (p.get(i) != curr) {
-                    return lca;
-                }
-            }
-            lca = curr;
-        }
-        return lca;
-
+        Pair<TreeNode, Integer> pair = dfs(root);
+        return pair.getKey();
     }
 
-    private boolean findPath(TreeNode root, TreeNode target, List<TreeNode> path){
+    private Pair<TreeNode,Integer> dfs(TreeNode root){
         if(root == null){
-            return false;
+            return new Pair<TreeNode, Integer>(null,0);
         }
 
-        if(root == target){
-            path.add(root);
-            return true;
+        Pair<TreeNode, Integer> left = dfs(root.left);
+        Pair<TreeNode, Integer> right = dfs(root.right);
+
+        if(left.getValue() == right.getValue()){
+            return new Pair<TreeNode, Integer>(root, left.getValue()+1);
+        }else if(left.getValue() > right.getValue()){
+            return new Pair<TreeNode, Integer>(left.getKey(), left.getValue()+1);
+        }else{
+            return new Pair<TreeNode, Integer>(right.getKey(), right.getValue()+1);
         }
-
-        path.add(root);
-        boolean left = findPath(root.left,target,path);
-        if(left) return true;
-        boolean right = findPath(root.right,target,path);
-        if(right) return true;
-        path.remove(path.size()-1);
-        return false;
-    }
-
-    private List<TreeNode> bfs(TreeNode root){
-        List<TreeNode> deepestLevel = new ArrayList<>();
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-
-        while(!q.isEmpty()){
-            int size = q.size();
-            deepestLevel.clear();
-            while(size-- > 0){
-                TreeNode node = q.poll();
-                deepestLevel.add(node);
-                if(node.left != null) q.offer(node.left);
-                if(node.right != null) q.offer(node.right);
-            }
-        }
-
-        return deepestLevel;
     }
 }
