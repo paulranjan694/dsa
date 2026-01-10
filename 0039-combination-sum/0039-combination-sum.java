a422465b8a0897104f1cb94abe354ecb1857e348
Ranjan Paul <paulranjan694@gmail.com>
2026-01-10 14:39:46 +0530

    Time: 2 ms (95.29%), Space: 45.7 MB (71.63%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,27 +1,36 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] arr, int t) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> sum = new ArrayList<>();
-        utils(arr,t,res,sum,0,0);
+        List<Integer> ds = new ArrayList<>();
+        int n = candidates.length;
+        utils(candidates, target, n-1, res,ds);
         return res;
     }
 
-    public void utils(int[] arr, int t,  List<List<Integer>> res,List<Integer> sum, int totalSum,int idx){
-        if(idx==arr.length && totalSum==t){
-            List<Integer> temp = new ArrayList<>(sum);
-            res.add(temp);
-            return ;
+    private void utils(int[] arr, int tar, int idx, List<List<Integer>> res, List<Integer> ds){
+        if(idx<0) return;
+        if(idx==0){
+            if(arr[idx]==tar){
+                List<Integer> list = new ArrayList<>(ds);
+                list.add(arr[idx]);
+                res.add(list);
+                return;
+            }
         }
-        if(totalSum > t || idx>=arr.length){
+
+        if(tar == 0){
+            List<Integer> list = new ArrayList<>(ds);
+            res.add(list);
             return;
         }
 
-        if(arr[idx] <= t){
-            sum.add(arr[idx]);
-            utils(arr,t,res,sum,totalSum+arr[idx],idx);
-            sum.remove(sum.size()-1);
+        if(tar-arr[idx] >= 0){
+            ds.add(arr[idx]);
+            utils(arr, tar-arr[idx], idx, res, ds);
+
+            ds.remove(ds.size()-1);
         }
-       
-        utils(arr,t,res,sum,totalSum,idx+1);
+
+        utils(arr, tar, idx-1, res, ds);
     }
 }
