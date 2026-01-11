07336b1fba48043caeb690f846eb7ea3b06e03ea
Ranjan Paul <paulranjan694@gmail.com>
2026-01-11 12:10:43 +0530

    Time: 25 ms (89.86%), Space: 63.6 MB (69.73%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -7,10 +7,13 @@ class Solution {
     private int lcs(String text1, String text2) {
         int l1 = text1.length(), l2 = text2.length();
         int[][] dp = new int[l1+1][l2+1];
+
+        char[] a1 = text1.toCharArray();
+        char[] a2 = text2.toCharArray();
         
         for(int i=1;i<=l1;i++){
             for(int j=1;j<=l2;j++){
-                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                if(a1[i-1] == a2[j-1]){
                     dp[i][j] = dp[i-1][j-1] + 1;
                 }else{
                     dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]);
