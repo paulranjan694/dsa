31772c36384bb84388eb99e695c26ac70ee9744a
Ranjan Paul <paulranjan694@gmail.com>
2026-01-13 23:54:46 +0530

    Time: 33 ms (34.02%), Space: 143.7 MB (81.2%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,37 +1,40 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] arr, int k) {
-        ArrayList<Integer> res = new ArrayList<>();
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        List<Integer> temp = new ArrayList<>();
+
         Deque<Integer> dq = new ArrayDeque<>();
-        int n = arr.length;
-        
+        int n = nums.length;
         for(int i=0;i<k;i++){
-            while(!dq.isEmpty() && arr[i] > arr[dq.peekLast()]){
+            while(!dq.isEmpty() && nums[i] > nums[dq.getLast()]){
                 dq.pollLast();
             }
-            dq.add(i);
+
+            dq.addLast(i);
         }
-        
-        res.add(arr[dq.peekFirst()]);
-        
+
+        temp.add(nums[dq.getFirst()]);
+
         for(int i=k;i<n;i++){
-            
-            if(!dq.isEmpty() && dq.peekFirst() <= i-k){
+            if(!dq.isEmpty() && dq.getFirst() <= i-k){
                 dq.pollFirst();
             }
-            
-            while(!dq.isEmpty() && arr[i] > arr[dq.peekLast()]){
+
+            while(!dq.isEmpty() && nums[i] > nums[dq.getLast()]){
                 dq.pollLast();
             }
+
             dq.addLast(i);
-            res.add(arr[dq.peekFirst()]);
+            temp.add(nums[dq.getFirst()]);
         }
 
-        int[] answer = new int[res.size()];
+        int[] res = new int[temp.size()];
 
-        for(int i=0;i<res.size();i++){
-            answer[i] = res.get(i);
+        for(int i=0;i<res.length;i++){
+            res[i] = temp.get(i);
         }
-        
-        return answer;
+
+        return res;
+
+
     }
 }
