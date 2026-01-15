766d3d74dda4a8552cff07239054ed81f02e5f19
Ranjan Paul <paulranjan694@gmail.com>
2026-01-16 00:17:19 +0530

    Time: 22 ms (0%), Space: 46.9 MB (10.29%) - LeetHub

---
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+        int longHBars = 1, longVBars=1;
+
+        Arrays.sort(hBars);
+        Arrays.sort(vBars);
+
+        int tempLen=1;
+        for(int i=1;i<hBars.length;i++){
+            if(hBars[i] == hBars[i-1] + 1){
+                tempLen++;
+            }else{
+                tempLen=1;
+            }
+            longHBars = Math.max(longHBars, tempLen);
+        }
+
+        tempLen=1;
+        for(int i=1;i<vBars.length;i++){
+            System.out.println(i);
+            if(vBars[i] == vBars[i-1] + 1){
+                tempLen++;
+            }else{
+                tempLen=1;
+            }
+            longVBars = Math.max(longVBars, tempLen);
+        }
+
+        int len = Math.min(longHBars, longVBars) + 1;
+
+        return len * len;
+    }
+}
