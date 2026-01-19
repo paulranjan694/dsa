c3536873d385f9984f6536bb3f5f722393796327
Ranjan Paul <paulranjan694@gmail.com>
2026-01-19 10:06:26 +0530

    Time: 18 ms (71.21%), Space: 48.1 MB (27.91%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -2,48 +2,44 @@ class Solution {
     public String removeKdigits(String s, int k) {
         if(s.length() <= k) return "0";
         
-        Stack<Integer> st = new Stack<>();
+        Stack<Character> st = new Stack<>();
         
         int idx=0, n = s.length();
         char[] arr = s.toCharArray();
         
-        while(idx < n && k > 0){
-            while(!st.isEmpty() && (arr[st.peek()]-'0') > (arr[idx]-'0')){
+        while(idx < n){
+            while(!st.isEmpty() && k > 0 && st.peek() > arr[idx]){
                 st.pop();
                 k--;
                 
                 if(k==0) break;
             }
             
-            st.push(idx);
+            st.push(arr[idx]);
             idx++;
         }
 
-        if(k!=0){
-            while(!st.isEmpty() && k >0){
-                st.pop();
-                k--;
-            }
+     
+        while(!st.isEmpty() && k >0){
+            st.pop();
+            k--;
         }
+        
         
         StringBuilder sb = new StringBuilder();
         
-        int i=n-1;
-        while(i>=idx){
-            sb.append(arr[i--]);
-        }
         
         while(!st.isEmpty()){
-            sb.append(arr[st.pop()]);
+            sb.append(st.pop());
         }
         
         String ss = sb.reverse().toString();
-        i=0;
+        int i=0;
         while(i<ss.length()-1 && ss.charAt(i) == '0'){
             i++;
         }
         
-        ss=i < ss.length() ?  ss.substring(i) : ss ;
+        ss=ss.substring(i);
         return ss;
     }
 }
