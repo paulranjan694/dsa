76217ad130f77ffcdd6e7c5835affa52b17cee1a
Ranjan Paul <paulranjan694@gmail.com>
2026-01-20 03:27:25 +0530

    Time: 0 ms (100%), Space: 52.9 MB (66.8%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,27 +1,25 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n = nums.length;
-        int l =0,h=n-1;
-        while(l<=h){
-            int mid = l + (h-l) /2;
-            if(mid-1 >= 0 && mid+1 < n &&( nums[mid-1] == nums[mid] || nums[mid+1] == nums[mid])){
-            if(nums[mid-1] == nums[mid]){
-                if(mid % 2 != 0){
-                    l=mid+1;
-                }else{
-                    h=mid-1;
-                }
-            }else{
-                if((mid) % 2 != 0){
-                    h=mid-1;
-                }else{
-                    l=mid+1;
-                }
-            }
-            }else{
+        int n=nums.length;
+        if(n>1 && nums[0]!=nums[1]) return nums[0];
+
+        if(n>1 && nums[n-1]!=nums[n-2]) return nums[n-1];
+
+        if(n==1) return nums[0];
+
+        int low=1, high=n-2;
+        while(low<=high){
+            int mid = (low+high)/2;
+
+            if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]){
                 return nums[mid];
+            }else if((nums[mid] == nums[mid-1] && (mid-1)%2==0 && mid%2!=0) || (nums[mid] == nums[mid+1] && mid%2==0 && (mid+1)%2!=0)){
+                low=mid+1;
+            }else{
+                high=mid-1;
             }
         }
+
         return -1;
     }
 }
