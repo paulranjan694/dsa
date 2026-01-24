3d64429ffcb9c25f0d11780d17ed16c56d18d2c0
Ranjan Paul <paulranjan694@gmail.com>
2026-01-24 19:15:11 +0530

    Time: 5 ms (65.54%), Space: 47.3 MB (43.28%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,49 +1,46 @@
 class Solution {
-    public int minDistance(String w1, String w2) {
-        int n = w1.length(),m=w2.length();
+    public int minDistance(String word1, String word2) {
+        int n = word1.length(), m = word2.length();
+
         int[][] dp = new int[n+1][m+1];
 
-        for(int i=1;i<=n;i++) dp[i][0] = i;
-        for(int j=1;j<=m;j++) dp[0][j] = j;
-        //dp[0][0] = 1;
+        for(int i=1;i<n+1;i++){
+            dp[i][0] = i;//1 based indexing
+        }
+
+        for(int j=1;j<m+1;j++){
+            dp[0][j] = j;//1 based indexing
+        }
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(w1.charAt(i-1)==w2.charAt(j-1)){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1];
                 }else{
-                    int insertop = dp[i][j-1];
-                    int deleteop = dp[i-1][j];
-                    int replaceop = dp[i-1][j-1];
-                    dp[i][j] = 1 + Math.min(insertop, Math.min(deleteop, replaceop));
+                    int insertOp = dp[i][j-1];
+                    int deleteOp = dp[i-1][j];
+                    int replaceOp = dp[i-1][j-1];
+                    dp[i][j]= 1 + Math.min(insertOp, Math.min(deleteOp,replaceOp));
                 }
-
             }
         }
 
         return dp[n][m];
+    }
+
+    private int utils(String s, String t, int i, int j){
         
-    }
-
-    public int minDistance2(String w1, String w2) {
-        int n = w1.length(),m=w2.length();
-        int[][] dp = new int[n][m];
-        for(int[] row : dp) Arrays.fill(row,-1);
-        return utils(w1,w2,n-1,m-1,dp);
-    }
-
-    private int utils(String w1, String w2, int i, int j, int[][] dp){
-        //base case
-        if(i<0) return j+1;
         if(j<0) return i+1;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(w1.charAt(i)==w2.charAt(j)){
-            return dp[i][j] = utils(w1,w2,i-1,j-1,dp);
+        if(i<0) return j+1;
+
+        if(s.charAt(i) == t.charAt(j)){
+            return utils(s,t,i-1,j-1);
         }
 
-        int insertop = utils(w1,w2,i,j-1,dp);
-        int deleteop = utils(w1,w2,i-1,j,dp);
-        int replaceop = utils(w1,w2,i-1,j-1,dp);
-        return dp[i][j] = 1 + Math.min(insertop, Math.min(deleteop, replaceop));
+        int insertOp = utils(s,t,i,j-1);
+        int deleteOp = utils(s,t,i-1,j);
+        int replaceOp = utils(s,t,i-1,j-1);
+        int minOp = 1 + Math.min(insertOp, Math.min(deleteOp,replaceOp));
+        return minOp;
     }
 }
