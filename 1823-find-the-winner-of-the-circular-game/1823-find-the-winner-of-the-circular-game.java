176363e0d5ddc3d25d10803fe65333c93582b017
Ranjan Paul <paulranjan694@gmail.com>
2026-01-24 12:37:04 +0530

    Time: 0 ms (100%), Space: 42.5 MB (51.24%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,12 +1,13 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        
         return utils(n,k)+1;
     }
-
     private int utils(int n, int k){
+        //base case: if only 1 remaining, index would be 0
         if(n==1) return 0;
 
-        return (utils(n-1,k)+k) % n;
+        int idx = utils(n-1,k);
+        int orgIdx = (idx + k) % n; // doing +k, to the idx to get the original index and doing %n so that it should go beyond n
+        return orgIdx;
     }
 }
