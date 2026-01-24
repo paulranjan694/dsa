be91e46b40746622e92ea0045b185ba90c3ae228
Ranjan Paul <paulranjan694@gmail.com>
2026-01-24 21:35:09 +0530

    Time: 21 ms (62.16%), Space: 63.5 MB (18.95%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,10 +1,46 @@
 class Solution {
+    public boolean isMatch2(String s, String p) {
+        int n = s.length(), m = p.length();
+        Boolean[][] dp = new Boolean[n+1][m+1];
+
+        dp[0][0] = true;
+
+        for(int i=1;i<=n;i++){
+            dp[i][0] = false;
+        }
+
+        for(int j=1;j<=m;j++){
+            if(p.charAt(j-1) == '*'){
+                dp[0][j] = true;
+            }else{
+                dp[0][j] = false;
+            }
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1) == p.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    if(p.charAt(j-1)=='?'){
+                        dp[i][j] = dp[i-1][j-1];
+                    }else if(p.charAt(j-1) == '*'){
+                        dp[i][j] = dp[i-1][j] | dp[i][j-1];
+                    }else{
+                        dp[i][j]=false;
+                    }
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
     public boolean isMatch(String s, String p) {
         int n = s.length(), m = p.length();
         Boolean[][] dp = new Boolean[n+1][m+1];
         return utils(s,p,n-1,m-1, dp);
     }
-
     private boolean utils(String s, String p, int i, int j, Boolean[][] dp){
         if(i<0 && j<0) {
             dp[i+1][j+1] = true;
@@ -33,9 +69,7 @@ class Solution {
             if(p.charAt(j)=='?'){
                 return dp[i+1][j+1] = utils(s,p,i-1,j-1, dp);
             }else if(p.charAt(j) == '*'){
-                boolean res = false;
-                res |= utils(s,p,i-1, j, dp) | utils(s,p,i, j-1, dp);
-                return dp[i+1][j+1] = res;
+                return dp[i+1][j+1] = utils(s,p,i-1, j, dp) | utils(s,p,i, j-1, dp);
             }else{
                 return dp[i+1][j+1]=false;
             }
