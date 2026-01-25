b43100124393092750ee088c3dbc7f5936e4867e
Ranjan Paul <paulranjan694@gmail.com>
2026-01-25 10:10:07 +0530

    Time: 1 ms (18.18%), Space: 46.7 MB (9.98%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,46 +1,28 @@
 class Solution {
-
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int[][] dp = new int[n+1][2];
-        
         //base case
-        dp[n][0]=0;
-        dp[n][1]=0;
+        dp[n][0]=dp[n][1] = 0;
 
         for(int day = n-1;day>=0;day--){
-            for(int buy = 0; buy < 2; buy++){
-                int profit = 0;
-                if(buy == 1){
-                    profit += Math.max((+prices[day]+dp[day+1][0]), dp[day+1][1]);
+            for(int buy=0;buy<2;buy++){
+                if(buy == 0){
+                    dp[day][buy] =  Math.max(-prices[day]+dp[day+1][1], dp[day+1][0]);
                 }else{
-                    profit += Math.max((-prices[day]+dp[day+1][1]), dp[day+1][0]);
+                    dp[day][buy] = Math.max(prices[day]+dp[day+1][0], dp[day+1][1]);
                 }
-                dp[day][buy] = profit;
             }
         }
         return dp[0][0];
     }
 
-    public int maxProfit2(int[] prices) {
-        int n = prices.length;
-        int[][] dp = new int[n+1][2];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
-        }
-        return utils(0,0, prices,dp);
-    }
-
-    private int utils(int day, int buy, int[] prices, int[][] dp){
-        if(day==prices.length) return 0;
-        if(dp[day][buy] != -1) return dp[day][buy];
-        int profit = 0;
-
-        if(buy == 1){
-            profit += Math.max((+prices[day]+utils(day+1,0,prices,dp)), utils(day+1,1,prices,dp));
+    private int solve(int[] prices, int idx,int buy){
+        if(idx==prices.length) return 0;
+        if(buy == 0){
+            return Math.max(-prices[idx]+solve(prices, idx+1,1), solve(prices,idx+1,0));
         }else{
-            profit += Math.max((-prices[day]+utils(day+1,1,prices,dp)), utils(day+1,0,prices,dp));
+            return Math.max(prices[idx]+solve(prices, idx+1,0), solve(prices,idx+1,1));
         }
-        return dp[day][buy] = profit;
     }
 }
