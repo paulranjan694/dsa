f817596f170ea6b76f9c4d77dadde17543e3e451
Ranjan Paul <paulranjan694@gmail.com>
2026-01-26 23:45:28 +0530

    Time: 16 ms (99.55%), Space: 45.1 MB (82.49%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,39 +1,34 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
+        List<Integer> res = new ArrayList<>();
         int n = nums.length;
-        
+        int[] dp = new int[n];
+        int[] hash = new int[n];
         Arrays.sort(nums);
-        List<Integer>[][] dp = new ArrayList[n][n+1];
-        
-        return lis(nums,0,-1,dp);
+
+        Arrays.fill(dp,1);
+        int lastIndex=0;
+        for(int i=1;i<n;i++){
+            hash[i] = i;
+
+            for(int j=0;j<i;j++){
+                if((nums[i]%nums[j] == 0) && dp[i] < 1 + dp[j]){
+                    dp[i] = 1 + dp[j];
+                    hash[i]=j;
+                }
+            }
+
+            if(dp[lastIndex] < dp[i]){
+                lastIndex = i;
+            }
+        }
+        res.add(nums[lastIndex]);
+        while(hash[lastIndex] != lastIndex){
+            lastIndex = hash[lastIndex];
+            res.add(nums[lastIndex]);
+        }
+
+        Collections.reverse(res);
+        return res;
     }
-
-    private List<Integer> lis(int[] nums, int idx, int prev, List<Integer>[][] dp ){
-        if(idx==nums.length) {
-            return new ArrayList<>();
-        }
-        
-        if (dp[idx][prev + 1] != null) {
-            return dp[idx][prev + 1];
-        }
-
-        List<Integer> take = new ArrayList<>();
-        if(prev == -1 || nums[idx] % nums[prev]==0){
-           take = new ArrayList<>();
-            take.add(nums[idx]);
-            take.addAll(lis(nums, idx + 1, idx, dp));
-        }
-
-
-        List<Integer> skip = lis(nums, idx + 1, prev, dp);
-
-        // Take the longer one
-        if (take.size() > skip.size()) {
-            dp[idx][prev + 1] = take;
-        } else {
-            dp[idx][prev + 1] = skip;
-        }
-
-        return dp[idx][prev + 1];
-    } 
 }
