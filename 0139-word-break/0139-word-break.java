31e7c5eef96d3080fbfa3836714b55ab7dbbea8e
Ranjan Paul <paulranjan694@gmail.com>
2026-01-28 01:14:48 +0530

    Time: 1 ms (99.57%), Space: 43 MB (91.47%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,23 +1,29 @@
 class Solution {
     Boolean[] memo;
     Set<String> dict;
-    int n;
+    int n, maxLen = 0;
+
     public boolean wordBreak(String s, List<String> wordDict) {
         n = s.length();
         dict = new HashSet<>(wordDict);
         memo = new Boolean[n];
-        return solve(s,0);
+
+        for(String w : wordDict){
+            maxLen = Math.max(maxLen, w.length());
+        }
+
+        return solve(s, 0);
     }
 
     private boolean solve(String s, int idx){
-        if(idx>=n) return true;
+        if(idx == n) return true;
         if(memo[idx] != null) return memo[idx];
-        for(int l=idx+1;l<=n;l++){
-            String temp = s.substring(idx,l);
-            if(dict.contains(temp) && solve(s,l)){
+
+        for(int l = idx + 1; l <= n && l - idx <= maxLen; l++){
+            if(dict.contains(s.substring(idx, l)) && solve(s, l)){
                 return memo[idx] = true;
             }
         }
-        return memo[idx]=false;
+        return memo[idx] = false;
     }
 }
