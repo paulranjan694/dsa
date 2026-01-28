c32bd9b909ef8b8f08c9ff2cf476c7cf077de02e
Ranjan Paul <paulranjan694@gmail.com>
2026-01-28 18:48:58 +0530

    Time: 73 ms (23.1%), Space: 45 MB (26.07%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,21 @@
+class Solution {
+    int[] memo;
+    public int numSquares(int n) {
+        memo = new int[n+1];
+        Arrays.fill(memo,-1);
+        return solve(n);
+    }
+
+    private int solve(int n){
+        if(n==0) return 0;
+        if(n<0) return Integer.MAX_VALUE-1000;
+        if(memo[n] != -1) return memo[n];
+        int limit = (int)Math.sqrt(n);
+        int min = Integer.MAX_VALUE;
+        for(int i=1;i<=limit;i++){
+            int t = 1+solve(n-i*i);
+            min=Math.min(min,t);
+        }
+        return memo[n]=min;
+    }
+}
