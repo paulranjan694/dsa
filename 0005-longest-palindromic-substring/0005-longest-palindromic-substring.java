7682ed9784fb2a3f12e670afe519a808dcc74962
Ranjan Paul <paulranjan694@gmail.com>
2026-01-29 20:36:16 +0530

    Time: 113 ms (29.83%), Space: 48 MB (12.74%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,41 +1,30 @@
 class Solution {
-    int len=0,start=0;
-    Boolean[][] memo;
     public String longestPalindrome(String s) {
-        int n = s.length();
-        memo = new Boolean[n+1][n+1];
-        solve(s.toCharArray(), 0,n-1);
-        return s.substring(start, start+len+1);
-    }
+        int n = s.length(), maxLen=0, startIdx=0;
+        char[] arr = s.toCharArray();
+        String res="";
+        boolean[][] dp = new boolean[n+1][n+1];
 
-    private boolean solve(char[] arr, int i, int j){
-        //base case
-        if(i>=j) return true;
-        if(memo[i][j] != null) return memo[i][j];
-        if(arr[i]==arr[j]){
-            if(isPalindrome(arr,i,j)){
-                if(len < j-i){
-                    len=j-i;
-                    start=i;
+        for(int len=1;len<=n;len++){
+            for(int i=0;i+len-1 < n;i++){
+                int j = i+len-1;
+
+                if(i==j){
+                    dp[i][j] = true;
+                }else if(i+1==j){
+                    dp[i][j] = (arr[i]==arr[j]);
+                }else{
+                    dp[i][j] = (arr[i] == arr[j]) && dp[i+1][j-1];
                 }
-                return memo[i][j]=true;
+
+                if(dp[i][j]){
+                    if(maxLen < len){
+                        maxLen = len;
+                        startIdx=i;
+                    }
+                }
             }
         }
-
-        boolean first = solve(arr,i,j-1);
-        boolean second = solve(arr,i+1,j);
-
-        return memo[i][j] = first || second;
-        
-    }
-
-    private boolean isPalindrome(char[] arr, int i, int j){
-        while(i<=j){
-            if(arr[i]!=arr[j]){
-                return false;
-            }
-            i++;j--;
-        }
-        return true;
+        return s.substring(startIdx, startIdx+maxLen); 
     }
 }
