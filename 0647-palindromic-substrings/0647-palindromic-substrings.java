057759c43865d78c5d7a8d45194848deb792028e
Ranjan Paul <paulranjan694@gmail.com>
2026-01-29 20:01:29 +0530

    Time: 397 ms (17.92%), Space: 43 MB (65.49%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,31 +1,24 @@
 class Solution {
     public int countSubstrings(String s) {
+        int n = s.length(), count=0;
         char[] arr = s.toCharArray();
-        int count=0,n=arr.length;
-        //if(n==1) return s;
-        int[][] dp = new int[n+1][n+1];
-
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                if(isPalindrome(arr,i,j,dp) == 1){
+                if(check(arr,i,j)){
                     count++;
                 }
             }
         }
-
         return count;
+        
     }
 
-    private int isPalindrome(char[] arr, int s, int e, int[][] dp){
-        if(s>=e) return 1;
-        if(dp[s][e] != -1) return dp[s][e];
-        if(arr[s]==arr[e]){
-            return dp[s][e] = isPalindrome(arr, s+1, e-1,dp);
+    private boolean check(char[] arr, int i, int j){
+        if(i>=j) return true;
+
+        if(arr[i]==arr[j]){
+            return check(arr,i+1,j-1);
         }
-        else return dp[s][e] = 0;
+        return false;
     }
 }
