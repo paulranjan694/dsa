318bb0a9739111a9143aa9846f9fdb65114c3597
Ranjan Paul <paulranjan694@gmail.com>
2026-01-30 20:42:05 +0530

    Time: 5 ms (54.89%), Space: 46.4 MB (69.98%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,38 +1,36 @@
 class Solution {
     public int countComponents(int n, int[][] edges) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        boolean[] vis = new boolean[n];
+        
+        List<List<Integer>> adj = new ArrayList<>();
         for(int i=0;i<n;i++){
-            //Arraylist<Integer> al = new Arraylist<Integer>();
-            adj.add(new ArrayList<Integer>());
+            adj.add(new ArrayList<>());
         }
-
+        
         for(int[] edge : edges){
             int u = edge[0];
             int v = edge[1];
+            
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-
-        int[] visited = new int[n];
-        int ans=0;
+        
+        int count=0;
         for(int i=0;i<n;i++){
-            if(visited[i]==0){
-                dfs(i,visited,adj);
-                ans++;
-            }else{
-                visited[i]=1;
+            if(!vis[i]){
+                dfs(adj,vis,i);
+                count++;
             }
         }
-        return ans;
+        return count;
     }
 
-    private void dfs(int node, int[] visited, ArrayList<ArrayList<Integer>> adj){
-        visited[node] = 1;
-        ArrayList<Integer> list = adj.get(node);
-        for(int nghbr : list){
-            if(visited[nghbr] != 1){
-                dfs(nghbr,visited,adj);
+    private void dfs(List<List<Integer>> adj, boolean[] vis, int u){
+       vis[u] = true;
+       for(int nbr : adj.get(u)){
+            if(!vis[nbr]){
+                dfs(adj, vis, nbr);
             }
-        }
+       }
     }
 }
