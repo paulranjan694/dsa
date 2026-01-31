99a4592cc3646726e50e174fac164f1023915687
Ranjan Paul <paulranjan694@gmail.com>
2026-01-31 22:43:02 +0530

    Time: 7 ms (61.93%), Space: 46.9 MB (85.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,49 +1,42 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
         List<List<Integer>> adj = new ArrayList<>();
+
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        
-        for(int i=0;i<prerequisites.length;i++){
-           int u = prerequisites[i][0];
-           int v = prerequisites[i][1];
-           
-           adj.get(u).add(v);
+
+        for(int[] preq : prerequisites){
+            int u = preq[1];
+            int v = preq[0];
+
+            adj.get(u).add(v);
+            indegree[v]++;
         }
-        
-        int[] indegree = new int[numCourses];
-        for(List<Integer> list: adj){
-            for(int v: list){
-                indegree[v]++;
-            }
-        }
-        
-        boolean[] visited = new boolean[numCourses];
-        //ArrayList<Integer> res = new ArrayList<>();
+
+        Queue<Integer> q = new LinkedList<>();
+
         int count=0;
-        
-       Queue<Integer> queue = new LinkedList<>();
-        for(int i =0; i< numCourses;i++){
-            if(indegree[i]== 0){
-                queue.add(i);
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i] == 0){
+                q.offer(i);
                 count++;
             }
         }
-        
-        while(!queue.isEmpty()){
-            int node = queue.poll();
-            //res.add(node);
-            
-            for(int v: adj.get(node)){
+
+        while(!q.isEmpty()){
+            int u = q.poll();
+
+            for(int v : adj.get(u)){
                 indegree[v]--;
-                if(indegree[v] == 0){
-                    queue.add(v);
+                if(indegree[v]==0){
+                    q.offer(v);
                     count++;
                 }
             }
         }
-        
+
         return count == numCourses;
     }
 }
