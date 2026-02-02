60b4493bf2816ead69f374c95a0539425b70c1bc
Ranjan Paul <paulranjan694@gmail.com>
2026-02-02 23:08:51 +0530

    Time: 298 ms (52.17%), Space: 105.4 MB (13.04%) - LeetHub

--- 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.java
+++ 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.java
@@ -1,106 +1,72 @@
-class Container {
-
-    private int k;
-    private int st1Size;
-    private int st2Size;
-    // st1 saves the k smallest values, st2 saves the other values
-    private TreeMap<Integer, Integer> st1;
-    private TreeMap<Integer, Integer> st2;
-    // sm represents the sum of the first k smallest elements
-    private long sm;
-
-    public Container(int k) {
-        this.k = k;
-        this.st1 = new TreeMap<>();
-        this.st2 = new TreeMap<>();
-        this.sm = 0;
-        this.st1Size = 0;
-        this.st2Size = 0;
-    }
-
-    private void removeOne(TreeMap<Integer, Integer> map, int key) {
-        int count = map.get(key);
-        if (count == 1) {
-            map.remove(key);
-        } else {
-            map.put(key, count - 1);
-        }
-    }
-
-    private void addOne(TreeMap<Integer, Integer> map, int key) {
-        map.put(key, map.getOrDefault(key, 0) + 1);
-    }
-
-    private void adjust() {
-        while (st1Size < k && !st2.isEmpty()) {
-            int x = st2.firstKey();
-            addOne(st1, x);
-            st1Size++;
-            sm += x;
-            removeOne(st2, x);
-            st2Size--;
-        }
-        while (st1Size > k) {
-            int x = st1.lastKey();
-            addOne(st2, x);
-            st2Size++;
-            removeOne(st1, x);
-            st1Size--;
-            sm -= x;
-        }
-    }
-
-    // insert element x
-    public void add(int x) {
-        if (!st2.isEmpty() && x >= st2.firstKey()) {
-            addOne(st2, x);
-            st2Size++;
-        } else {
-            addOne(st1, x);
-            st1Size++;
-            sm += x;
-        }
-        adjust();
-    }
-
-    // delete element x
-    public void erase(int x) {
-        if (st1.containsKey(x)) {
-            removeOne(st1, x);
-            st1Size--;
-            sm -= x;
-        } else if (st2.containsKey(x)) {
-            removeOne(st2, x);
-            st2Size--;
-        }
-        adjust();
-    }
-
-    // sum of the first k smallest elements
-    public long sum() {
-        return sm;
-    }
-}
+import java.util.*;
 
 class Solution {
-
     public long minimumCost(int[] nums, int k, int dist) {
         int n = nums.length;
-        Container cnt = new Container(k - 2);
-        for (int i = 1; i < k - 1; i++) {
-            cnt.add(nums[i]);
-        }
 
-        long ans = cnt.sum() + nums[k - 1];
-        for (int i = k; i < n; i++) {
-            int j = i - dist - 1;
-            if (j > 0) {
-                cnt.erase(nums[j]);
+        // TreeSet to store (value, index) pairs
+        TreeSet<int[]> kMinimum = new TreeSet<>(
+            (a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0])
+                                   : Integer.compare(a[1], b[1])
+        );
+
+        TreeSet<int[]> remaining = new TreeSet<>(
+            (a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0])
+                                   : Integer.compare(a[1], b[1])
+        );
+
+        long sum = 0; // sum of (k-1) smallest elements
+        long result = Long.MAX_VALUE;
+
+        int i = 1;
+
+        // Build initial window
+        while (i - dist < 1) {
+            int[] cur = new int[]{nums[i], i};
+            kMinimum.add(cur);
+            sum += nums[i];
+
+            if (kMinimum.size() > k - 1) {
+                int[] largest = kMinimum.pollLast();
+                sum -= largest[0];
+                remaining.add(largest);
             }
-            cnt.add(nums[i - 1]);
-            ans = Math.min(ans, cnt.sum() + nums[i]);
+            i++;
         }
 
-        return ans + nums[0];
+        // Sliding window
+        while (i < n) {
+            int[] cur = new int[]{nums[i], i};
+            kMinimum.add(cur);
+            sum += nums[i];
+
+            if (kMinimum.size() > k - 1) {
+                int[] largest = kMinimum.pollLast();
+                sum -= largest[0];
+                remaining.add(largest);
+            }
+
+            result = Math.min(result, sum);
+
+            // Remove element that goes out of window
+            int removeIdx = i - dist;
+            int[] toRemove = new int[]{nums[removeIdx], removeIdx};
+
+            if (kMinimum.remove(toRemove)) {
+                sum -= nums[removeIdx];
+
+                if (!remaining.isEmpty()) {
+                    int[] promote = remaining.pollFirst();
+                    kMinimum.add(promote);
+                    sum += promote[0];
+                }
+            } else {
+                remaining.remove(toRemove);
+            }
+
+            i++;
+        }
+
+        return nums[0] + result;
     }
 }
