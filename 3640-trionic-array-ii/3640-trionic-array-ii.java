5d6318a65874e65a77c647dbf6d02236e7de5c33
Ranjan Paul <paulranjan694@gmail.com>
2026-02-04 13:14:51 +0530

    Time: 48 ms (15.38%), Space: 115.5 MB (5.13%) - LeetHub

--- 3640-trionic-array-ii/3640-trionic-array-ii.java
+++ 3640-trionic-array-ii/3640-trionic-array-ii.java
@@ -7,7 +7,53 @@ class Solution {
         for(long[] a : memo){
             Arrays.fill(a,Long.MIN_VALUE);
         }
-        return solve(0,0, nums); //solve(i, trend=0)
+
+        for(int trend=0;trend<4;trend++){
+            memo[n][trend] = trend==3 ? 0 : Long.MIN_VALUE/2;
+        }
+
+        for(int idx = n-1;idx>=0;idx--){
+            for(int trend=0;trend<4;trend++){
+                long take = Long.MIN_VALUE/2;
+                long skip = Long.MIN_VALUE/2;
+
+                if(trend==0){
+                    skip = solve(idx+1, trend, nums);
+                }
+
+                if(trend==3){
+                    take = nums[idx];//finish the sub-array here.
+                }
+
+                if(idx+1 < n){
+                    int curr = nums[idx], next = nums[idx+1];
+                    if(trend==0 && curr<next){
+                        take = Math.max(take, curr + memo[idx+1][1]);
+                    }else if(trend==1){
+                        if(curr < next){
+                            take = Math.max(take, curr + memo[idx+1][1]);
+                        }else if(curr > next){
+                            take = Math.max(take, curr + memo[idx+1][2]);
+                        }
+                    }else if(trend==2){
+                        if(curr > next){
+                            take = Math.max(take, curr + memo[idx+1][2]);
+                        }else if(curr < next){
+                            take = Math.max(take, curr + memo[idx+1][3]);
+                        }
+                    }else if(trend==3 && next > curr){
+                        take = Math.max(take, curr + memo[idx+1][3]);
+                    }
+                }
+
+                memo[idx][trend]=Math.max(skip,take);
+            }
+        }
+        return memo[0][0];
+
+
+
+        //return solve(0,0, nums); //solve(i, trend=0)
     }
 
     private long solve(int idx, int trend, int[] nums){
