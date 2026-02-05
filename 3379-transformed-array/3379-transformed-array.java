f810a81983fd6656f0b9c0daa688a04b2894548d
Ranjan Paul <paulranjan694@gmail.com>
2026-02-05 08:38:51 +0530

    Time: 1 ms (98.72%), Space: 46.9 MB (13.78%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.java
+++ 3379-transformed-array/3379-transformed-array.java
@@ -3,16 +3,10 @@ class Solution {
         int n = nums.length;
         int[] res = new int[n];
 
-        for(int i=0;i<n;i++){
-            if(nums[i] > 0){
-                int idx = (i+nums[i]) % n;
-                res[i] = nums[idx];
-            }else if(nums[i] < 0){
-                int idx = (i + (n-(Math.abs(nums[i])%n))) % n;
-                 res[i] = nums[idx];
-            }else{
-                res[i]=nums[i];
-            }
+        for (int i = 0; i < n; i++) {
+            int idx = (i + nums[i]) % n;
+            if (idx < 0) idx += n;
+            res[i] = nums[idx];
         }
         return res;
     }
