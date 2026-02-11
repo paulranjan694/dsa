3b976d5b5025f8bf50bdd742c48e90774da287aa
Ranjan Paul <paulranjan694@gmail.com>
2026-02-11 22:28:42 +0530

    Time: 560 ms (89.05%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,9 +1,8 @@
-WITH ranked AS (
-    SELECT Id,
-           ROW_NUMBER() OVER(PARTITION BY email ORDER BY id ASC) AS row_num
-    FROM Person
-)
-DELETE FROM Person
-WHERE Id IN (
-    SELECT Id FROM ranked WHERE row_num > 1
+# Write your MySQL query statement below
+
+delete from Person where id in ( 
+    select id from (
+        select id, dense_rank() over(partition by email order by id asc) as rnk from Person
+    )t where t.rnk > 1
 );
+
