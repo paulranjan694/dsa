6ad2b49acbedb80786f6088a37fc21476882687a
Ranjan Paul <paulranjan694@gmail.com>
2026-04-01 00:24:20 +0530

    Time: 11 ms (68.01%), Space: 49.4 MB (46.75%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -17,32 +17,34 @@ class Solution {
         int[] res = new int[n+1];
 
         Arrays.fill(res,Integer.MAX_VALUE);
-
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0]-b[0]);
 
         res[k]=0;
-
         pq.offer(new int[]{0,k});
 
         while(!pq.isEmpty()){
-            int[] node = pq.poll();
-            int src = node[1], t = node[0];
+            int[] temp = pq.poll();
 
-            for(int[] nbrNode : adj.get(src)){
-                int v = nbrNode[0], t1 = nbrNode[1];
-                int tempTime = t1+t;
-                if(tempTime < res[v]){
-                    res[v] = tempTime;
-                    pq.offer(new int[]{tempTime, v});
+            int u = temp[1];
+            int t = temp[0];
+
+            for(int[] nbrs : adj.get(u)){
+                int v = nbrs[0];
+                int t1 = nbrs[1];
+                int newt = t+t1;
+
+                if(newt < res[v]){
+                    res[v] = newt;
+                    pq.offer(new int[]{newt,v});
                 }
             }
         }
 
         int minTime = Integer.MIN_VALUE;
-        for(int i=1;i<=n;i++){
-            if(res[i]==Integer.MAX_VALUE) return -1;
-            minTime = Math.max(minTime, res[i]);
+
+        for(int r=1;r<res.length;r++){
+            minTime = Math.max(minTime, res[r]);
         }
-        return minTime;
+        return minTime == Integer.MAX_VALUE ? -1 : minTime;
     }
 }
