5e3e77cb43928c873f8f1e4e68d1e390e5c1433c
Ranjan Paul <paulranjan694@gmail.com>
2026-03-31 23:12:22 +0530

    Time: 8 ms (28.1%), Space: 46.7 MB (96.69%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,42 +1,39 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];
         List<List<Integer>> adj = new ArrayList<>();
 
         for(int i=0;i<numCourses;i++){
-            adj.add(new ArrayList<>());
+            List<Integer> list = new ArrayList<>();
+            adj.add(list);
         }
 
-        for(int[] preq : prerequisites){
-            int u = preq[1];
-            int v = preq[0];
+        int[] indegree = new int[numCourses];
+        for(int i=0;i<prerequisites.length;i++){
+            int u = prerequisites[i][1];
+            int v = prerequisites[i][0];
 
             adj.get(u).add(v);
             indegree[v]++;
         }
 
-        Queue<Integer> q = new LinkedList<>();
-
-        int count=0;
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> topo = new ArrayList<>();
         for(int i=0;i<numCourses;i++){
             if(indegree[i] == 0){
-                q.offer(i);
-                count++;
+                queue.add(i);
             }
         }
 
-        while(!q.isEmpty()){
-            int u = q.poll();
-
-            for(int v : adj.get(u)){
-                indegree[v]--;
-                if(indegree[v]==0){
-                    q.offer(v);
-                    count++;
+        while(!queue.isEmpty()){
+            int u = queue.poll();
+            topo.add(u);
+            for(int nbr : adj.get(u)){
+                indegree[nbr]--;
+                if(indegree[nbr] == 0){
+                    queue.add(nbr);
                 }
             }
         }
-
-        return count == numCourses;
+        return topo.size() == numCourses;
     }
 }
